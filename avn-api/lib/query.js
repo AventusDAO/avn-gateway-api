6fe96fe13c434f960cd8d16882e0762b004926b7
fluorostani <glyn.dimond@artos.io>
2021-09-23 16:08:05 +0100

    address comment

--- avn-api/lib/query.js
+++ avn-api/lib/query.js
@@ -1,7 +1,8 @@
 const axios = require('axios');
 
 const Query = function Query(gateway) {
-  this.gateway = gateway;
+  Query.gateway = gateway;
+  this.gateway = Query.gateway;
   this.id = 1;
   this.getTotalAvt = Query.getTotalAvt;
   this.getAvtBalance = Query.getAvtBalance;
@@ -9,25 +10,26 @@ const Query = function Query(gateway) {
   this.getAccountNonce = Query.getAccountNonce;
 };
 
+Query.gateway;
+
 Query.getTotalAvt = async function () {
-  return await postRequest(this.gateway, {jsonrpc: '2.0', id: this.id++, method: 'getTotalAvt', params: []});
+  return await postRequest({jsonrpc: '2.0', id: this.id++, method: 'getTotalAvt', params: []});
 };
 
 Query.getAvtBalance = async function (account) {
-  return await postRequest(this.gateway, {jsonrpc: '2.0', id: this.id++, method: 'getAvtBalance', params: [account]});
+  return await postRequest({jsonrpc: '2.0', id: this.id++, method: 'getAvtBalance', params: [account]});
 };
 
 Query.getTokenBalance = async function (account, token) {
-  return await postRequest(this.gateway, {jsonrpc: '2.0', id: this.id++, method: 'getTokenBalance', params: [account, token]});
+  return await postRequest({jsonrpc: '2.0', id: this.id++, method: 'getTokenBalance', params: [account, token]});
 };
 
 Query.getAccountNonce = async function (account) {
-  return await postRequest(this.gateway, {jsonrpc: '2.0', id: this.id++, method: 'getAccountNonce', params: [account]});
+  return await postRequest({jsonrpc: '2.0', id: this.id++, method: 'getAccountNonce', params: [account]});
 };
 
-
-async function postRequest(gateway, request) {
-  const response = (await axios.post(gateway, request)).data;
+async function postRequest(request) {
+  const response = (await axios.post(Query.gateway, request)).data;
   return response.result || response.error.message;
 }
 

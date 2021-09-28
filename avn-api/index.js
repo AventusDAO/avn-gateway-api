6252b94454544600ab31b6a9b839010be7aca7c3
fluorostani <glyn.dimond@artos.io>
2021-09-28 17:07:53 +0100

    Update index.js

--- avn-api/index.js
+++ avn-api/index.js
@@ -5,12 +5,13 @@ const Poll = require('./lib/poll.js');
 
 const AvnApi = function AvnApi(gateway) {
   let id = 1;
-  this.id = () => id++;
+  this.id = id;
+  this.nextId = () => id++;
   this.gateway = gateway;
   this.version = version;
-  this.query = new Query(gateway, this.id);
-  this.send = new Send(gateway, this.id);
-  this.poll = new Poll(gateway, this.id);
+  this.query = new Query(gateway, this.nextId);
+  this.send = new Send(gateway, this.nextId);
+  this.poll = new Poll(gateway, this.nextId);
 };
 
 

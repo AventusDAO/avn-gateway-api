585811e3032ee438402ac8ee014ea359959bfc83
fluorostani <39763263+fluorostani@users.noreply.github.com>
2021-09-28 10:43:09 +0100

    SYS-1291 Begin adding send methods to avn-api (#14)

--- avn-api/index.js
+++ avn-api/index.js
@@ -1,10 +1,12 @@
 const version = require('./package.json').version;
 const Query = require('./lib/query.js');
+const Send = require('./lib/send.js');
 
 const AvnApi = function AvnApi(gateway) {
-  this.gatweway = gateway;
+  this.gateway = gateway;
   this.version = version;
   this.query = new Query(gateway);
+  this.send = new Send(gateway);
 };
 
 module.exports = AvnApi;
--- avn-api/test.js
+++ avn-api/test.js
@@ -15,15 +15,23 @@ const token = '0x2adce7ada36d86253aa63bcf4aad9f84ccb9480e';
 async function main() {
   const api = new AvnApi('https://n67ibi1ujh.execute-api.eu-west-2.amazonaws.com');
 
-  console.log('Version:                             ', api.version);
-  console.log('Total AVT:                           ', await api.query.getTotalAvt());
-  console.log('User AVT balance by address:         ', await api.query.getAvtBalance(user1.address));
-  console.log('User AVT balance by public key:      ', await api.query.getAvtBalance(user1.publicKey));
-  console.log('User token balance by address:       ', await api.query.getTokenBalance(user2.address, token));
-  console.log('User token balance by public key:    ', await api.query.getTokenBalance(user2.publicKey, token));
-  console.log('User account nonce by address:       ', await api.query.getAccountNonce(user1.address));
-  console.log('User account nonce by public key:    ', await api.query.getAccountNonce(user1.publicKey));
-  console.log('Expect error:                        ', await api.query.getAccountNonce('0000'));
+
+  console.log('\n***INFO**');
+  console.log('Version:', api.version);
+  console.log('Gateway:', api.gateway);
+
+  console.log('\n***QUERIES***');
+  console.log('Total AVT:                             ', await api.query.getTotalAvt());
+  console.log('User AVT balance by address:           ', await api.query.getAvtBalance(user1.address));
+  console.log('User AVT balance by public key:        ', await api.query.getAvtBalance(user2.publicKey));
+  console.log('User token balance by address:         ', await api.query.getTokenBalance(user1.address, token));
+  console.log('User token balance by public key:      ', await api.query.getTokenBalance(user2.publicKey, token));
+  console.log('User account nonce by address:         ', await api.query.getAccountNonce(user1.address));
+  console.log('User account nonce by public key:      ', await api.query.getAccountNonce(user2.publicKey));
+  console.log('Expect error:                          ', await api.query.getAccountNonce('0000'));
+
+  console.log('\n***TRANSACTIONS***');
+  console.log('Transfer AVT using recipient address:  ', await api.send.transferAvt(user2.address, '1'));
 
   return;
 }

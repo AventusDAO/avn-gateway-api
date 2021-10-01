96f98d12c60553ff432e18457dfcb527555e165b
Nahu Seyoum <nahu.seyoum@artos.io>
2021-10-02 00:39:58 +0100

    verify awt token

--- lambdas/authorisation-handler/index.js
+++ lambdas/authorisation-handler/index.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const bigInt = require('big-integer');
+const { hexToU8a, u8aToHex, u8aConcat } = require('@polkadot/util');
 const { cryptoWaitReady, signatureVerify } = require('@polkadot/util-crypto');
 const { TypeRegistry } = require('@polkadot/types');
 
@@ -19,64 +20,81 @@ const ValidRequestResponse = {
 };
 
 exports.handler = async(event) => {
+  // encapsulate all the logic to make local testing easier
+  return await validateAwtToken(event);
+};
 
+async function validateAwtToken(event) {
   console.log("Authorisation lambda called");
 
   const awtToken = getAwtTokenIfAny(event);
-
-  if (awtToken) {
-      // check the age of the token
-      if (tokenAgeIsValid(awtToken) !== true) {
-        console.log(`Token is either expired or issued in the future`);
-        return InvalidRequestResponse;
-      }
-
-      // check the signature
-      if (isSignatureValid(awtToken) !== true) {
-        console.log(`The avnPublicKey signature is not valid`);
-        return InvalidRequestResponse;
-      }
-
-      // check the user balance on the chain
-      const hasAvtBalance = await userHasAvtBalance(awtToken);
-      if (hasAvtBalance !== true) {
-        console.log(`User does not have enough balance to use the avn gateway api`);
-        return InvalidRequestResponse;
-      }
-
-      return ValidRequestResponse;
+  if (!awtToken) {
+    console.log(`Token is missing or not correctly formatted`);
+    return InvalidRequestResponse;
   }
 
-  return InvalidRequestResponse;
-};
+  // check the age of the token
+  if (tokenAgeIsValid(awtToken) !== true) {
+    console.log(`Token is either expired or issued in the future`);
+    return InvalidRequestResponse;
+  }
+
+  // check the signature
+  if (await isSignatureValid(awtToken) !== true) {
+    console.log(`The avnPublicKey signature is not valid`);
+    return InvalidRequestResponse;
+  }
+
+  // check the user balance on the chain
+  if (await userHasAvtBalance(awtToken) !== true) {
+    console.log(`User does not have enough balance to use the avn gateway api`);
+    return InvalidRequestResponse;
+  }
+
+  return ValidRequestResponse;
+}
+
+/*
+    Helper functions
+*/
 
 async function userHasAvtBalance(awtToken) {
   // query the chain for balance info
   try {
     const response = await axios.post(AVN_API_QUERY_ENDPOINT, {palletName: 'system', storageName: 'account', params: [awtToken.pk]});
     const avtBalance = bigInt(response.data.data.free.replace('0x',''), 16);
-    return avtBalance.gt(MIN_AVT_BALANCE);
+    console.log(`AVT balance: ${avtBalance.toString()}`)
+    return avtBalance.geq(MIN_AVT_BALANCE);
   } catch (err) {
     console.log(`Error checking AVT balance for user: ${err}`);
     return false;
   }
 }
 
-function isSignatureValid(awtToken) {
-  // run this await code after as much validation as possible
-  await cryptoWaitReady();
+async function isSignatureValid(awtToken) {
+  try {
+    // run this await code after as much validation as possible
+    await cryptoWaitReady();
 
-  const encodedAvnPublicKey = encodeAvnPublicKeyForVerification(awtToken.pk, awtToken.iat);
-
-  const verificationResult = await signatureVerify(encodedAvnPublicKey, awtToken.sig, awtToken.pk);
-  return verificationResult.isValid;
+    const encodedAvnPublicKey = encodeAvnPublicKeyForVerification(awtToken.pk, awtToken.iat);
+    const verificationResult = await signatureVerify(encodedAvnPublicKey, awtToken.sig, awtToken.pk);
+    return verificationResult.isValid;
+  } catch (err) {
+    console.error(`Error verifying awt token signature: ${err}`);
+    return false;
+  }
 }
 
 function tokenAgeIsValid(token) {
-  const issuedAt = new Date(token.iat);
-  const tokenAge = new Date() - issuedAt;
+  try {
+    const issuedAt = new Date(token.iat);
+    const tokenAge = new Date() - issuedAt;
 
-  return tokenAge > 0 && tokenAge > MAX_TOKEN_AGE_MSEC;
+    return tokenAge > 0 && tokenAge < MAX_TOKEN_AGE_MSEC;
+  } catch (err) {
+    console.error(`Error checking the age of the awt token: ${err}`);
+    return false;
+  }
 }
 
 function getAwtTokenIfAny(event) {
@@ -87,17 +105,16 @@ function getAwtTokenIfAny(event) {
       return JSON.parse(decodedToken);
     }
   } catch (err) {
-    console.error(`Error extracting awt token from request: ${err}`)
+    console.error(`Error extracting awt token from request: ${err}`);
+    return null;
   }
-
-  return null;
 }
 
 function encodeAvnPublicKeyForVerification(avnPublicKey, issuedAt) {
   const encodedData = u8aConcat(
-      registry.createType('Text', SIGNING_CONTEXT).toU8a(false),
-      registry.createType('AccountId', hexToU8a(avnPublicKey)).toU8a(true),
-      registry.createType('Text', issuedAt).toU8a(false)
+    registry.createType('Text', SIGNING_CONTEXT).toU8a(false),
+    registry.createType('AccountId', hexToU8a(avnPublicKey)).toU8a(true),
+    registry.createType('Text', issuedAt).toU8a(false)
   );
 
   return u8aToHex(encodedData);

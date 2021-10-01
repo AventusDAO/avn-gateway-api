4328ea000db98cbb81f919aad87e2e2d3e205b41
Nahu Seyoum <nahu.seyoum@artos.io>
2021-10-01 19:20:02 +0100

    wip

--- lambdas/authorisation-handler/index.js
+++ lambdas/authorisation-handler/index.js
@@ -1,48 +1,97 @@
+const axios = require('axios');
+const bigInt = require('big-integer');
 const { cryptoWaitReady, signatureVerify } = require('@polkadot/util-crypto');
 const { TypeRegistry } = require('@polkadot/types');
 
+const AVN_API_QUERY_ENDPOINT = 'http://ec2-35-178-74-219.eu-west-2.compute.amazonaws.com:3000/avnQuery';
 const SIGNING_CONTEXT = 'awt_gateway_api';
 const MAX_TOKEN_AGE_MSEC = 60000;
+const MIN_AVT_BALANCE = bigInt("100000000000000000000");
+const AUTH_PREFIX = 'Bearer ';
 const registry = new TypeRegistry();
 
+const InvalidRequestResponse = {
+  "isAuthorized": false
+};
+
+const ValidRequestResponse = {
+  "isAuthorized": true
+};
 
 exports.handler = async(event) => {
-  const tokenPrefix = 'Bearer ';
+
   console.log("Authorisation lambda called");
 
-  let response = {
-      "isAuthorized": false
-  };
+  const awtToken = getAwtTokenIfAny(event);
 
-  const hasAwtToken = event.headers.authorization &&
-      (event.headers.authorization.startsWith(tokenPrefix) || event.headers.authorization.startsWith(tokenPrefix.toLowerCase()));
-
-  if (hasAwtToken) {
-      const token = event.headers.authorization.split(' ')[1];
-      const decodedToken = JSON.parse(Buffer.from(token, 'base64').toString('ascii'));
-
+  if (awtToken) {
       // check the age of the token
-      const issuedAt = new Date(decodedToken.iat);
-      const tokenAge = new Date() - issuedAt;
-
-      if (tokenAge < 0 || tokenAge > MAX_TOKEN_AGE_MSEC) {
-        response.isAuthorized = false;
-        console.log(`Token is either expired or issue in the future`);
+      if (tokenAgeIsValid(awtToken) !== true) {
+        console.log(`Token is either expired or issued in the future`);
+        return InvalidRequestResponse;
       }
 
-      const encodedAvnPublicKey = encodeAvnPublicKeyForVerification(decodedToken.pk, decodedToken.iat);
-
-      // no point doing this before we make sure there is a valid token
-      await cryptoWaitReady();
-
-      const verificationResult = await signatureVerify(encodedAvnPublicKey, decodedToken.sig, decodedToken.pk);
-      if (verificationResult.isValid === true) {
-        response.isAuthorized = true;
+      // check the signature
+      if (isSignatureValid(awtToken) !== true) {
+        console.log(`The avnPublicKey signature is not valid`);
+        return InvalidRequestResponse;
       }
+
+      // check the user balance on the chain
+      const hasAvtBalance = await userHasAvtBalance(awtToken);
+      if (hasAvtBalance !== true) {
+        console.log(`User does not have enough balance to use the avn gateway api`);
+        return InvalidRequestResponse;
+      }
+
+      return ValidRequestResponse;
   }
 
-  return response;
+  return InvalidRequestResponse;
 };
+
+async function userHasAvtBalance(awtToken) {
+  // query the chain for balance info
+  try {
+    const response = await axios.post(AVN_API_QUERY_ENDPOINT, {palletName: 'system', storageName: 'account', params: [awtToken.pk]});
+    const avtBalance = bigInt(response.data.data.free.replace('0x',''), 16);
+    return avtBalance.gt(MIN_AVT_BALANCE);
+  } catch (err) {
+    console.log(`Error checking AVT balance for user: ${err}`);
+    return false;
+  }
+}
+
+function isSignatureValid(awtToken) {
+  // run this await code after as much validation as possible
+  await cryptoWaitReady();
+
+  const encodedAvnPublicKey = encodeAvnPublicKeyForVerification(awtToken.pk, awtToken.iat);
+
+  const verificationResult = await signatureVerify(encodedAvnPublicKey, awtToken.sig, awtToken.pk);
+  return verificationResult.isValid;
+}
+
+function tokenAgeIsValid(token) {
+  const issuedAt = new Date(token.iat);
+  const tokenAge = new Date() - issuedAt;
+
+  return tokenAge > 0 && tokenAge > MAX_TOKEN_AGE_MSEC;
+}
+
+function getAwtTokenIfAny(event) {
+  try {
+    const rawToken = event.headers.authorization;
+    if (rawToken && (rawToken.startsWith(AUTH_PREFIX) || rawToken.startsWith(AUTH_PREFIX.toLowerCase()))) {
+      const decodedToken = Buffer.from(rawToken.split(' ')[1], 'base64').toString('ascii');
+      return JSON.parse(decodedToken);
+    }
+  } catch (err) {
+    console.error(`Error extracting awt token from request: ${err}`)
+  }
+
+  return null;
+}
 
 function encodeAvnPublicKeyForVerification(avnPublicKey, issuedAt) {
   const encodedData = u8aConcat(

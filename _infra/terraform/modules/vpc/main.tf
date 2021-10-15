f8c0ca61c2ec96034baee74bd19c6ab38286df2e
sleepert <john.terry@aventus.io>
2021-10-15 14:33:53 +0100

    create Ireland VPC and peer to dev

---
+++ _infra/terraform/modules/vpc/main.tf
@@ -0,0 +1,69 @@
+resource "aws_vpc" "gateway" {
+  cidr_block           = var.vpc_cidr_block
+  instance_tenancy     = var.instance_tenancy
+  enable_dns_hostnames = var.enable_dns_hostnames
+
+  tags = {
+    Name = var.name
+  }
+}
+
+resource "aws_subnet" "private_subnets" {
+  for_each   = var.private_zone_ips
+  vpc_id     = aws_vpc.gateway.id
+  cidr_block = each.value
+
+  tags = {
+    Name = "${var.name}-private-${each.key}"
+  }
+}
+
+resource "aws_subnet" "public_subnets" {
+  for_each   = var.public_zone_ips
+  vpc_id     = aws_vpc.gateway.id
+  cidr_block = each.value
+
+  tags = {
+    Name = "${var.name}-public-${each.key}"
+  }
+}
+
+resource "aws_nat_gateway" "gateway" {
+  connectivity_type = "public"
+  subnet_id         = aws_subnet.public_subnets["a"].id
+}
+
+resource "aws_vpc_peering_connection" "gateway_api" {
+  peer_owner_id = var.avn_vpc_owner_id
+  peer_vpc_id   = var.avn_vpc_id
+  peer_region   = var.peer_region
+  vpc_id        = aws_vpc.gateway.id
+  auto_accept   = false
+
+  requester {
+    allow_remote_vpc_dns_resolution = var.enable_dns_hostnames
+  }
+
+  tags = {
+    "Side" = "Requester"
+  }
+}
+
+provider "aws" {
+  alias  = "avn"
+  region = var.peer_region
+
+  assume_role {
+    role_arn = "arn:aws:iam::602004642405:role/jenkins-access"
+  }
+}
+
+resource "aws_vpc_peering_connection_accepter" "peer" {
+  provider                  = aws.avn
+  vpc_peering_connection_id = aws_vpc_peering_connection.gateway_api.id
+  auto_accept               = true
+
+  tags = {
+    Side = "Accepter"
+  }
+}
---
+++ _infra/terraform/modules/vpc/variables.tf
@@ -0,0 +1,65 @@
+variable "name" {
+  type        = string
+  description = "VPC tagged name"
+  default     = "gateway-api"
+}
+
+variable "region" {
+  type    = string
+  default = "eu-west-2"
+}
+
+variable "instance_tenancy" {
+  type        = string
+  description = "Instance launch configuration, can be one of default, dedicated or host. See https://docs.aws.amazon.com/autoscaling/ec2/userguide/auto-scaling-dedicated-instances.html"
+  default     = "default"
+}
+
+variable "enable_dns_hostnames" {
+  type        = bool
+  description = "enable/disable DNS hostnames in the VPC"
+  default     = true
+}
+
+variable "vpc_cidr_block" {
+  type        = string
+  description = "VPC Cidr block, must contain enough IPs to allow to scale"
+  default     = "172.16.0.0/20"
+}
+
+variable "private_zone_ips" {
+  type        = map
+  description = "CIDR blocks of private availability zones"
+  default     = {
+    "a": "172.16.0.0/22",
+    "b": "172.16.4.0/22",
+    "c": "172.16.8.0/22"
+  }
+}
+
+variable "public_zone_ips" {
+  type        = map
+  description = "CIDR blocks of public availability zones"
+  default     = {
+    "a": "172.16.12.0/24",
+    "b": "172.16.13.0/24",
+    "c": "172.16.14.0/24"
+  }
+}
+
+variable "avn_vpc_owner_id" {
+  type        = string
+  description = "AWS account ID of the VPC used by the avn blockchain"
+  default     = "602004642405"
+}
+
+variable "avn_vpc_id" {
+  type        = string
+  description = "VPC ID that houses the AnN blockchain"
+}
+
+variable "peer_region" {
+  type        = string
+  description = "Region of the avn blockchain VPC"
+  default     = "eu-west-2"
+}

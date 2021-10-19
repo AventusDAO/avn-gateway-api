e70da78b9cbeb7d12fa65dfd818fff5e4deef6d6
sleepert <john.terry@aventus.io>
2021-10-19 11:50:36 +0100

    security group id array

--- _infra/terraform/modules/redis/main.tf
+++ _infra/terraform/modules/redis/main.tf
@@ -8,7 +8,7 @@ resource "aws_elasticache_cluster" "redis" {
   engine_version       = var.redis_version
   port                 = 6379
   subnet_group_name    = aws_elasticache_subnet_group.redis.name
-  security_group_ids   = aws_security_group.redis.id
+  security_group_ids   = [aws_security_group.redis.id]
 }
 
 resource "aws_elasticache_replication_group" "redis" {

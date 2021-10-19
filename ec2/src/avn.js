bfa45424c47cd11d3b41ba11492ecdbc0d58c2b2
fluorostani <glyn.dimond@artos.io>
2021-10-19 19:19:25 +0100

    Update avn.js

--- ec2/src/avn.js
+++ ec2/src/avn.js
@@ -74,16 +74,14 @@ async function poll(requestId) {
 }
 
 async function smartNonce(sender) {
-  let nonce = await redis.INCR(sender)
-  log.trace('REDIS NONCE', typeof nonce, nonce)
+  let address = sender.address.toString()
+  let nonce = await redis.INCR(address)
 
-  if (nonce == 1 || nonce == '1') {
+  if (nonce === 1) {
     nonce = await api.rpc.system.accountNextIndex(sender);
-    log.trace('CHAIN NONCE', typeof nonce, nonce)
-    await redis.SETEX(sender, SMARTNONCE_EXPIRY_IN_SECONDS, nonce)
-    log.trace('SET REDIS OKAY', nonce)
+    await redis.SETEX(address, SMARTNONCE_EXPIRY_IN_SECONDS, nonce)
   } else {
-    await redis.EXPIRE(sender, SMARTNONCE_EXPIRY_IN_SECONDS)
+    await redis.EXPIRE(address, SMARTNONCE_EXPIRY_IN_SECONDS)
   }
 
   return nonce
@@ -94,9 +92,7 @@ async function signAndSend(txn) {
 
   try {
     log.trace(`Encoded Transaction: ${txn}`)
-    let nonce = await smartNonce(sender)
-    log.trace('SMARTNONCE', typeof nonce, nonce)
-
+    const nonce = await smartNonce(sender)
     let receipt = await txn.signAndSend(sender, { nonce: nonce });
     let requestId = receipt.toString()
     result = { requestId: requestId }

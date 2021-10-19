b6c8646924c4d742d9f79607b5893b41bcfce62e
sleepert <john.terry@aventus.io>
2021-10-19 11:49:06 +0100

    output public subnet ips

--- _infra/terraform/environments/sandbox/main.tf
+++ _infra/terraform/environments/sandbox/main.tf
@@ -28,5 +28,5 @@ module "redis" {
   replication_enabled = false
   vpc_id              = module.vpc.vpc_id
   subnet_ids          = module.vpc.private_subnets
-  ip_whitelist        = module.vpc.public_subnets
+  ip_whitelist        = module.vpc.public_subnet_ips
 }
--- _infra/terraform/modules/vpc/outputs.tf
+++ _infra/terraform/modules/vpc/outputs.tf
@@ -13,3 +13,9 @@ output "public_subnets" {
     for subnet in aws_subnet.public_subnets : subnet.id
   ])
 }
+
+output "public_subnet_ips" {
+  value = toset([
+    for subnet in aws_subnet.public_subnets : subnet.cidr_block
+  ])
+}

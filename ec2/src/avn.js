062d4972c34db412437c24f5c8860f0cfff20c19
nahuseyoum <39748285+nahuseyoum@users.noreply.github.com>
2021-10-20 15:17:47 +0100

    SYS-1281 Store requestId in redis when sending transactions (#55)

--- ec2/src/avn.js
+++ ec2/src/avn.js
@@ -4,15 +4,9 @@ const config = require('multiconfig').load()
 const log4js = require('log4js')
 const log = log4js.getLogger()
 const avn_types = require('./avnTypes')
+const redis = require('./redis')
 
-const POLL_STATES = {
-  Pending: 'Pending',
-  Processed: 'Processed',
-  Rejected: 'Rejected',
-  SendingFailed: 'SendingFailed'
-}
-
-const URL = config.avnUrl
+const AVN_URL = config.avnUrl
 const SENDER = config.senderSuri
 let api, sender
 
@@ -56,9 +50,9 @@ async function poll(requestId) {
     const response = res.data.data.hits.hits
 
     if (response.length > 0) {
-      state = response[0]._source.isFailed === true ? POLL_STATES.Rejected : POLL_STATES.Processed
+      state = response[0]._source.isFailed === true ? 'Rejected' : 'Processed'
     } else {
-      state = POLL_STATES.Pending
+      state = 'Pending'
     }
 
     result = { state: state }
@@ -81,51 +75,48 @@ async function signAndSend(txn) {
 
     let receipt = await signedTransaction.send()
     let requestId = receipt.toString()
-    result = { requestId: requestId }
 
-    // TODO: Add logic to store the requestId somewhere (probably a DB) so it can be used when Polling a result
+    result = { requestId }
   } catch (err) {
     log.error(`Failed sending transaction: ${err}`)
     throw err
   }
 
+  // TODO: Add nonce to addPendingAvnTransaction if available
+  redis.addPendingAvnTransaction(result.requestId, sender.address.toString())
+
   return result
 }
 
-async function connectToAvN(url) {
-  let provider = new WsProvider(url)
-  const avnConnection = await ApiPromise.create({
+async function connectToAvN() {
+  log.info(`Creating a connection to the AVN on: ${AVN_URL}`)
+
+  let provider = new WsProvider(AVN_URL)
+  api = await ApiPromise.create({
     provider,
     types: avn_types.description,
     typesSpec: avn_types.nodeTypes
   })
 
+  sender = createAccount(SENDER)
+
   const [chain, nodeName, nodeVersion] = await Promise.all([
-    avnConnection.rpc.system.chain(),
-    avnConnection.rpc.system.name(),
-    avnConnection.rpc.system.version()
+    api.rpc.system.chain(),
+    api.rpc.system.name(),
+    api.rpc.system.version()
   ])
 
-  log.info(`You are connected to chain ${chain} (${URL}) using ${nodeName} v${nodeVersion}\n`)
-
-  return avnConnection
+  log.info(`You are connected to chain ${chain} (${AVN_URL}) using ${nodeName} v${nodeVersion}\n`)
+  log.info(`Relayer address: ${sender.address.toString()}`)
 }
 
-async function createAccount(suri) {
+function createAccount(suri) {
   const keyring = new Keyring({ type: 'sr25519' })
-  return await keyring.addFromUri(suri)
-}
-
-async function instantiateEC2() {
-  log.info(`Creating a connection to the AVN on: ${URL}`)
-  api = await connectToAvN(URL)
-
-  sender = await createAccount(SENDER)
-  log.info(`Using sender with address: ${sender.address.toString()}`)
+  return keyring.addFromUri(suri)
 }
 
 module.exports = {
-  instantiateEC2,
+  connectToAvN,
   query,
   tx,
   proxy,

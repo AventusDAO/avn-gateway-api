539a13907ef37f0299eac5e8d98344b26f4df214
fluorostani <glyn.dimond@artos.io>
2021-10-20 17:44:26 +0100

    Update redis.js

--- ec2/src/redis.js
+++ ec2/src/redis.js
@@ -93,14 +93,13 @@ function buildTransactionJson(senderAddress, senderNonce) {
 }
 
 async function getNextNonce(senderAddress) {
-  const key = NONCE_NAMESPACE + senderAddress
-  const [nextNonce, ignore]  = await redisClient.multi().incr(key).expire(key, NONCE_EXPIRY_IN_SECONDS).exec()
+  const [nextNonce, ignore]  = await redisClient.multi().incr(senderAddress).expire(senderAddress, NONCE_EXPIRY_IN_SECONDS).exec()
   // If the nonce does not exist (or has expired) redis will return an incremented 0 value, i.e.: 1
   return (nextNonce === 1) ? undefined : nextNonce
 }
 
 async function setNonce(senderAddress, nonce) {
-  await redisClient.setEx(NONCE_NAMESPACE + senderAddress, NONCE_EXPIRY_IN_SECONDS, nonce.toString())
+  await redisClient.setEx(senderAddress, NONCE_EXPIRY_IN_SECONDS, nonce.toString())
 }
 
 module.exports = {

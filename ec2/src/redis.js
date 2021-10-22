d1b7984308d8a7ac048db6018814bba3eaed23e1
Nahu Seyoum <nahu.seyoum@artos.io>
2021-10-22 14:39:37 +0100

    update max limit

--- ec2/src/redis.js
+++ ec2/src/redis.js
@@ -22,7 +22,7 @@ const transactionStates = {
 }
 
 const PENDING_TRANSACTIONS_KEY = 'PendingTransactionsList'
-const MAX_RANDOM_SELECTION_SIZE = 2000
+const MAX_RANDOM_SELECTION_SIZE = 1500
 const NONCE_NAMESPACE = 'Nonce.'
 const NONCE_EXPIRY_IN_SECONDS = 5
 

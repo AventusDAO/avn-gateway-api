415a8da491880e07f01c069f665f5250924d2851
Nahu Seyoum <nahu.seyoum@artos.io>
2021-10-22 01:30:40 +0100

    added temp logging

--- ec2/src/redis.js
+++ ec2/src/redis.js
@@ -82,11 +82,17 @@ async function updateAvnTransactionStatus(transactionHash, status, blockNumber) 
 }
 
 async function resolvePendingAvnTransactions(transactions) {
-  for (const tx of transactions) {
-    await updateAvnTransactionStatus(tx.transactionHash, tx.state, tx.blockNumber)
-  }
-  // const txPromises = transactions.map(tx => updateAvnTransactionStatus(tx.transactionHash, tx.state, tx.blockNumber))
-  // await Promise.allSettled(txPromises)
+  // for (const tx of transactions) {
+  //   await updateAvnTransactionStatus(tx.transactionHash, tx.state, tx.blockNumber)
+  // }
+
+  const txPromises = transactions.map(tx => updateAvnTransactionStatus(tx.transactionHash, tx.state, tx.blockNumber))
+  const r = await Promise.allSettled(txPromises)
+
+  //TODO: Temporary logging - remove me
+  const fulfilled = r.filter(p => p.status === 'fulfilled').length
+  log.trace(`Total result: ${r}, fulfilled: ${fulfilled}`)
+  return fulfilled
 }
 
 async function getAllPendingTransactions() {

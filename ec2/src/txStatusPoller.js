91b5110e410f7163ac17af44ecde5644249e0e8e
Nahu Seyoum <nahu.seyoum@artos.io>
2021-10-25 13:03:15 +0100

    adding region

--- ec2/src/txStatusPoller.js
+++ ec2/src/txStatusPoller.js
@@ -4,6 +4,11 @@ const log = require('log4js').configure(config.log4Js).getLogger();
 
 async function resolvePendingTransactionsState() {
   log.trace(`Invoking status update handler lambda`)
+
+  AWS.config.update({
+    region: 'eu-west-1',
+  });
+
   new AWS.Lambda().invoke({
     FunctionName: 'tx-status-update-handler'
   }).promise();

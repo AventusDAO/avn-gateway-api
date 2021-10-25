130bc084f8777076b95c1f5033d61440c2de9302
Nahu Seyoum <nahu.seyoum@artos.io>
2021-10-25 12:29:29 +0100

    remove the keys

--- ec2/src/txStatusPoller.js
+++ ec2/src/txStatusPoller.js
@@ -2,29 +2,11 @@ const config = require('multiconfig').load();
 const AWS = require('aws-sdk');
 const log = require('log4js').configure(config.log4Js).getLogger();
 
-const params = {
-  FunctionName: 'tx-status-update-handler'
-};
-
 async function resolvePendingTransactionsState() {
-  // You shouldn't hard-code your keys in production!
-  // http://docs.aws.amazon.com/AWSJavaScriptSDK/guide/node-configuring.html
-
-  AWS.config.update({
-    // accessKeyId: 'AWSAccessKeyId',
-    // secretAccessKey: 'AWSAccessKeySecret',
-    region: 'eu-west-1',
-  });
-
-  AWS.config.getCredentials(function(err) {
-    if (err) log.error(err.stack);
-    // credentials not loaded
-    else {
-      log.info(`Access key: ${AWS.config.credentials.accessKeyId}, Region: ${AWS.config.region}`);
-    }
-  });
-
-  new AWS.Lambda().invoke(params).promise();
+  log.trace(`Invoking status update handler lambda`)
+  new AWS.Lambda().invoke({
+    FunctionName: 'tx-status-update-handler'
+  }).promise();
 };
 
 module.exports = {

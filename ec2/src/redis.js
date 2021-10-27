f2230540c67fda30db07083bff3b8bbb3f115a87
fluorostani <39763263+fluorostani@users.noreply.github.com>
2021-10-27 17:32:47 +0100

    Update ec2/src/redis.js
    Co-authored-by: nahuseyoum <39748285+nahuseyoum@users.noreply.github.com>

--- ec2/src/redis.js
+++ ec2/src/redis.js
@@ -42,7 +42,6 @@ async function connect() {
     }
   ]);
 
-
   new Redis(config.redis.redisUrl)
   log.info('Connected to Redis database:',(await redisClient.hello()).map((e,i) => i%2 == 0 ?  e+':': e+', ').join(''))
 

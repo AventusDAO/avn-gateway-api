c1674421937170b4b0cfe4457d3a074bd49249cf
sleepert <john.terry@aventus.io>
2021-10-27 17:04:44 +0100

    SYS-1472 eks terraform module (#66)
    * Create the EKS module and apply to sandbox
    
    * assume jenkins role
    
    * assume jenkins-access for s3
    
    * assume role
    
    * remove encryption for sandbox
    
    * apply aws supported terraform module
    
    * resolve confict
    
    * reapply encrytion to statefile

--- _infra/terraform/environments/sandbox/main.tf
+++ _infra/terraform/environments/sandbox/main.tf
@@ -1,4 +1,11 @@
 
+locals {
+  name            = "stargate"
+  cluster_version = "1.21"
+  region          = "eu-west-1"
+  account_id      = "352429414196"
+}
+
 module "lambda_functions" {
   source               = "../../modules/lambda"
   region               = var.region
@@ -21,4 +28,72 @@ module "vpc" {
   avn_vpc_id               = "vpc-074c6e19e26ba4a23"
   peer_public_route_table  = "rtb-0a0b61707b33e0a75"
   peer_private_route_table = "rtb-00b575bea946b34bc"
+
+  private_subnet_additional_tags = {
+    "kubernetes.io/cluster/${local.name}" = "shared"
+    "kubernetes.io/role/internal-elb"     = "1"
+  }
+
+  public_subnet_additional_tags = {
+    "kubernetes.io/cluster/${local.name}" = "shared"
+    "kubernetes.io/role/elb"              = "1"
+  }
+}
+
+data "aws_eks_cluster" "eks" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "eks" {
+  name = module.eks.cluster_id
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.eks.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.eks.token
+}
+
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+
+  cluster_version = local.cluster_version
+  cluster_name    = local.name
+  vpc_id          = module.vpc.vpc_id
+  subnets         = module.vpc.private_subnets
+
+  cluster_endpoint_private_access = true
+  cluster_endpoint_public_access  = true
+
+  node_groups = {
+    stargate = {
+      create_launch_template = true
+
+      disk_size       = 20
+      disk_type       = "gp3"
+
+      desired_capacity = 1
+      max_capacity     = 10
+      min_capacity     = 1
+
+      instance_types = ["t3.medium"]
+      capacity_type  = "ON_DEMAND"
+      k8s_labels = {
+        Environment = "sandbox"
+        GithubRepo  = "avn-gateway-api"
+        GithubOrg   = "Aventus-Network-Services"
+      }
+      update_config = {
+        max_unavailable = 1
+      }
+    }
+  }
+
+  map_roles = [
+    {
+      rolearn  = "arn:aws:iam::${local.account_id}:role/AWSReservedSSO_AdministratorAccess_a2b4587f5d23a564"
+      username = "adminuser:{{SessionName}}"
+      groups   = ["system:masters"]
+    },
+  ]
 }

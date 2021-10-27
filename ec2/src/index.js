a9816e7b914ff76421b0827f680c6ba61d6b4b9e
fluorostani <glyn.dimond@artos.io>
2021-10-27 17:37:24 +0100

    avnPort -> serverPort

--- ec2/src/index.js
+++ ec2/src/index.js
@@ -10,7 +10,7 @@ log4js.configure(config.log4Js)
 const log = log4js.getLogger()
 
 const app = express()
-const port = config.avnPort
+const port = config.serverPort
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json({limit: '50mb'}))

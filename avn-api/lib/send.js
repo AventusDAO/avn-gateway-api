a7bfcd677a20ab819e97c847ff08c127a14bd3c7
Nahu Seyoum <nahu.seyoum@artos.io>
2021-10-28 15:48:23 +0100

    SYS-1437 proxy avt support

--- avn-api/lib/send.js
+++ avn-api/lib/send.js
@@ -5,33 +5,38 @@ const proxyApi = require('./proxy.js');
 
 const MAX_TX_PROCESSING_TIME = 3000;
 
-function Send(api, queryApi) {
-  this.transferAvt = generateFunction(transferAvt, api);
+function Send(api, queryApi, avtContractAddress) {
+  this.transferAvt = generateFunction(transferAvt, api, queryApi);
   this.transferToken = generateFunction(transferToken, api, queryApi);
   this.nonceMap = {};
+  this.avtContractAddress = avtContractAddress;
 }
 
-function transferAvt(api) {
-  return async function(account, amount) {
-    return await this.postRequest(api, 'transferAvt', [account, amount.toString()]);
-  }
+function transferAvt(api, queryApi) {
+  return async function (relayer, from, to, amount) {
+    return await proxyTokenTransfer(api, queryApi, relayer, from, to, this.avtContractAddress, amount);
+  };
 }
 
 function transferToken(api, queryApi) {
   return async function (relayer, from, to, token, amount) {
-    let nonce = await this.smartNonce(queryApi, from);
-    let signature = proxyApi.transferToken.createAuthorisationSignature(relayer, from, to, token, amount, nonce);
-
-    return await this.postRequest(api, 'proxy',
-      {
-        pallet: 'tokenManager',
-        method: 'signedTransfer',
-        signature,
-        relayer,
-        innerArgs: {from, to, token, amount}
-      }
-    );
+    return await proxyTokenTransfer(api, queryApi, relayer, from, to, token, amount);
   };
+}
+
+function proxyTokenTransfer(api, queryApi, relayer, from, to, token, amount) {
+  let nonce = await this.smartNonce(queryApi, from);
+  let signature = proxyApi.transferToken.createAuthorisationSignature(relayer, from, to, token, amount, nonce);
+
+  return await this.postRequest(api, 'proxy',
+    {
+      pallet: 'tokenManager',
+      method: 'signedTransfer',
+      signature,
+      relayer,
+      innerArgs: {from, to, token, amount}
+    }
+  );
 }
 
 function generateFunction(functionName, api, queryApi) {

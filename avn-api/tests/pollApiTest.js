6afb9e45224fc26e01b00afa0f2f6ac9037f187a
Nahu Seyoum <nahu.seyoum@artos.io>
2021-10-29 12:18:37 +0100

    test for rejected transaction polling

--- avn-api/tests/pollApiTest.js
+++ avn-api/tests/pollApiTest.js
@@ -14,15 +14,30 @@ describe('Polling api calls:', async() => {
   })
 
   describe('requestState', async () => {
-    let requestId;
+    let requestId, invalidRequestId;
 
     before(async () => {
       requestId = await api.send.transferAvt(recipient, 1);
-      console.log(`polling requestId: ${requestId}`)
+      invalidRequestId = await api.send.transferAvt(recipient, "9000000000000000000000000");
+      console.log(`Valid requestId: ${requestId}, Invalid requestId: ${invalidRequestId}`)
     })
 
     it('returns a pending status for a valid request ID', async () => {
       assert.equal(await api.poll.requestState(requestId), 'Pending');
+    })
+
+    it('returns a rejected status when a transaction fails to be executed', async () => {
+      const maxPoll = 5;
+      let status;
+
+      for (i = 0; i < 10; i ++) {
+        await helper.sleep(3000);
+        status = await api.poll.requestState(invalidRequestId);
+        if (status === 'Rejected') break;
+        console.log(`   Current status (invalid request): ${status}`)
+      }
+
+      assert.equal(status, 'Rejected');
     })
 
     it('returns a processed status for a valid request ID', async () => {
@@ -33,7 +48,7 @@ describe('Polling api calls:', async() => {
         await helper.sleep(3000);
         status = await api.poll.requestState(requestId);
         if (status === 'Processed') break;
-        console.log(`   Current status: ${status}`)
+        console.log(`   Current status (valid request): ${status}`)
       }
 
       assert.equal(status, 'Processed');

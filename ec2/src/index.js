cd5203dc3f6b466c44d9f949c17573b2c30ed5a2
fluorostani <glyn.dimond@artos.io>
2021-10-29 11:16:31 +0100

    remove cluster

--- ec2/src/index.js
+++ ec2/src/index.js
@@ -4,10 +4,7 @@ const avn = require('./avn')
 const redis = require('./redis')
 const txStatusPoller = require('./txStatusPoller')
 const express = require('express')
-const cluster = require('cluster')
 const log4js = require('log4js')
-
-const numCPUs = require('os').cpus().length
 
 log4js.configure(config.log4Js)
 const log = log4js.getLogger()
@@ -87,21 +84,9 @@ app.post('/resolvePendingTransactions', async (req, res, next) => {
   }
 })
 
-if (cluster.isMaster) {
-  log.info(`EC2 Master ${process.pid} is running`)
-
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork()
-  }
-
-  cluster.on('exit', (worker, code, signal) => {
-    log.info(`worker ${worker.process.pid} died`)
-  })
-} else {
-  app.listen(port, () => {
-    log.info(`EC2 avn-connector listening on port ${port}`)
-  })
-}
+app.listen(port, () => {
+  log.info(`EC2 avn-connector listening on port ${port}`)
+})
 
 async function instantiateEC2() {
   await avn.connectToAvN()

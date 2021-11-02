9a5e3c345411d98efd88db9cf818718149a3cb2b
Steven Gong <54836231+steven-gong-aventus@users.noreply.github.com>
2021-11-02 16:03:16 +0000

    SYS-1509 Replaced the usage of aws-sdk in lambda with smaller client-secrets-manager sdk (#85)
    * Replaced the usage of aws-sdk in lambda with smaller client-secrets-manager sdk
    * Removed some ; in script files

--- lambdas/send-handler-mq/secretsManager.js
+++ lambdas/send-handler-mq/secretsManager.js
@@ -1,29 +1,26 @@
 'use strict'
 
-const AWS = require('aws-sdk');
+const { SecretsManagerClient, GetSecretValueCommand } = require("@aws-sdk/client-secrets-manager")
 
-// TODO: Fix the logger used by the caller:
-//   lambda function is using console to log
-//   ec2 script is using log4js
+module.exports = SecretsManager
 
-module.exports = SecretsManager;
-
-function SecretsManager(region, log) {
-  this.smClient = new AWS.SecretsManager({region: region});
-  this.log = log;
+function SecretsManager(region) {
+  this.smClient = new SecretsManagerClient({ region: region })
 }
 
 SecretsManager.prototype.getSecret = async function(secretId) {
-  let self = this;
-  return await new Promise((resolve, reject) => {
-    self.smClient.getSecretValue({SecretId: secretId}, function(err, data) {
-      if (err) {
-        self.log.error('[SECRET MANAGER] get secret value error', err.message);
-        throw err;
-      } else if ('SecretString' in data) {
-        const secret = JSON.parse(data.SecretString);
-        resolve(secret);
-      }
-    });
-  })
+  const params = { SecretId: secretId }
+  const command = new GetSecretValueCommand(params)
+  try {
+    const response = await this.smClient.send(command)
+    if ('SecretString' in response) {
+      const secret = JSON.parse(response.SecretString)
+      return secret
+    } else {
+      throw Error('SecretString not found in response')
+    }
+  } catch (error) {
+    console.error('[SECRET MANAGER] get secret value error', error.message)
+    throw error
+  }
 }

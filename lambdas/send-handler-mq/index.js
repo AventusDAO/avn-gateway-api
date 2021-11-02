ee093cb1acb6c8af6a4a8dbfecea528dbc57a596
Steven Gong <steven.gong@artos.io>
2021-11-02 11:05:21 +0000

    Replaced the usage of aws-sdk in lambda with smaller client-secrets-manager sdk.

--- lambdas/send-handler-mq/index.js
+++ lambdas/send-handler-mq/index.js
@@ -1,75 +1,75 @@
-const utils = require('../common/utils.js');
-const EC2 = require('../common/resources.json').ec2_endpoint;
-const axios = require('axios');
-const MQSender = require('./mqSender.js'); // TODO: SYS-1529 Create a lambda layer for the shared modules
+const utils = require('../common/utils.js')
+const EC2 = require('../common/resources.json').ec2_endpoint
+const axios = require('axios')
+const MQSender = require('./mqSender.js')
 
 // TODO: SYS-1546 To check if this needs an update after we setup the k8t proxy
-let mqSender;
+let mqSender
 
 const connectToMQ = async () => {
   if (!mqSender || !mqSender.amqpConnection) {
-    mqSender = new MQSender(process.env.SECRET_MANAGER_REGION, process.env.MQ_SECRET_ARN, process.env.MQ_BROKER_AMQP_ENDPOINT);
-    await mqSender.connectToMessageBroker();
+    mqSender = new MQSender(process.env.SECRET_MANAGER_REGION, process.env.MQ_SECRET_ARN, process.env.MQ_BROKER_AMQP_ENDPOINT)
+    await mqSender.connectToMessageBroker()
   } 
 }
 
 exports.handler = async (event) => {
-  await connectToMQ();
   try {
+    await connectToMQ()
     return {
       statusCode: 200,
       body: JSON.stringify(await processRequest(event.body))
-    };
+    }
   } catch(err) {
     return {
       statusCode: 500,
       error: {message: err.message}
     }
   }
-};
+}
 
 async function sendTx(queueName, palletName, method, params) {
   try {
     // TODO: SYS-1425 Create a global ID to return as response result.
-    return await mqSender.sendMessageToMQ(queueName, {palletName: palletName, method: method, params: params});
+    return await mqSender.sendMessageToMQ(queueName, {palletName: palletName, method: method, params: params})
   } catch (e) {
-    console.error('sendTx Error:', e.message);
-    return e.message;
+    console.error('sendTx Error:', e.message)
+    return e.message
   }
 }
 
 async function sendProxyTx(palletName, method, params) {
-  let response;
+  let response
   try {
-    response = await axios.post(EC2 + 'avnProxy', {palletName: palletName, method: method, params: params});
+    response = await axios.post(EC2 + 'avnProxy', {palletName: palletName, method: method, params: params})
   } catch (e) {
-    console.error('sendProxyTx Error:', e);
-    throw true;
+    console.error('sendProxyTx Error:', e)
+    throw true
   }
-  return response.data.requestId;
+  return response.data.requestId
 }
 
 async function processRequest(requestObject) {
-  let responseObject = {jsonrpc: '2.0'};
-  let call;
+  let responseObject = {jsonrpc: '2.0'}
+  let call
 
   try {
-    call = JSON.parse(requestObject);
+    call = JSON.parse(requestObject)
   } catch (e) {
-    console.error('error processing request object', e);
-    responseObject.error = {code:-32700, message:'Parse error'};
-    responseObject.id = null;
-    return responseObject;
+    console.error('error processing request object', e)
+    responseObject.error = {code:-32700, message:'Parse error'}
+    responseObject.id = null
+    return responseObject
   }
 
   if (typeof call.method !== 'string') {
-    responseObject.error = {code:-32600, message:'Invalid Request'};
+    responseObject.error = {code:-32600, message:'Invalid Request'}
   } else {
-    responseObject = await callSwitch(call, responseObject);
+    responseObject = await callSwitch(call, responseObject)
   }
 
-  responseObject.id = call.id;
-  return responseObject;
+  responseObject.id = call.id
+  return responseObject
 }
 
 async function callSwitch(call, responseObject) {
@@ -77,25 +77,25 @@ async function callSwitch(call, responseObject) {
     case 'transferAvt':
       if (utils.isValidAccountId(call.params[0]) && utils.isValidAmount(call.params[1])) {
         try {
-          responseObject.result = await sendTx(process.env.MQ_AVN_TX_QUEUE, 'balances', 'transfer', [call.params[0], call.params[1]]);
+          responseObject.result = await sendTx(process.env.MQ_AVN_TX_QUEUE, 'balances', 'transfer', [call.params[0], call.params[1]])
         } catch (e) {
-          responseObject.error = {code:-32603, message:'Internal error'};
+          responseObject.error = {code:-32603, message:'Internal error'}
         }
       } else {
-        responseObject.error = {code:-32602, message:'Invalid params'};
+        responseObject.error = {code:-32602, message:'Invalid params'}
       }
-      break;
+      break
 
     case 'proxy':
-      let pallet = call.params.pallet;
-      let method = call.params.method;
+      let pallet = call.params.pallet
+      let method = call.params.method
 
-      let formatter = codeFormatters[pallet][method];
+      let formatter = codeFormatters[pallet][method]
 
       if (!formatter) {
-        responseObject.error = {code:-32601, message:'Method not found'};
+        responseObject.error = {code:-32601, message:'Method not found'}
       } else if (!formatter.validate(call)) {
-        responseObject.error = {code:-32602, message:'Invalid params'};
+        responseObject.error = {code:-32602, message:'Invalid params'}
       } else {
         try {
           let proof = {
@@ -105,27 +105,27 @@ async function callSwitch(call, responseObject) {
               Sr25519: call.params.signature
             }
           }
-          responseObject.result = await sendProxyTx(pallet, method, formatter.encode(proof, call.params.innerArgs));
+          responseObject.result = await sendProxyTx(pallet, method, formatter.encode(proof, call.params.innerArgs))
         } catch (e) {
-          responseObject.error = {code:-32603, message:'Internal error'};
+          responseObject.error = {code:-32603, message:'Internal error'}
         }
       }
-      break;
+      break
 
     default:
-      responseObject.error = {code:-32601, message:'Method not found'};
+      responseObject.error = {code:-32601, message:'Method not found'}
   }
-  return responseObject;
+  return responseObject
 }
 
 const codeFormatters = {
   balances: {
     transfer : {
       validate: function(params0, params1) {
-        return (utils.isValidAccountId(params0) && utils.isValidAmount(params1));
+        return (utils.isValidAccountId(params0) && utils.isValidAmount(params1))
       },
       encode: function(params0, params1) {
-        return [params0, params1];
+        return [params0, params1]
       }
     },
   },
@@ -138,20 +138,20 @@ const codeFormatters = {
           && utils.isValidAccountId(call.params.innerArgs.to)
           && utils.isValidTokenId(call.params.innerArgs.token)
           && utils.isValidAmount(call.params.innerArgs.amount.toString())
-        );
+        )
       },
       encode: function(proof, innerArgs) {
-        return [proof, innerArgs.from, innerArgs.to, innerArgs.token, innerArgs.amount];
+        return [proof, innerArgs.from, innerArgs.to, innerArgs.token, innerArgs.amount]
       }
     }
   },
-};
+}
 
 // async function testlocal(n) {
-//   await connectToMQ();
-//   for (var i = 0; i < n; i++) {
-//     console.info('transferAvt:', await processRequest(`{"jsonrpc": "2.0", "method":"transferAvt", "params":["5DAgxVxKmnJ7hfhDEB9UetZm4jR2MPjGZGrmJZjirSVJDdMr", "2"], "id":${i}}`));
-//     await sleep(1000);
+//   await connectToMQ()
+//   for (var i = 0 i < n i++) {
+//     console.info('transferAvt:', await processRequest(`{"jsonrpc": "2.0", "method":"transferAvt", "params":["5DAgxVxKmnJ7hfhDEB9UetZm4jR2MPjGZGrmJZjirSVJDdMr", "2"], "id":${i}}`))
+//     await sleep(1000)
 //   }
 // }
 
@@ -159,4 +159,4 @@ const codeFormatters = {
 //   return new Promise((resolve, reject) => setTimeout(resolve, ms) )
 // }
 
-// testlocal(1);
+// testlocal(1)

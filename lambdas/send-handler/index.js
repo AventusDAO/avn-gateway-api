0c8ed8a2d839d7ab1497bcf64e3fac424ffa476b
Steven Gong <steven.gong@artos.io>
2021-11-05 14:26:10 +0000

    Copied common folde to a layer folder, and updated reference in send-handler.

--- lambdas/send-handler/index.js
+++ lambdas/send-handler/index.js
@@ -1,4 +1,4 @@
-const utils = require('../common/utils.js')
+const utils = require('../layer/utils.js')
 const MQSender = require('./mqSender.js')
 
 // TODO: SYS-1546 To check if this needs an update after we setup the k8t proxy

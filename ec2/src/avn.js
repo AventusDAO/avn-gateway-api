13bc8b0d6c849dd2fc42082888b33d203671aba8
Nahu Seyoum <nahu.seyoum@artos.io>
2021-11-05 17:07:04 +0000

    adding more logs

--- ec2/src/avn.js
+++ ec2/src/avn.js
@@ -39,8 +39,12 @@ async function poll(requestId) {
   }
 
   try {
+    log.info("Poll ", requestId)
+
     if (!isTransactionHash(requestId)) {
+      log.info("getting txHash for ", requestId)
       requestId = await redis.getTransactionHashByRequestId(requestId)
+      log.info("txHash: ", requestId)
     }
 
     let tx = await redis.getAvnTransaction(requestId)
@@ -90,6 +94,7 @@ async function signAndSend(requestId, txn) {
     throw err
   }
 
+  log.info("addPendingAvnTransaction: ", requestId, result.transactionHash)
   redis.addPendingAvnTransaction(requestId, result.transactionHash, sender.address.toString(), nonce.toString())
 
   return result

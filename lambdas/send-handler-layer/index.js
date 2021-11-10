3f6b488ece4b5a92b748c9a4b9efe5dfbb1ad491
Steven Gong <steven.gong@artos.io>
2021-11-10 22:25:53 +0000

    Restructured the layer files, and created a update-lambda.js file

---
+++ lambdas/send-handler-layer/index.js
@@ -0,0 +1,175 @@
+const utils = require('/opt/nodejs/utils.js')
+const MQSender = require('./mqSender.js')
+
+// TODO: SYS-1546 To check if this needs an update after we setup the k8t proxy
+let mqSender
+const connectToMQ = async () => {
+  if (!mqSender || !mqSender.amqpConnection || !mqSender.amqpConnected) {
+    mqSender = new MQSender(
+      process.env.SECRET_MANAGER_REGION,
+      process.env.MQ_SECRET_ARN,
+      process.env.MQ_BROKER_AMQP_ENDPOINT
+    )
+    await mqSender.connectToMessageBroker()
+  }
+}
+
+exports.handler = async event => {
+  try {
+    await connectToMQ()
+    return {
+      statusCode: 200,
+      body: JSON.stringify(await processRequest(event.body))
+    }
+  } catch (err) {
+    return {
+      statusCode: 500,
+      error: { message: err.message },
+      body: JSON.stringify({ jsonrpc: '2.0', id: null, error: { code: -32603, message: 'Internal error' } })
+    }
+  }
+}
+
+async function sendTx(callId, txType, queueName, palletName, method, params) {
+  try {
+    return await mqSender.sendMessageToMQ(queueName, { callId, txType, palletName, method, params })
+  } catch (err) {
+    throw err
+  }
+}
+
+async function processRequest(requestObject) {
+  let responseObject = { jsonrpc: '2.0' }
+  let call
+
+  try {
+    call = JSON.parse(requestObject)
+  } catch (err) {
+    utils.logError('failed to parse JSON', null, 'send-handler.processRequest.parse', err)
+    responseObject.error = { code: -32700, message: 'Parse error' }
+    responseObject.id = null
+    return responseObject
+  }
+
+  if (typeof call.method !== 'string') {
+    utils.logError('method type must be string', call.id, 'send-handler.processRequest.method', call.method)
+    responseObject.error = { code: -32600, message: 'Invalid Request' }
+  } else {
+    responseObject = await callSwitch(call, responseObject)
+  }
+
+  responseObject.id = call.id
+  return responseObject
+}
+
+async function callSwitch(call, responseObject) {
+  switch (call.method) {
+    case 'transferAvt':
+      if (utils.isValidAccountId(call.params[0]) && utils.isValidAmount(call.params[1])) {
+        try {
+          responseObject.result = await sendTx(
+            call.id,
+            'avnTx',
+            process.env.MQ_AVN_TX_QUEUE,
+            'balances',
+            'transfer', 
+            [
+              call.params[0],
+              call.params[1]
+            ]
+          )
+        } catch (err) {
+          utils.logError('failed to send transaction', call.id, 'send-handler.transferAvt.sendTx', err)
+          responseObject.error = { code: -32603, message: 'Internal error' }
+        }
+      } else {
+        utils.logError('invalid params', call.id, 'send-handler.transferAvt.params', call.params)
+        responseObject.error = { code: -32602, message: 'Invalid params' }
+      }
+      break
+
+    case 'proxy':
+      let pallet = call.params.pallet
+      let method = call.params.method
+
+      let formatter = codeFormatters[pallet][method]
+
+      if (!formatter) {
+        utils.logError('method not found', call.id, 'send-handler.proxy.method', call)
+        responseObject.error = { code: -32601, message: 'Method not found' }
+      } else if (!formatter.validate(call)) {
+        utils.logError('invalid params', call.id, 'send-handler.proxy.params', call.params)
+        responseObject.error = { code: -32602, message: 'Invalid params' }
+      } else {
+        try {
+          let proof = {
+            signer: call.params.innerArgs.from,
+            relayer: call.params.relayer,
+            signature: {
+              Sr25519: call.params.signature
+            }
+          }
+          responseObject.result = await sendTx(
+            call.id,
+            'avnProxy',
+            process.env.MQ_AVN_TX_QUEUE,
+            pallet,
+            method,
+            formatter.encode(proof, call.params.innerArgs)
+          )
+        } catch (err) {
+          utils.logError('failed to send proxy transaction', call.id, 'send-handler.proxy.sendProxyTx', err)
+          responseObject.error = { code: -32603, message: 'Internal error' }
+        }
+      }
+      break
+
+    default:
+      utils.logError('method not found', call.id, 'send-handler.callSwitch.default', method)
+      responseObject.error = { code: -32601, message: 'Method not found' }
+  }
+  return responseObject
+}
+
+const codeFormatters = {
+  balances: {
+    transfer: {
+      validate: function(params0, params1) {
+        return utils.isValidAccountId(params0) && utils.isValidAmount(params1)
+      },
+      encode: function(params0, params1) {
+        return [params0, params1]
+      }
+    }
+  },
+  tokenManager: {
+    signedTransfer: {
+      validate: function(call) {
+        return (
+          utils.isValidAccountId(call.params.relayer) &&
+          utils.isValidAccountId(call.params.innerArgs.from) &&
+          utils.isValidAccountId(call.params.innerArgs.to) &&
+          utils.isValidTokenId(call.params.innerArgs.token) &&
+          utils.isValidAmount(call.params.innerArgs.amount.toString())
+        )
+      },
+      encode: function(proof, innerArgs) {
+        return [proof, innerArgs.from, innerArgs.to, innerArgs.token, innerArgs.amount]
+      }
+    }
+  }
+}
+
+// async function testlocal(n) {
+//   await connectToMQ()
+//   for (var i = 0; i < n; i++) {
+//     console.info('transferAvt:', await processRequest(`{"jsonrpc": "2.0", "method":"transferAvt", "params":["5DAgxVxKmnJ7hfhDEB9UetZm4jR2MPjGZGrmJZjirSVJDdMr", "2"], "id":${i}}`))
+//     await sleep(1000)
+//   }
+// }
+
+// function sleep(ms) {
+//   return new Promise((resolve, reject) => setTimeout(resolve, ms) )
+// }
+
+// testlocal(1)
---
+++ lambdas/update-lambda.js
@@ -0,0 +1,125 @@
+#!/usr/bin/env node
+
+const {
+  LambdaClient,
+  GetFunctionCommand,
+  ListLayersCommand,
+  PublishLayerVersionCommand,
+  UpdateFunctionCodeCommand,
+  UpdateFunctionConfigurationCommand
+} = require('@aws-sdk/client-lambda')
+const aws = new LambdaClient({ region: 'eu-west-1' })
+const fs = require('fs')
+const join = require('path').join
+const { spawn } = require('child-process-async')
+const os = require('os')
+const zipdir = require('zip-dir')
+
+let layers
+
+// TODO:
+// Update lambda test scripts
+
+async function main() {
+  const lambda = process.argv[2]
+  if (!lambda) {
+    console.log('lambda name is required')
+    process.exit(1)
+  }
+
+  const paths = {
+    lambda: join(__dirname, lambda),
+    lambdaIdx: join(__dirname, lambda, 'index.js'),
+    lambdaPkg: join(__dirname, lambda, 'package.json'),
+    layerPkg: join(__dirname, 'layer/nodejs/package.json')
+  }
+
+  removeDependenciesAlreadyInLayer(paths)
+  await installNpmModules(lambda, paths.lambda)
+  const usesLambdaLayer = await updateDependenciesReferences(paths.lambdaIdx)
+  if (usesLambdaLayer) {
+    layers = await getLambdaLayer(lambda, 'common')
+  }
+  await publishSourceCode(lambda, layers)
+}
+
+async function getLambdaLayer(lambda, layerName) {
+  const functionDetails = await aws.send(new GetFunctionCommand({ FunctionName: lambda }))
+  if (!functionDetails.Configuration?.Layers)
+  {
+    layer = await aws.send(new ListLayersCommand({ LayerName: layerName }))
+    if (layer) {
+      return [layer.Layers[0].LatestMatchingVersion.LayerVersionArn]
+    } else {
+      return [await createLambdaLayer(layerName)]
+    }
+  }
+}
+ 
+async function createLambdaLayer(layerName) {
+  // TODO: install layer modules 
+  // TODO: Create a zip file for the layer  
+  await aws.send(new PublishLayerVersionCommand({
+    LayerName: layerName,
+    Content: {
+      ZipFile: base64Encode(zipFile)
+    }
+  }))
+}
+
+async function publishSourceCode(lambda, layers) {
+  console.log('Publishing', lambda, 'to AWS...')
+  const params = { ZipFile: await zipdir(lambda), FunctionName: lambda }
+  const config = { FunctionName: lambda, Description: `${lambda} - Update script deployment`, Layers: layers }
+
+  try {
+    await aws.send(new UpdateFunctionConfigurationCommand(config))
+    await aws.send(new UpdateFunctionCodeCommand(params))
+    console.log('Published ', lambda)
+  } catch (err) {
+    console.log(lambda, '- Error:', err)
+  }
+}
+
+async function updateDependenciesReferences(lambdaIdxPath) {
+  return await new Promise((resolve, reject) => {
+    fs.readFile(lambdaIdxPath, 'utf8', function (err, fileBody) {
+      if (err) {
+        console.log(err)
+        reject(err)
+      }
+      var updatedFileBody = fileBody.replace(/..\/layer\//gs, '/opt/nodejs/');
+  
+      fs.writeFile(lambdaIdxPath, updatedFileBody, 'utf8', function (err) {
+        if (err) {
+          console.log(err)
+          reject(err)
+        }
+        resolve(updatedFileBody.includes('\/opt\/nodejs\/'))
+      })
+    })
+  })
+}
+
+async function installNpmModules(lambda, lambdaPath) {
+   const npmCmd = os.platform().startsWith('win') ? 'npm.cmd' : 'npm'
+   const child = spawn(npmCmd, ['i'], { env: process.env, cwd: lambdaPath, stdio: 'ignore' })
+   await child.on('exit', () => {
+     console.log('Node modules for', lambda, 'updated')
+   })
+}
+
+function removeDependenciesAlreadyInLayer(paths) {
+  const lambdaPkg = require(paths.lambdaPkg)
+  const layerPkg = require(paths.layerPkg)
+  
+  Object.entries(layerPkg.dependencies).forEach(
+    ([module, _version] = dependency) => {
+      if (lambdaPkg.dependencies[module])
+        delete lambdaPkg.dependencies[module]
+    }
+  )
+  fs.writeFileSync(paths.lambdaPkg, JSON.stringify(lambdaPkg, null, 2))
+}
+
+if (require.main === module) main()

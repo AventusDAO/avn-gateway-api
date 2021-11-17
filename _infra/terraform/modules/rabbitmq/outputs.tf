5aeed08b15b3d7860a20688aa492e5b6335fad10
sleepert <john.terry@aventus.io>
2021-11-17 17:17:40 +0000

    undo uneeded change to rabbit_security_group output

--- _infra/terraform/modules/rabbitmq/outputs.tf
+++ _infra/terraform/modules/rabbitmq/outputs.tf
@@ -1,5 +1,5 @@
 output "rabbit_security_group" {
-  value = var.publicly_accessible ? null : [aws_security_group.rabbit["private"].id]
+  value = var.publicly_accessible ? null : aws_security_group.rabbit["private"].id
 }
 
 output "secret_arn" {

3cba67eba15c4f1e9d355482362ea86122885f3f
Steven Gong <54836231+steven-gong-aventus@users.noreply.github.com>
2021-11-19 09:39:18 +0000

    SYS-1529 Merged update scripts into one script (#109)
    * Merged all update-*.js into 1 update.js file

--- lambdas/update.js
+++ lambdas/update.js
@@ -1,41 +1,81 @@
 /*
   Description:
-    This script publishes any changes made within the lambda function folder to the corresponding AWS lambda function.
+    This script can update a lambda layer, a lambda function on AWS, and a lambda function to support local test
+    And it can be executed as:
+    
+    `node update.js [target] [lambda_function]`
 
-    It contains the following steps:
+    target: layer
+      publish a new version of a lambda layer on AWS with the following steps:
+      1. Install all dependencies declared in layer/nodejs/package.json file
+      2. Create a new version of the lambda layer in AWS with a zip file contains all files within the lambdas/nodejs/ folder
+
+    target: lambda [all | lambda_function]
+      publish all lambda functions or a single lambda function with the following steps:
       1. Clean up node modules in a lambda function, remove any dependencies already defined in the layer folder.
       2. Install all lambda function dependencies defined in the package.json file within lambda function folder
       3. Compress all files within the lambda function folder into a zip file and publish it to the AWS lambda function
       4. Attach or update the lambda layer to the latest version if it is required by the lambda function files.
 
-    Note:
-      * If there are changes also made in the layer folder, please run update-layer.js first
-      * In order to use the testlocal function at the end of each lambda function's index.js file, please run update-local.js before executing the local test
+    target: local [all | lambda_function]
+      merges layer dependencies into all lambda function or a single lambda function's dependencies list, 
+      and updates all layer file URIs from /opt/nodejs/ to ../layer/nodejs/ in lambda function files if there is any
 */
 
 const {
   LambdaClient,
-  GetFunctionCommand,
   ListLayersCommand,
+  PublishLayerVersionCommand,
+  UpdateFunctionConfigurationCommand,
   UpdateFunctionCodeCommand
 } = require('@aws-sdk/client-lambda')
 const aws = new LambdaClient({ region: 'eu-west-1' })
 const fs = require('fs')
 const { join } = require('path')
 const zipdir = require('zip-dir')
-const { LAYER_NAME, createLambdaLayer, publishLambdaLayer } = require('./update-layer.js')
-const { installPkgDependencies, getAllFilesPaths } = require('./utils.js')
+const { replaceRef, mergePkgDependencies, installPkgDependencies, getAllFilesPaths } = require('./utils.js')
 
+const TARGETS = ['layer', 'lambda', 'local']
+const LAYER_NAME = 'common-layer'
 const LAMBDAS = ['poll-handler', 'query-handler', 'send-handler', 'authorisation-handler', 'tx-status-update-handler']
 
 async function main() {
-  const lambda = process.argv[2]
+  const target = process.argv[2]
+  const lambda = process.argv[3]
   const lambdas = lambda === 'all' ? LAMBDAS : [lambda]
-  await Promise.allSettled(
-    lambdas.map(lambda => {
-      updateNodeModulesAndPublish(lambda)
-    })
-  )
+
+  switch (target) {
+    case 'layer':
+      try {
+        const paths = {
+          layerPath: join(__dirname, 'layer'),
+          layerNodejsPath: join(__dirname, 'layer/nodejs')
+        }
+        await installPkgDependencies(LAYER_NAME, paths.layerNodejsPath)
+        await createLambdaLayer(LAYER_NAME, paths.layerPath)
+      } catch (err) {
+        console.log('Updating lambda layer failed', err.message)
+      }
+      break
+    case 'lambda':
+      await Promise.allSettled(
+        lambdas.map(lambda => {
+          LAMBDAS.includes(lambda)
+            ? updateNodeModulesAndPublish(lambda)
+            : console.log('Error: no such lambda %s', lambda)
+        })
+      )
+      break
+    case 'local':
+      await Promise.allSettled(
+        lambdas.map(lambda => {
+          LAMBDAS.includes(lambda) ? updateNodeModulesAndFiles(lambda) : console.log('Error: no such lambda %s', lambda)
+        })
+      )
+      break
+    default:
+      console.log('target %s is not supported, please choose between %s', target, TARGETS.join(', '))
+  }
 }
 
 async function updateNodeModulesAndPublish(lambda) {
@@ -126,8 +166,43 @@ async function publishSourceCode(lambda) {
   }
 }
 
-if (require.main === module) main()
-
-module.exports = {
-  LAMBDAS
+async function publishLambdaLayer(lambda, description, layers) {
+  try {
+    await aws.send(
+      new UpdateFunctionConfigurationCommand({
+        FunctionName: lambda,
+        Description: description,
+        Layers: layers
+      })
+    )
+  } catch (err) {
+    console.log(lambda, '- Error:', err)
+  }
 }
+
+async function createLambdaLayer(layerName, layerPath) {
+  const response = await aws.send(
+    new PublishLayerVersionCommand({
+      LayerName: layerName,
+      Content: { ZipFile: await zipdir(layerPath) }
+    })
+  )
+  console.log(`Lambda layer ${layerName} updated to version ${response.Version}`)
+  return response
+}
+
+async function updateNodeModulesAndFiles(lambda) {
+  const layerPkg = join(__dirname, 'layer/nodejs/package.json')
+  const lambdaPkg = join(__dirname, lambda, 'package.json')
+  mergePkgDependencies(layerPkg, lambdaPkg)
+
+  const lambdaPath = join(__dirname, lambda)
+  await installPkgDependencies(lambda, lambdaPath)
+
+  const lambdaFilesPaths = getAllFilesPaths(lambdaPath)
+  lambdaFilesPaths.forEach(filePath => {
+    replaceRef(filePath, '/opt/nodejs', '../layer/nodejs')
+  })
+}
+
+if (require.main === module) main()

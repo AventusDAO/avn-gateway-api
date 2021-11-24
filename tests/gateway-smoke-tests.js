4979eeabb93379a6f859d828a60c7cf773eef534
Steven Gong <steven.gong@artos.io>
2021-11-24 14:36:20 +0000

    Added smoke tests for AVN Gateway

---
+++ tests/gateway-smoke-tests.js
@@ -0,0 +1,33 @@
+const assert = require('chai').assert
+const helper = require('../avn-api/tests/helper')
+const accounts = helper.ACCOUNTS
+const BN = helper.BN
+
+const waitForTxToBeMined = async () => await helper.sleep(5000)
+
+describe('AVN Gateway Smoke Tests', function() {
+  let api, relayer, sender, recipient, requestId
+
+  before(async () => {
+    api = await helper.avnApi()
+    relayer = accounts.relayer.address
+    sender = accounts.sender.address
+    recipient = accounts.user1.address
+  })
+
+  it('query account balance', async () => {
+    const senderBalance = await api.query.getAvtBalance(sender)
+    assert(senderBalance)
+  })
+
+  it('transfer AVT', async () => {
+    const amount = new BN('1')
+    requestId = await api.send.transferAvt(relayer, sender, recipient, amount)
+    await waitForTxToBeMined()
+    assert(requestId)
+  })
+
+  it('poll request state', async () => {
+    assert.equal(await api.poll.requestState(requestId), 'Pending')
+  })
+})

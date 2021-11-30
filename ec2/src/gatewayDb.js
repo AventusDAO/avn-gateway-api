bc84c3caba345db14d3197e22040148f614a8ab7
Nahu Seyoum <nahu.seyoum@artos.io>
2021-11-30 10:21:11 +0000

    implement logic

--- ec2/src/gatewayDb.js
+++ ec2/src/gatewayDb.js
@@ -93,31 +93,42 @@ async function createUserFeesCollectionIfRequired(db) {
   }
 }
 
-
 async function collectionExists(db, collectionName) {
   return (await db.listCollections().toArray()).some(col => col.name.toLowerCase() === collectionName.toLowerCase())
 }
 
 // transactionType and senderAddress are optional
-async function getFees(relayerAddress, userAddress, transactionType) {
+async function getFees(relayerAddress, userAddress) {
   if (!relayerAddress) {
     throw new Error(`Relayer address is a mandatory field`)
   }
 
-  //*** This will be implemented in the next PR ***/
+  const feesCursor = await db.collection(FEES_COLLECTION_NAME).find({ "relayer": relayerAddress }).limit(1);
 
-  // const feesCursor = await db.collection(FEES_COLLECTION_NAME).find({ "relayer": relayerAddress }).limit(1);
+  if (await feesCursor.hasNext()) {
+    const relayerFees =  await feesCursor.next()
+    const userFees = await getUserFeesIfAny(relayerAddress, userAddress)
+    // Apply additional filtering based on transactionType and senderAddress
 
-  // if (await feesCursor.hasNext()) {
-  //   const fees =  await feesCursor.next();
-  //   // Apply additional filtering based on transactionType and senderAddress
-
-  //   return
-  // }
-
-  // return undefined;
+    return { ...defaultFees, ...relayerFees.fees, ...userFees.fees}
+  }
 
   return defaultFees
+}
+
+async function getUserFeesIfAny(relayerAddress, userAddress) {
+  if (!relayerAddress || !userAddress) {
+    log.trace(`Relayer address or User address is missing. RelayerAddress: ${relayerAddress}, UserAddress: ${userAddress}`)
+    return undefined
+  }
+
+  const userFeesCursor = await db.collection(FEES_COLLECTION_NAME).find({ "relayer": relayerAddress, "user": userAddress }).limit(1);
+
+  if (await userFeesCursor.hasNext()) {
+    return (await feesCursor.next()).fees;
+  }
+
+  return undefined
 }
 
 module.exports = {

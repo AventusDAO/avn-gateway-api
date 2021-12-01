a01e1ccf1e96736e1936126ff8e87e26f6c0348f
sleepert <john.terry@aventus.io>
2021-12-01 10:52:49 +0000

    SYS-1662 apply documentdb module

---
+++ _infra/terraform/modules/documentdb/variables.tf
@@ -0,0 +1,50 @@
+variable "backup_retention_period" {
+  description = "Retention period for backups"
+  type        = number
+  default     = 7
+}
+
+variable "apply_changes_immediately" {
+  description = "Apply changes immediately or during the maintenance window"
+  type        = bool
+  default     = true
+}
+
+variable "deletion_protection" {
+  description = "If enabled the cluster cannot be accidently destroyed"
+  type        = bool
+  default     = false
+}
+
+variable "secret_recovery_window" {
+  description = "Recovery window for the secret. A value of 0 means the secret can be deleted instantly"
+  type        = number
+  default     = 0
+}
+
+variable "username" {
+  description = "documentdb master username"
+  type        = string
+  default     = "AvnGateway"
+}
+
+variable "subnet_ids" {
+  description = "subnet id to deploy the cluster"
+  type        = list(string)
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "cluster_instance_count" {
+  description = "number of instances"
+  type        = number
+  default     = 1
+}
+
+variable "instance_type" {
+  description = "Cluster instance type"
+  type        = string
+  default     = "db.t4g.medium"
+}

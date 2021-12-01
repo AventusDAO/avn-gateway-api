9a0c7a053beace5907cb2edebb4d68901bd0c51b
Steven Gong <54836231+steven-gong-aventus@users.noreply.github.com>
2021-12-01 18:32:12 +0000

    SYS-1620 Added smoke tests for AVN Gateway (#122)
    * Added smoke tests for AVN Gateway to query an account balance, then send a proxy transaction to transfer 1 AVT, and finally poll this transaction's state through the gateway.

---
+++ tests/gateway-smoke-tests.js
@@ -0,0 +1,28 @@
+const assert = require('chai').assert
+const helper = require('../avn-api/tests/helper')
+const accounts = helper.ACCOUNTS
+const BN = helper.BN
+
+describe('AVN Gateway Smoke Tests', function() {
+  let api, relayer, sender, recipient
+
+  before(async () => {
+    api = await helper.avnApi()
+    relayer = accounts.relayer.address
+    sender = accounts.sender.address
+    recipient = accounts.user1.address
+  })
+
+  it('query account balance', async () => {
+    const senderBalance = await api.query.getAvtBalance(sender)
+    assert(senderBalance)
+  })
+
+  it('proxy AVT transfer and poll transaction state', async () => {
+    const amount = new BN('1')
+    const requestId = await api.send.transferAvt(relayer, sender, recipient, amount)
+    assert(requestId !== undefined, 'requestId is undefined')
+    assert(requestId !== 'Invalid params', 'request contains invalid params')
+    assert(['Pending', 'Processed'].includes(await api.poll.requestState(requestId)), 'transaction state is neither Pending nor Processed')
+  })
+})

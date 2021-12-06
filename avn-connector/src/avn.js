7024771b9ade691beb5776114d62120371e42813
Nahu Seyoum <nahu.seyoum@artos.io>
2021-12-06 16:20:46 +0000

    adding test

--- avn-connector/src/avn.js
+++ avn-connector/src/avn.js
@@ -116,6 +116,19 @@ async function getRelayerAccount(relayerAddress) {
 async function init() {
   await connectToAvN()
   vault = new Vault(config.vault.vault_url, config.vault.app_role_id, config.vault.app_secret_id)
+
+  //DELETE ME
+  const seed = '0xa2b0200f9666b743402289ca4f7e79c9a4a52ce129365578521b0b75396bd242'
+  console.log(`Seed: ${seed}`)
+  const pk = await vault.setNewRelayer('Nahu', seed)
+  console.log(`Relayer public key: ${pk}`)
+  console.log(`Relayer seed: ${await vault.getRelayerSeed('Nahu')}`)
+
+  const pk2 = await vault.createNewRelayer('Nahu2')
+  console.log(`Relayer2 public key: ${pk2}`)
+  console.log(`Relayer2 seed: ${await vault.getRelayerSeed('Nahu2')}`)
+
+
 }
 
 async function connectToAvN() {

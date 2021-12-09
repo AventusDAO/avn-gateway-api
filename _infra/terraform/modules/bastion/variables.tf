a6d3664f8ec0d69a78712ae7054aa931b3842dd8
Thanos Doukoudakis <thanos.doukoudakis@artos.io>
2021-12-09 16:59:26 +0000

    SYS-1624 Create bastion module
    Splits the bastion code on a seperate module to make it more modular.
    Improves readability and reduces code duplication.

---
+++ _infra/terraform/modules/bastion/variables.tf
@@ -0,0 +1,23 @@
+variable "ssh_allowed_ips" {
+  type        = list(string)
+  description = "Allowed ips for incoming ssh connections"
+  default     = [
+    "18.135.167.38/32",     # IP of the nat of Testnet (Jenkins)
+  ]
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "The ID of the VPC"
+}
+
+variable "public_subnet_id" {
+  type        = string
+  description = "The main public subnet ID"
+}
+
+variable "ssh_key_name" {
+  type        = string
+  description = "The ssh key used to create the instance & allow ssh access"
+}
+

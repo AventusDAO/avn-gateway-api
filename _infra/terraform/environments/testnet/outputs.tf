032fff36b1e7f8c9c5c6ad58344e5f14b501ff05
sleepert <john.terry@aventus.io>
2021-12-13 12:15:46 +0000

    takeout the testnet config

--- _infra/terraform/environments/testnet/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "avn_connector_ips" {
-  value = join(",", module.dns.avn_connector_ips)
-}
-
-output "main_subnet" {
-  value = module.vpc.private_subnets[0]
-}
--- _infra/terraform/environments/testnet/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "region" {
-  default = "eu-west-1"
-}
-
-variable "service_version" {
-  type        = string
-  description = "Version of the lambda functions, will usually be a github short sha or pr number"
-  default     = "latest"
-}

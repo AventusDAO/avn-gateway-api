14ab2f26c64ff5c2de8fd10eaf9d4b75016cedd8
sleepert <john.terry@aventus.io>
2021-12-13 11:17:42 +0000

    SYS-1621 apply testnet configuration

---
+++ _infra/terraform/environments/testnet/variables.tf
@@ -0,0 +1,9 @@
+variable "region" {
+  default = "eu-west-1"
+}
+
+variable "service_version" {
+  type        = string
+  description = "Version of the lambda functions, will usually be a github short sha or pr number"
+  default     = "latest"
+}

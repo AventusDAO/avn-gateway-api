dd5fcfe0eb8cefa894c7f4ee698fb52e5d2b5259
sleepert <john.terry@aventus.io>
2021-12-13 12:05:26 +0000

    Update _infra/terraform/environments/testnet/vault.tf
    Co-authored-by: Thanos Doukoudakis <56822898+thadouk@users.noreply.github.com>

--- _infra/terraform/environments/testnet/vault.tf
+++ _infra/terraform/environments/testnet/vault.tf
@@ -43,7 +43,7 @@ ${module.bastion.public_ip} ansible_user=ubuntu
 [vault_server]
 ${module.avn-vault.instance_ip_addr} ansible_user=ubuntu
 [vault_server:vars]
-ansible_ssh_common_args='-o ProxyCommand="ssh -W %h:%p -q ubuntu@${module.bastion.public_ip}"'
+ansible_ssh_common_args='-o ProxyCommand="ssh -o StrictHostKeyChecking=no -W %h:%p -q ubuntu@${module.bastion.public_ip}"'
 api_addr_value='https://${module.avn-vault.fqdn}:8200'
 aws_region='${module.avn-vault.aws_region}'
 kms_key_id='${module.avn-vault.kms_key_id}'

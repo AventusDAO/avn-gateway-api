4b0b73fcbc40ea0a94337fa640771f14a7dd86c5
sleepert <john.terry@aventus.io>
2021-12-14 14:21:56 +0000

    SYS-1699 deploy ingress controller (#182)
    * SYS-1699 Deploy Nginx ingress controller
    
    * SYS-1699 disable external loadbalancing and fix ingress

--- _infra/terraform/environments/cba/main.tf
+++ _infra/terraform/environments/cba/main.tf
@@ -4,7 +4,7 @@ locals {
   environment            = "cba"
   cluster_version        = "1.21"
   account_id             = "602004642405"
-  avn_connector_endpoint = "http://avn-connector.${local.environment}.aventus.internal:8080/"
+  avn_connector_endpoint = "http://avn-connector.${local.environment}.aventus.internal/"
   block_explorer_url     = "https://avn.cba-stargate.aventus.io:3000"
   vault_recovery_window  = 0
 }

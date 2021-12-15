f0bce26b5de3b3d49d053a0011295c92164cf2b9
sleepert <john.terry@aventus.io>
2021-12-15 11:57:52 +0000

    SYS-1621 apply testnet configuration (#177)
    * SYS-1621 apply testnet configuration
    
    * Update _infra/terraform/environments/testnet/vault.tf
    
    Co-authored-by: Thanos Doukoudakis <56822898+thadouk@users.noreply.github.com>
    
    * Update _infra/terraform/environments/testnet/vault.tf
    
    Co-authored-by: Thanos Doukoudakis <56822898+thadouk@users.noreply.github.com>
    
    * add variable for node size
    
    * SYS-1621 change testnet bucket path as suggested
    
    * add testnet .env
    
    * Change websocket address as suggested
    
    Co-authored-by: Thanos Doukoudakis <56822898+thadouk@users.noreply.github.com>

---
+++ _infra/terraform/environments/testnet/outputs.tf
@@ -0,0 +1,25 @@
+output "avn_connector_ips" {
+  value = join(",", module.dns.avn_connector_ips)
+}
+
+output "main_subnet" {
+  value = join(",", module.vpc.private_subnets)
+}
+
+output "redis_password" {
+  sensitive = true
+  value = module.redis.password
+}
+
+output "redis_username" {
+  sensitive = true
+  value = module.redis.username
+}
+
+output "redis_security_group_id" {
+  value = module.redis.security_group_id
+}
+
+output "private_hosted_zone_id" {
+  value = module.dns.private_hosted_zone_id
+}

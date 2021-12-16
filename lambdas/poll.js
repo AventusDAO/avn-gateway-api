6ddcf9b51eecf79c4b504e4e418ce36a3d02f761
fluorostani <glyn.dimond@artos.io>
2021-12-16 00:12:38 +0000

    SYS-1675 Add lambdas back in serverless form

---
+++ lambdas/poll.js
@@ -0,0 +1,64 @@
+const utils = require('/opt/utils.js')
+
+const AVN_CONNECTOR_ENDPOINT = process.env.AVN_CONNECTOR_ENDPOINT
+
+exports.handler = async event => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(await processRequest(event.body))
+  }
+  return response
+}
+
+async function poll(callId, requestId) {
+  let response
+  try {
+    response = await utils.axios.post(AVN_CONNECTOR_ENDPOINT + 'avnPoll', { callId, requestId })
+  } catch (err) {
+    throw err
+  }
+  return response.data.error || response.data.status
+}
+
+async function processRequest(requestObject) {
+  let responseObject = { jsonrpc: '2.0' }
+  let call
+
+  try {
+    call = JSON.parse(requestObject)
+  } catch (err) {
+    utils.logError('failed to parse JSON', null, 'poll-handler.processRequest.parse', err)
+    responseObject.error = { code: -32700, message: 'Parse error' }
+    responseObject.id = null
+    return responseObject
+  }
+
+  if (typeof call.method !== 'string') {
+    utils.logError('method type must be string', call.id, 'poll-handler.processRequest.method', call.method)
+    responseObject.error = { code: -32600, message: 'Invalid Request' }
+  } else {
+    responseObject = await makeCall(call, responseObject)
+  }
+
+  responseObject.id = call.id
+  return responseObject
+}
+
+async function makeCall(call, responseObject) {
+  if (call.method !== 'requestState') {
+    utils.logError("method must be 'requestState'", call.id, 'poll-handler.makeCall.method', call.method)
+    responseObject.error = { code: -32601, message: 'Method not found' }
+  } else if (utils.isValidUUID(call.params[0])) {
+    try {
+      responseObject.result = await poll(call.id, call.params[0])
+    } catch (err) {
+      utils.logError('failed to poll chain', call.id, 'poll-handler.poll', err)
+      responseObject.error = { code: -32603, message: 'Internal error' }
+    }
+  } else {
+    utils.logError('invalid request ID', call.id, 'poll-handler.makeCall.requestId', call.params[0])
+    responseObject.error = { code: -32602, message: 'Invalid params' }
+  }
+
+  return responseObject
+}

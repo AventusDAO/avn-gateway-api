fd063eed7cf500f4017e860f82e4a05d39439aa6
sleepert <john.terry@aventus.io>
2021-12-20 11:01:41 +0000

    SYS-1622 add mainnet configuration (#185)
    * add mainnet configuration
    
    * SYS-1622 change accountif to mainnet from testnet
    
    * SYS-1622 apply recent changes to mainnet config
    
    * change to mainnet
    
    * change vault recovery window to 30 days

---
+++ _infra/terraform/environments/mainnet/provider.tf
@@ -0,0 +1,35 @@
+terraform {
+  backend "s3" {
+    encrypt                = true
+    bucket                 = "tf-state-avn-1"
+    key                    = "env:/production/mainnet/avn-gateway/terraform.tfstate"
+    region                 = "eu-west-2"
+    skip_region_validation = "true"
+  }
+
+  required_version = ">= 0.14"
+
+  required_providers {
+    aws = {
+      version = ">= 3.6.2"
+      source  = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+  
+  assume_role {
+    role_arn = "arn:aws:iam::503742778456:role/jenkins-access"
+  }
+}
+
+provider "aws" {
+  alias  = "aventus"
+  region = var.region
+
+  assume_role {
+    role_arn = "arn:aws:iam::707061609910:role/jenkins-access"
+  }
+}

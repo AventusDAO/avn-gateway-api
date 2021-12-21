53ecc1b4d0b0f35b9c330ef63bf249886bec9b04
fluorostani <39763263+fluorostani@users.noreply.github.com>
2021-12-21 09:43:25 +0000

    SYS-1686 & SYS-1670 Refactor query handler and improve errors (#213)

--- avn-gateway/query-handler.js
+++ avn-gateway/query-handler.js
@@ -10,14 +10,14 @@ exports.handler = async event => {
   return response
 }
 
-async function queryChain(callId, palletName, storageName, params, responseFormatter) {
-  let response
+async function queryChain(responseObject, callId, palletName, storageName, params, responseFormatter) {
   try {
-    response = await utils.axios.post(AVN_CONNECTOR_ENDPOINT + 'avnQuery', { callId, palletName, storageName, params })
+    const response = await utils.axios.post(AVN_CONNECTOR_ENDPOINT + 'avnQuery', { callId, palletName, storageName, params })
+    responseObject.result = response.data.error || responseFormatter(response.data, params)
   } catch (err) {
-    throw err
+    utils.logError('failed to query chain', callId, err)
+    responseObject.error = { code: -32603, message: 'Internal error' }
   }
-  return response.data.error || responseFormatter(response.data, params)
 }
 
 async function processRequest(requestObject) {
@@ -37,7 +37,7 @@ async function processRequest(requestObject) {
     utils.logError('method type must be string', call.id, call.method)
     responseObject.error = { code: -32600, message: 'Invalid Request' }
   } else {
-    responseObject = await callSwitch(call, responseObject)
+    await callSwitch(call, responseObject)
   }
 
   responseObject.id = call.id
@@ -47,129 +47,146 @@ async function processRequest(requestObject) {
 async function callSwitch(call, responseObject) {
   switch (call.method) {
     case 'getTotalAvt':
-      try {
-        responseObject.result = await queryChain(call.id, 'balances', 'totalIssuance', [], formatNumberAsString)
-      } catch (err) {
-        utils.logError('failed to query chain', call.id, err)
-        responseObject.error = { code: -32603, message: 'Internal error' }
-      }
+      await getTotalAvt(call, responseObject)
       break
     case 'getAvtBalance':
-      if (utils.isValidAccountId(call.params[0])) {
-        try {
-          responseObject.result = await queryChain(call.id, 'system', 'account', [call.params[0]], formatBalanceAsString)
-        } catch (err) {
-          utils.logError('failed to query chain', call.id, err)
-          responseObject.error = { code: -32603, message: 'Internal error' }
-        }
-      } else {
-        utils.logError('invalid account ID', call.id, call.params[0])
-        responseObject.error = { code: -32602, message: 'Invalid params' }
-      }
+      await getAvtBalance(call, responseObject)
       break
     case 'getTokenBalance':
-      if (utils.isValidAccountId(call.params[0]) && utils.isValidEthereumAddress(call.params[1])) {
-        try {
-          responseObject.result = await queryChain(
-            call.id,
-            'tokenManager',
-            'balances',
-            [[call.params[1], call.params[0]]],
-            formatNumberAsString
-          )
-        } catch (err) {
-          utils.logError('failed to query chain', call.id, err)
-          responseObject.error = { code: -32603, message: 'Internal error' }
-        }
-      } else {
-        utils.logError('invalid params', call.id, call.params)
-        responseObject.error = { code: -32602, message: 'Invalid params' }
-      }
+      await getTokenBalance(call, responseObject)
       break
     case 'getAccountNonce':
-      if (utils.isValidAccountId(call.params[0])) {
-        try {
-          responseObject.result = await queryChain(call.id, 'tokenManager', 'nonces', [call.params[0]], formatNumberAsString)
-        } catch (err) {
-          utils.logError('failed to query chain', call.id, err)
-          responseObject.error = { code: -32603, message: 'Internal error' }
-        }
-      } else {
-        utils.logError('invalid account ID', call.id, call.params[0])
-        responseObject.error = { code: -32602, message: 'Invalid params' }
-      }
+      await getAccountNonce(call, responseObject)
       break
     case 'getNftNonce':
-      if (utils.isValidNftId(call.params[0])) {
-        try {
-          responseObject.result = await queryChain(call.id, 'nftManager', 'nfts', [call.params[0]], formatNftNonceAsString)
-        } catch (err) {
-          utils.logError('failed to query chain', call.id, err)
-          responseObject.error = { code: -32603, message: 'Internal error' }
-        }
-      } else {
-        utils.logError('invalid nft ID', call.id, call.params[0])
-        responseObject.error = { code: -32602, message: 'Invalid params' }
-      }
+      await getNftNonce(call, responseObject)
       break
     case 'getNftId':
-      if (utils.isValidString(call.params[0])) {
-        try {
-          responseObject.result = await queryChain(call.id, 'nftManager', 'nfts', ['entries', call.params[0]], filterNftId)
-        } catch (err) {
-          utils.logError('failed to query chain', call.id, err)
-          responseObject.error = { code: -32603, message: 'Internal error' }
-        }
-      } else {
-        utils.logError('invalid external ref', call.id, call.params[0])
-        responseObject.error = { code: -32602, message: 'Invalid params' }
-      }
+      await getNftId(call, responseObject)
       break
     case 'getAccountPaymentNonce':
-      if (utils.isValidAccountId(call.params[0])) {
-        try {
-          responseObject.result = await queryChain(call.id, 'avnProxy', 'paymentNonces', [call.params[0]], formatNumberAsString)
-        } catch (err) {
-          utils.logError('failed to query chain', call.id, err)
-          responseObject.error = { code: -32603, message: 'Internal error' }
-        }
-      } else {
-        utils.logError('invalid account ID', call.id, call.params[0])
-        responseObject.error = { code: -32602, message: 'Invalid params' }
-      }
+      await getAccountPaymentNonce(call, responseObject)
       break
     case 'getAvtContractAddress':
-      try {
-        responseObject.result = await queryChain(call.id, 'tokenManager', 'aVTTokenContract', [], res => res.toString())
-      } catch (err) {
-        utils.logError('failed to query chain', call.id, err)
-        responseObject.error = { code: -32603, message: 'Internal error' }
-      }
+      await getAvtContractAddress(call, responseObject)
       break
     case 'getRelayerFees':
-      try {
-        const relayer = call.params[0]
-        const user = call.params[1]
-        const transactionType = call.params[2]
-        const response = await utils.axios.post(AVN_CONNECTOR_ENDPOINT + 'relayerFees', {
-          relayer,
-          user,
-          transactionType
-        })
-        responseObject.result = response.data
-      } catch (err) {
-        utils.logError('failed to call avn-connector', call.id, err)
-        responseObject.error = { code: -32603, message: 'Internal error' }
-      }
+      await getRelayerFees(call, responseObject)
       break
     default:
       utils.logError('method not found', call.id, call.method)
       responseObject.error = { code: -32601, message: 'Method not found' }
   }
-  return responseObject
 }
 
-const formatNumberAsString = data => utils.toBnString(data)
+async function getTotalAvt(call, responseObject) {
+  await queryChain(responseObject, call.id, 'balances', 'totalIssuance', [], formatNumAsString)
+}
+
+async function getAvtBalance(call, responseObject) {
+  const accountId = call.params[0]
+
+  if (utils.isValidAccountId(accountId) === false) {
+    utils.logError('invalid account ID', call.id, accountId)
+    responseObject.error = { code: -32602, message: 'Invalid params' }
+  } else {
+    await queryChain(responseObject, call.id, 'system', 'account', [accountId], formatBalanceAsString)
+  }
+}
+
+async function getTokenBalance(call, responseObject) {
+  const accountId = call.params[0]
+  const token = call.params[1]
+
+  try {
+    if (utils.isValidAccountId(accountId) === false) throw 'account ID'
+    if (utils.isValidEthereumAddress(token) === false) throw 'token'
+  } catch (param) {
+    const errorMsg = 'invalid ' + param
+    utils.logError(errorMsg, call.id, call.params)
+    responseObject.error = { code: -32602, message: 'Invalid params' }
+    return
+  }
+
+  await queryChain(responseObject, call.id, 'tokenManager', 'balances', [[token, accountId]], formatNumAsString)
+}
+
+async function getAccountNonce(call, responseObject) {
+  const accountId = call.params[0]
+
+  if (utils.isValidAccountId(accountId) === false) {
+    utils.logError('invalid account ID', call.id, accountId)
+    responseObject.error = { code: -32602, message: 'Invalid params' }
+  } else {
+    await queryChain(responseObject, call.id, 'tokenManager', 'nonces', [accountId], formatNumAsString)
+  }
+}
+
+async function getNftNonce(call, responseObject) {
+  const nftId = call.params[0]
+
+  if (utils.isValidNftId(nftId) === false) {
+    utils.logError('invalid nft ID', call.id, nftId)
+    responseObject.error = { code: -32602, message: 'Invalid params' }
+  } else {
+    await queryChain(responseObject, call.id, 'nftManager', 'nfts', [nftId], formatNftNonceAsString)
+  }
+}
+
+async function getNftId(call, responseObject) {
+  const externalRef = call.params[0]
+
+  if (utils.isValidString(externalRef) === false) {
+    utils.logError('invalid external ref', call.id, externalRef)
+    responseObject.error = { code: -32602, message: 'Invalid params' }
+  } else {
+    await queryChain(responseObject, call.id, 'nftManager', 'nfts', ['entries', externalRef], filterNftId)
+  }
+}
+
+async function getAccountPaymentNonce(call, responseObject) {
+  const accountId = call.params[0]
+
+  if (utils.isValidAccountId(accountId) === false) {
+    utils.logError('invalid account ID', call.id, accountId)
+    responseObject.error = { code: -32602, message: 'Invalid params' }
+  } else {
+    await queryChain(responseObject, call.id, 'avnProxy', 'paymentNonces', [accountId], formatNumAsString)
+  }
+}
+
+async function getAvtContractAddress(call, responseObject) {
+  await queryChain(responseObject, call.id, 'tokenManager', 'aVTTokenContract', [], formatAsString)
+}
+
+async function getRelayerFees(call, responseObject) {
+  const relayer = call.params[0]
+  const user = call.params[1]
+  const transactionType = call.params[2]
+
+  try {
+    if (utils.isValidAccountId(relayer) === false) throw 'relayer'
+    if (user && utils.isValidAccountId(user) === false) throw 'user'
+    if (transactionType && utils.isValidTransactionType(transactionType) === false) throw 'transaction type'
+  } catch (param) {
+    const errorMsg = 'invalid ' + param
+    utils.logError(errorMsg, call.id, call.params)
+    responseObject.error = { code: -32602, message: 'Invalid params' }
+    return
+  }
+
+  try {
+    const response = await utils.axios.post(AVN_CONNECTOR_ENDPOINT + 'relayerFees', { relayer, user, transactionType })
+    responseObject.result = response.data
+  } catch (err) {
+    utils.logError('failed to call avn-connector', call.id, err)
+    responseObject.error = { code: -32603, message: 'Internal error' }
+  }
+}
+
+const formatAsString = data => data.toString()
+
+const formatNumAsString = data => utils.toBnString(data)
 
 const formatBalanceAsString = data => utils.toBnString(data.data.free)
 

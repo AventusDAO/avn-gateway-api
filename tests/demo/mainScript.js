2c829274ab1ed790d529da2812ddd3b61f167695
alexpartos <alex.pinto@artos.io>
2021-12-24 11:46:25 +0000

    Main Script updates

--- tests/demo/mainScript.js
+++ tests/demo/mainScript.js
@@ -2,10 +2,9 @@ const BN = require('bn.js')
 const colors = require('colors');
 const prompt = require('prompt-sync')()
 const yargs = require('yargs')
-const { accounts } = require('./demo-accounts.json')
+const { accounts } = require('./demo-accounts-testnet.json')
 const AvnApi = require('avn-api');
 const { Keyring } = require('@polkadot/keyring');
-const { create } = require('domain');
 const keyring = new Keyring({ type: 'sr25519', ss58Format: 42 })
 
 const dummyT1Authority = '0xd6ae8250b8348c94847280928c79fb3b63ca453e'
@@ -22,28 +21,28 @@ async function main() {
     let config = argv.config
 
     const CONFIG = require(config)
-    console.log(`Connecting to ${CONFIG.gateway}`)
     let api = await avnApi(CONFIG.gateway)
 
     let totalAVT = new BN(await api.query.getTotalAvt())
-    console.log('Total Chain AVT balance', totalAVT.toString())
+    console.log('Total Chain AVT balance'.magenta, totalAVT.toString())
 
     let SENDER = await initAccount(accounts.sender.mnemonic)
-    let RELAYER = await initAccount(accounts.relayer.mnemonic)
-    let RECEIVER = await initAccount(accounts.receiver.mnemonic)
+    let relayerAddress = CONFIG.relayer
+    let receiverAddress = CONFIG.receiver
+
     console.log(`Account balances`.bold)
-
     let senderBalance = new BN(await queryBalance(api, SENDER.address))
-    let relayerBalance = new BN(await queryBalance(api, RELAYER.address))
-    let receiverBalance = new BN(await queryBalance(api, RECEIVER.address))
-    console.log(`[${SENDER.address}]: `.brightBlue + `${senderBalance}`)
-    console.log(`[${RELAYER.address}]: `.brightBlue + `${relayerBalance}`)
-    console.log(`[${RECEIVER.address}]: `.brightBlue + `${receiverBalance}`)
+    let relayerBalance = new BN(await queryBalance(api, relayerAddress))
+    let receiverBalance = new BN(await queryBalance(api, receiverAddress))
+    console.log(`Sender   [${SENDER.address}]: `.brightBlue + `${formatBalance(senderBalance)}`)
+    console.log(`Relayer  [${relayerAddress}]: `.brightBlue + `${formatBalance(relayerBalance)}`)
+    console.log(`Receiver [${receiverAddress}]: `.brightBlue + `${formatBalance(receiverBalance)}`)
 
-    // TODO: fix
+    prompt('\nPress Enter to Transfer Demo')
+
     let txFee
     try {
-        txFee = await api.query.getRelayerFees(RELAYER.address, SENDER.address)
+        txFee = await api.query.getRelayerFees(relayerAddress, SENDER.address)
     } catch (err) {
         console.log('Error obtaining fees', err)
         txFee = 'N/A'
@@ -52,24 +51,26 @@ async function main() {
 
     let amount = new BN(1000)
     try {
-        const requestId = await api.send.transferAvt(RELAYER.address, RECEIVER.address, amount)
+        console.log(`\nTransferring ${amount} AVT from Sender to Receiver`.magenta)
+        const requestId = await api.send.transferAvt(relayerAddress, receiverAddress, amount.toString())
+        await confirmStatus(api, requestId, 'Processed')
     } catch (err) {
         console.log('Error sending transferAVT transaction', err)
     }
 
     let newSenderBalance = new BN(await queryBalance(api, SENDER.address))
-    let newRelayerBalance = new BN(await queryBalance(api, RELAYER.address))
-    let newReceiverBalance = new BN(await queryBalance(api, RECEIVER.address))
-    console.log(`[${SENDER.address}]: `.brightBlue + `${newSenderBalance}`)
-    console.log(`[${RELAYER.address}]: `.brightBlue + `${newRelayerBalance}`)
-    console.log(`[${RECEIVER.address}]: `.brightBlue + `${newReceiverBalance}`)
+    let newRelayerBalance = new BN(await queryBalance(api, relayerAddress))
+    let newReceiverBalance = new BN(await queryBalance(api, receiverAddress))
+    console.log(`Sender   [${SENDER.address}]: `.brightBlue + `${formatBalance(newSenderBalance)}`)
+    console.log(`Relayer  [${relayerAddress}]: `.brightBlue + `${formatBalance(newRelayerBalance)}`)
+    console.log(`Receiver [${receiverAddress}]: `.brightBlue + `${formatBalance(newReceiverBalance)}`)
 
     let relayerGain = newRelayerBalance.sub(relayerBalance)
-    let senderPaid = newSenderBalance.sub(senderBalance)
+    let senderPaid = senderBalance.sub(newSenderBalance)
+
     let relayerFees = senderPaid.sub(amount)
     let networkFees = relayerFees.sub(relayerGain)
     let receiverGain = newReceiverBalance.sub(receiverBalance)
-
 
     console.log(`Receiver's gain: ${receiverGain}`.yellow.bold)
     console.log()
@@ -87,35 +88,36 @@ async function main() {
     let externalRef = createExternalRef()
     let nftId
     try {
-        const requestId = await api.send.mintSingleNft(RELAYER.address, externalRef, royalties, dummyT1Authority)
-        await helper.confirmStatus(api, requestId, 'Processed')
+        console.log('\nMinting new NFT'.magenta)
+        const requestId = await api.send.mintSingleNft(relayerAddress, externalRef, royalties, dummyT1Authority)
+        await confirmStatus(api, requestId, 'Processed')
         nftId = await api.query.getNftId(externalRef)
         console.log(`Minted new NFT: ${nftId} with ref ${externalRef}`)
     } catch (err) {
         console.log('Error sending mint NFT transaction', err)
     }
 
-    // Find the owner of the NFT in the AvN
-
     try {
-        let requestId = await api.send.listFiatNftForSale(RELAYER.address, nftId)
-        console.log(`Listed NFT for sale: ${nftId}`)
-        prompt('...')
-        requestId = await api.send.transferFiatNft(RELAYER.address, SENDER.address, nftId)
-        console.log(`and transferred it to ${SENDER.address}`)
+        console.log(`\nListing NFT for sale: ${nftId}`.magenta)
+        let requestId = await api.send.listFiatNftForSale(relayerAddress, nftId)
+        await confirmStatus(api, requestId, 'Processed')
+
+        console.log(`\nand transferring it to ${SENDER.address}`.magenta)
+        requestId = await api.send.transferFiatNft(relayerAddress, SENDER.address, nftId)
+        await confirmStatus(api, requestId, 'Processed')
     } catch (err) {
         console.log('Error sending list NFT transaction', err)
     }
 
-    // Find the owner
 
     try {
-        let requestId = await api.send.listFiatNftForSale(RELAYER.address, nftId)
-        console.log(`Re-Listed NFT for sale: ${nftId}`)
-        prompt('...')
-        requestId = await api.send.cancelFiatNftListing(relayer, nftId)
-        requestId = await api.send.cancelFiatNftListing(RELAYER.address, RECIPIENT.address, nftId)
-        console.log(`but cancelled the sale`)
+        console.log(`\nRe-Listing NFT for sale: ${nftId}`.magenta)
+        let requestId = await api.send.listFiatNftForSale(relayerAddress, nftId)
+        await confirmStatus(api, requestId, 'Processed')
+
+        console.log(`but cancelling the sale`)
+        requestId = await api.send.cancelFiatNftListing(relayerAddress, nftId)
+
     } catch (err) {
         console.log('Error sending list NFT transaction', err)
     }
@@ -150,6 +152,42 @@ async function initAccount(suri) {
 async function queryBalance(api, address) {
     let balance = await api.query.getAvtBalance(address)
     return balance.toString()
+}
+
+async function confirmStatus(api, requestId, expectedStatus) {
+    console.log(`Waiting to confirm gateway request ${requestId}...`)
+    if (!requestId) throw new Error('RequestId cannot be null')
+
+    for (i = 0; i < 10; i++) {
+        await sleep(3000)
+        const status = await api.poll.requestState(requestId)
+        if (status !== 'Pending' && status !== 'Transaction not found') {
+            if (status !== expectedStatus) console.log(`Unexpected different status: ${status} - ${expectedStatus}`.red)
+            break
+        }
+    }
+    console.log(`... confirmed!`)
+}
+
+async function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+function formatBalance(balanceAsBN) {
+    let balanceAsStr = balanceAsBN.toString()
+    if (balanceAsStr.length > 18) {
+        let units = balanceAsStr.substring(0, balanceAsStr.length - 18)
+        let remainder = balanceAsStr.substring(units.length)
+        if (units.length < 3) {
+            let leadingZeroes = new Array(3 - units.length + 1).join(' ')
+            units = leadingZeroes + units
+        }
+        return `${units}.${remainder} AVT`
+    } else if (balanceAsStr.length > 15) {
+        let units = balanceAsStr.substring(0, balanceAsStr.length - 15)
+        let remainder = balanceAsStr.substring(units.length)
+        return `${units}.${remainder} milliAVT`
+    } else return balanceAsStr
 }
 
 (async () => {

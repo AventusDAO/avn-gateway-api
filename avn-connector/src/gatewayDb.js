a5afc5d5134cd0f237316ce01cc6cddb40ece872
sleepert <john.terry@aventus.io>
2022-01-05 16:35:40 +0000

    SYS-1502 change log messages and fix fluentbit config

--- avn-connector/src/gatewayDb.js
+++ avn-connector/src/gatewayDb.js
@@ -143,7 +143,8 @@ async function getRelayerFees(relayerAddress) {
 
 async function getUserFeesIfAny(relayerAddress, userAddress) {
   if (!relayerAddress || !userAddress) {
-    log.trace(`Relayer address or User address is missing. RelayerAddress: ${relayerAddress}, UserAddress: ${userAddress}`)
+    log.trace({message: "Relayer address or User address is missing.", 
+      RelayerAddress: relayerAddress, UserAddress: userAddress})
     return undefined
   }
 

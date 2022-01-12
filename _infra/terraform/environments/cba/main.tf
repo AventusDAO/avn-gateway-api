2b3b6860715f1b5323fc998cb2a22645e011642f
sleepert <john.terry@aventus.io>
2022-01-12 09:18:12 +0000

    apply working version for eks module

--- _infra/terraform/environments/cba/main.tf
+++ _infra/terraform/environments/cba/main.tf
@@ -139,7 +139,8 @@ provider "kubernetes" {
 }
 
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "17.24.0"
 
   cluster_version   = local.cluster_version
   cluster_name      = local.name

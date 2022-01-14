5a3b4e40e9bd3070511a1fbab0dc72635b6348db
fluorostani <glyn.dimond@artos.io>
2022-01-14 12:32:25 +0000

    handle in common

--- avn-gateway/poll-handler.js
+++ avn-gateway/poll-handler.js
@@ -15,11 +15,11 @@ async function processRequest(request) {
   try {
     call = JSON.parse(request)
   } catch (err) {
-    return utils.errorResponse('parse', 'failed to parse JSON', err, request, createResponse())
+    return utils.errorResponse('parse', 'failed to parse JSON', err, request, null)
   }
 
   if (typeof call.method !== 'string') {
-    return utils.errorResponse('request', 'method type must be string', call.method, request, createResponse(call.id))
+    return utils.errorResponse('request', 'method type must be string', call.method, request, call.id)
   } else {
     return await makeCall(call, request)
   }
@@ -27,13 +27,13 @@ async function processRequest(request) {
 
 async function makeCall(call, request) {
   if (call.method !== 'requestState') {
-    return utils.errorResponse('method', "method must be 'requestState'", call.method, request, createResponse(call.id))
+    return utils.errorResponse('method', "method must be 'requestState'", call.method, request, call.id)
   }
 
   const { requestId } = call.params
 
   if (utils.isValidRequestId(requestId) === false) {
-    return utils.errorResponse('params', 'invalid request ID', requestId, request, createResponse(call.id))
+    return utils.errorResponse('params', 'invalid request ID', requestId, request, call.id)
   }
 
   return await poll(call, request, requestId)
@@ -43,18 +43,8 @@ async function poll(call, request, requestId) {
   try {
     const avnResponse = await utils.axios.post(AVN_CONNECTOR_ENDPOINT + 'avnPoll', { call.id, requestId })
     const result = avnResponse.data.error || avnResponse.data.status
-    return createResponse(call.id, result)
+    return utils.validResponse(call.id, result)
   } catch (err) {
-    return utils.errorResponse('internal', 'failed to poll chain', err, request, createResponse(call.id))
-  }
-}
-
-function createResponse(id, result) {
-  if (id === undefined) id = null
-
-  if (result === undefined) {
-    return { jsonrpc: '2.0', id }
-  } else {
-    return { jsonrpc: '2.0', id, result }
+    return utils.errorResponse('internal', 'failed to poll chain', err, request, call.id)
   }
 }

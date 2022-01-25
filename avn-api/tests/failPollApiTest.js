8603715399da78fc3e9fdac520919f77e76ad5d0
Micael Franco <micael.franco@aventus.io>
2022-01-25 13:13:01 +0000

    SYS-1783 Fail cases for Poll api and test patterns file implemented

--- avn-api/tests/failPollApiTest.js
+++ avn-api/tests/failPollApiTest.js
@@ -1,23 +1,24 @@
 const chai = require('chai');
 const expect = chai.expect;
-const assert = chai.assert;
 const testPatterns = require('./testPatterns.js');
+const helper = require('./helper.js');
 
-describe('Fail Poll api calls:', async () => {
-  before(async () => {
-    //set up params
-  });
-
-  beforeEach(async () => {
-    //reset state for isolation of tests
-  });
-
-  describe('requestState', async () => {
-    //requestState(requestId)
-    describe('fails when called', async () => {
-      describe('With invalid request', async () => {
-        await testPatterns.invalidRequest('Request', 'requestId', 'validCallData');
+// IIFE function to make async calls available before my test suite
+// Must be run with --delay flag
+(async function () {
+  const api = await helper.avnApi();
+  describe('Fail Poll api calls:', async () => {
+    describe('requestState', async () => {
+      //requestState(requestId)
+      describe('fails when called', async () => {
+        let validCallData = {
+          requestId: 'Processed'
+        };
+        describe('With invalid request', async () => {
+          await testPatterns.invalidRequestState('Request', 'requestId', validCallData, api.poll.requestState);
+        });
       });
     });
   });
-});
+  run();
+})();
--- avn-api/tests/testPatterns.js
+++ avn-api/tests/testPatterns.js
@@ -1,6 +1,7 @@
 const chai = require('chai');
 const expect = chai.expect;
-const assert = chai.assert;
+chai.use(require('chai-as-promised'));
+const helper = require('./helper.js');
 
 /**
  * This method encapsulates in one call all the possible tests for an invalid account. It could be used with any account.
@@ -8,70 +9,129 @@ const assert = chai.assert;
  * But the arguments have to be tuned in a way that can be used generically.
  *
  * @param message A context to qualify each individual test as part of this block
- * @param accountFieldName An identified to the part of the request (validCallData) that is being tested in this block.
+ * @param fieldName An identified to the part of the request (validCallData) that is being tested in this block.
  * @param validCallData An object that can be used to create a valid request. It should be as completely formed as possible,
  * so that this code (which is generic) does not have to know how to create requests
+ * @param testFunction The function being tested
  */
-
-async function invalidAccount(message, accoutAddress, validCallData) {
+async function invalidAccount(message, fieldName, validCallData, testFunction) {
   it(message + ' is empty', async () => {
-    // Modify call data by making a single account field invalid. In this case, empty
-    // validCallData[accountFieldName] = ''
-    // submit request
-    // check success / failure
+    validCallData[fieldName] = '';
+    await expect(testFunction(...Object.values(validCallData))).to.be.rejectedWith(/Expected non-null, non-empty base58 input/);
   });
 
   it(message + ' is undefined', async () => {
-    // Modify call data by making a single account field invalid. In this case, undefined
-    // validCallData[accountFieldName] = ''
-    // submit request
-    // check success / failure
+    validCallData[fieldName] = undefined;
+    await expect(testFunction(...Object.values(validCallData))).to.be.rejectedWith(/Expected non-null, non-empty base58 input/);
   });
 
   // Valid formats: SS58 (variable length) and hex string (fixed length: 32 bytes)
-  it(message + ' is in invalid format', async () => {});
+  it(message + ' is in invalid format', async () => {
+    validCallData[fieldName] = 'invalid_format';
+    await expect(testFunction(...Object.values(validCallData))).to.be.rejectedWith(/Invalid base58 character/);
+  });
 
-  it(message + ' is too short hex string to be a public key', async () => {});
+  it(message + ' is too short hex string to be a public key', async () => {
+    validCallData[fieldName] = '0x30ccad92fa31a27621c5fdf872c0244d92b0211662c5bce869d93edf79120f2';
+    await expect(testFunction(...Object.values(validCallData))).to.be.rejectedWith(/Invalid base58 character/);
+  });
 
-  it(message + ' is too ,ong hex string to be a public key', async () => {});
+  it(message + ' is too long hex string to be a public key', async () => {
+    validCallData[fieldName] = '0x30ccad92fa31a27621c5fdf872c0244d92b0211662c5bce869d93edf79120f2eq';
+    await expect(testFunction(...Object.values(validCallData))).to.be.rejectedWith(/Invalid base58 character/);
+  });
 }
 
-async function invalidEthereumAccount(message, accoutAddress, validCallData) {
-  it(message + ' is empty');
-  it(message + ' is undefined');
-  it(message + ' is in invalid format');
+function invalidAmount(message, fieldName, validCallData, testFunction) {
+  it(message + 'is greater than senders balance', async () => {
+    validCallData[fieldName] = 100000000000000000000000000000000000000000000000000000000000000;
+    await expect(testFunction(...Object.values(validCallData))).to.be.rejectedWith(/Invalid amount type:/);
+  });
+  //TODO Fix error message "Cannot read property 'toString' of undefined"
+  xit(message + 'is undefined', async () => {
+    validCallData[fieldName] = undefined;
+    console.log(await testFunction(...Object.values(validCallData)));
+  });
+  it(message + 'is zero', async () => {
+    validCallData[fieldName] = 0;
+    await expect(testFunction(...Object.values(validCallData))).to.be.rejectedWith(/Invalid amount type:/);
+  });
+  it(message + 'is a negative value', async () => {
+    validCallData[fieldName] = -1;
+    await expect(testFunction(...Object.values(validCallData))).to.be.rejectedWith(/Invalid amount type:/);
+  });
+  it(message + 'is not a number', async () => {
+    validCallData[fieldName] = 'string';
+    await expect(testFunction(...Object.values(validCallData))).to.be.rejectedWith(/Invalid amount type:/);
+  });
 }
 
-function invalidAmount(message, amount, token, validCallData) {
-  it(message + 'is greater than senders balance');
-  it(message + 'is undefined');
-  it(message + 'is zero');
-  it(message + 'is a negative value');
-  it(message + 'is not a number');
+function invalidEthereumToken(message, fieldName, validCallData, testFunction) {
+  it(message + 'is empty', async () => {
+    validCallData[fieldName] = '';
+    await expect(testFunction(...Object.values(validCallData))).to.be.rejectedWith(/Invalid base58 character/);
+  });
+  it(message + 'is undefined', async () => {
+    validCallData[fieldName] = undefined;
+    await expect(testFunction(...Object.values(validCallData))).to.be.rejectedWith(/Invalid base58 character/);
+  });
+  it(message + 'is in invalid format', async () => {
+    validCallData[fieldName] = 'invalid_format';
+    await expect(testFunction(...Object.values(validCallData))).to.be.rejectedWith(/Invalid base58 character/);
+  });
 }
 
-function invalidEthereumToken(message, token, validCallData) {
-  it(message + 'is empty');
-  it(message + 'is undefined');
-  it(message + 'is in invalid format');
+function invalidNftId(message, fieldName, validCallData, testFunction) {
+  it(message + ' is empty', async () => {
+    validCallData[fieldName] = '';
+    await expect(testFunction(...Object.values(validCallData))).to.be.rejectedWith(
+      /Cannot read property 'postRequest' of undefined/
+    );
+  });
+  it(message + ' is undefined', async () => {
+    validCallData[fieldName] = undefined;
+    await expect(testFunction(...Object.values(validCallData))).to.be.rejectedWith(
+      /Cannot read property 'postRequest' of undefined/
+    );
+  });
+  it(message + ' doesnt exist', async () => {
+    validCallData[fieldName] = 'idThatDoesntExist';
+    await expect(testFunction(...Object.values(validCallData))).to.be.rejectedWith(
+      /Cannot read property 'postRequest' of undefined/
+    );
+  });
 }
 
-function invalidNftId(message, id, validCallData) {
-  it(message + 'is empty');
-  it(message + 'is undefined');
-  it(message + 'doesnt exist');
+function invalidExternalReference(message, fieldName, validCallData, testFunction) {
+  it(message + 'is empty', async () => {
+    validCallData[fieldName] = '';
+    await expect(testFunction(...Object.values(validCallData))).to.be.rejectedWith(/String is not populated:/);
+  });
+  it(message + 'is undefined', async () => {
+    validCallData[fieldName] = undefined;
+    await expect(testFunction(...Object.values(validCallData))).to.be.rejectedWith(/String is not populated:/);
+  });
+  it(message + 'is in invalid format', async () => {
+    validCallData[fieldName] = 'invalid_reference';
+    await expect(testFunction(...Object.values(validCallData))).to.be.rejectedWith(
+      /Cannot read property 'nftsMap' of undefined/
+    );
+  });
 }
 
-function invalidExternalReference(message, id, validCallData) {
-  it(message + 'is empty');
-  it(message + 'is undefined');
-  it(message + 'is in invalid format');
-}
-
-function invalidRequest(message, requestId, validCallData) {
-  it(message + ' is empty');
-  it(message + ' is undefined');
-  it(message + ' valid but not existent');
+function invalidRequestState(message, fieldName, validCallData, testFunction) {
+  it(message + ' is empty', async () => {
+    validCallData[fieldName] = '';
+    await expect(testFunction(...Object.values(validCallData))).to.be.rejectedWith(/Invalid request ID type:/);
+  });
+  it(message + ' is undefined', async () => {
+    validCallData[fieldName] = undefined;
+    await expect(testFunction(...Object.values(validCallData))).to.be.rejectedWith(/Invalid request ID type:/);
+  });
+  it(message + ' valid but not existent', async () => {
+    validCallData[fieldName] = 'idThatDoesntExist';
+    await expect(testFunction(...Object.values(validCallData))).to.be.rejectedWith(/Invalid request ID type:/);
+  });
 }
 
 module.exports = {
@@ -80,6 +140,5 @@ module.exports = {
   invalidEthereumToken,
   invalidNftId,
   invalidExternalReference,
-  invalidRequest,
-  invalidEthereumAccount
+  invalidRequestState
 };

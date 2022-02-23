3c5e50f9cfc563c046a941289fce2459b537bd27
fluorostani <glyn.dimond@artos.io>
2022-02-23 17:51:02 +0000

    Update avn.js

--- avn-connector/src/avn.js
+++ avn-connector/src/avn.js
@@ -51,7 +51,7 @@ async function poll(requestId) {
       log.error(`No transaction found for requestId: ${requestId}`);
       return { error: 'Transaction not found' };
     }
-    log.trace("TXTXTXTXTXTXTXTXTXXTXTXTXXTXTXTXTXTXTXTXTX", tx)
+    log.trace({txtxtxtxtxtxtxtxtxtxttxtxtxtxtxttxtxtxtxtxtxttxtxtxtxttxtxtxt: tx})
     return { status: tx.status };
   } catch (error) {
     log.error(`Error getting transaction status for requestId ${requestId}: ${error}`);

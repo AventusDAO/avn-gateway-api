37dcb708a1fcbe549a388dd715d7b728420465a2
Nahu Seyoum <nahu.seyoum@artos.io>
2022-03-18 01:10:01 +0000

    more logs

--- avn-connector/src/stakingHelper.js
+++ avn-connector/src/stakingHelper.js
@@ -1,5 +1,7 @@
 const BN = require('bn.js');
 const BN_ZERO = new BN(0);
+const log4js = require('log4js');
+const log = log4js.getLogger();
 
 function calculateBondedAmount(stakingInfo) {
   let bonded = BN_ZERO;
@@ -21,16 +23,16 @@ function calculateUnbondingAmount(stakingInfo) {
     return BN_ZERO;
   }
 
-  console.log("Unlocking data:", JSON.stringify(stakingInfo.unlocking, null, 2));
+  log.info("Unlocking data:", JSON.stringify(stakingInfo.unlocking, null, 2));
   const filtered = stakingInfo.unlocking
     .filter(({ remainingEras, value }) => value.gt(BN_ZERO) && remainingEras.gt(BN_ZERO))
     .map(unlock => {
-      console.log("Value: ", new BN(unlock.value).toString());
+      log.info("Value: ", new BN(unlock.value).toString());
       return unlock.value;
     });
 
   const amount = filtered.reduce((total, value) => total.iadd(value), BN_ZERO);
-  console.log("Amount: ", amount);
+  log.info("Amount: ", amount);
 
   return amount;
 }

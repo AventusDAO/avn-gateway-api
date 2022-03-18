ee5de5dcc11d76251339a03f618b6d0dc04cdbef
Nahu Seyoum <nahu.seyoum@artos.io>
2022-03-18 09:06:54 +0000

    even more logs

--- avn-connector/src/stakingHelper.js
+++ avn-connector/src/stakingHelper.js
@@ -23,13 +23,22 @@ function calculateUnbondingAmount(stakingInfo) {
     return BN_ZERO;
   }
 
+  log.info("BN_ZERO: ", BN_ZERO.toString());
+  log.info(`BN_ZERO: ${BN_ZERO.toString()}`);
+
   log.info("\nUnlocking data:", JSON.stringify(stakingInfo.unlocking, null, 2));
+
   const filtered = stakingInfo.unlocking
     .filter(({ remainingEras, value }) => {
-      log.warn("value: ", new BN(value).toString());
-      log.warn("remainingEras: ", new BN(remainingEras).toString());
-      log.warn("value.gt(BN_ZERO): ", new BN(value).gt(BN_ZERO));
-      log.warn("remainingEras.gt(BN_ZERO): ", new BN(remainingEras).gt(BN_ZERO));
+      log.warn(`value: ${new BN(value).toString()}`);
+      log.warn(`remainingEras: ${new BN(remainingEras).toString()}`);
+
+      log.warn(`value.gt(BN_ZERO): ${value.gt(BN_ZERO)}`);
+      log.warn(`remainingEras.gt(BN_ZERO): ${remainingEras.gt(BN_ZERO)}`);
+
+      log.warn(`value.gt(BN_ZERO): ${new BN(value).gt(BN_ZERO)}`);
+      log.warn(`remainingEras.gt(BN_ZERO): ${new BN(remainingEras).gt(BN_ZERO)}`);
+
       return new BN(value).gt(BN_ZERO) && new BN(remainingEras).gt(BN_ZERO);
     })
     .map(unlock => {
@@ -37,9 +46,17 @@ function calculateUnbondingAmount(stakingInfo) {
       return unlock.value;
     });
 
-  log.warn("filtered: ", JSON.stringify(filtered));
+  log.warn("filtered JSON: ", JSON.stringify(filtered));
+  log.warn(`filtered String: ${JSON.stringify(filtered)}`);
+  log.warn(`filtered raw: `, filtered);
+  log.warn(`filtered raw: ${filtered}`);
 
-  const amount = filtered.reduce((total, value) => total.iadd(value), BN_ZERO);
+  const amount = filtered.reduce((total, value) => {
+    log.warn(` [reducer] - value: ${value}`);
+    log.warn(` [reducer] - total: ${total}`);
+    return total.iadd(value), BN_ZERO;
+  });
+
   log.warn("\n\nAmount: ", amount.toString());
 
   return amount;

447d723aa053549f836f71a34fe72ca01dfc213d
fluorostani <glyn.dimond@artos.io>
2022-03-28 12:59:47 +0100

    Update send-handler.js

--- avn-gateway/send-handler.js
+++ avn-gateway/send-handler.js
@@ -240,6 +240,7 @@ function getPaymentInfo(user, relayer, relayerFee, proxyProof, feePaymentSignatu
   }
 
   return {
+    payer: user,
     recipient: relayer,
     amount: relayerFee,
     signature: {

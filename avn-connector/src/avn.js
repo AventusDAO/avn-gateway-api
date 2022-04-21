5668a5cc67129a93d039b1711a568dd8e280d7d3
fluorostani <glyn.dimond@artos.io>
2022-04-21 19:02:59 +0100

    Update avn.js

--- avn-connector/src/avn.js
+++ avn-connector/src/avn.js
@@ -104,14 +104,13 @@ async function getNonce(senderAddress) {
 }
 
 async function getLowerData(blockNumber, transactionIndex) {
-  console.log('%j', api.rpc.rpc)
-  // let summaryRange = await getSummaryRange(blockNumber);
-  // let lowerData = await api.rpc.lower_data(summaryRange[0], summaryRange[1], blockNumber, transactionIndex);
-  // const data = JSON.parse(Buffer.from(lowerData, 'hex').toString());
-  // const leaf = '0x'+Buffer.from(data.encoded_leaf).toString('hex');
-  // const merklePath = '[' + data.merkle_path.join(',').replace(/'/g, '') + ']';
-  //
-  // return { leaf, merklePath };
+  let summaryRange = await getSummaryRange(blockNumber);
+  let lowerData = await api.rpc.rpc.lower_data(summaryRange[0], summaryRange[1], blockNumber, transactionIndex);
+  const data = JSON.parse(Buffer.from(lowerData, 'hex').toString());
+  const leaf = '0x'+Buffer.from(data.encoded_leaf).toString('hex');
+  const merklePath = '[' + data.merkle_path.join(',').replace(/'/g, '') + ']';
+
+  return { leaf, merklePath };
 }
 
 async function getEthTxHash(summaryRange) {

0f46d0309125b8ca6a2eabe85469b08b748a79e8
fluorostani <glyn.dimond@artos.io>
2022-04-21 11:51:06 +0100

    Update avn.js

--- avn-connector/src/avn.js
+++ avn-connector/src/avn.js
@@ -116,7 +116,7 @@ async function getSummaryRange(blockNumber) {
 }
 
 async function getEthTxHash(from_block, to_block) {
-  console.log("\n\n\n", api.query.summary.roots, "\n\n\n")
+  console.log("\n\n\n", api.query.summary.roots.toJSON(), "\n\n\n")
   let rootData = await api.query.summary.roots(from_block, to_block, null);
   console.log("\n\n\n", rootData, "\n\n\n")
   if (rootData.length === 0) {

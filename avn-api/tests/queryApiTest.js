4adff0ed0d7c51c75a373b5989ddaa43ff7bfd1f
Nahu Seyoum <nahu.seyoum@artos.io>
2022-04-21 17:16:33 +0100

    fix tests

--- avn-api/tests/queryApiTest.js
+++ avn-api/tests/queryApiTest.js
@@ -195,7 +195,7 @@ describe('Query api calls:', async () => {
   });
 
   describe('getStakingStats', async () => {
-    const defaultMaxNominatorsRewardedPerValidatorBN = new BN(265);
+    const defaultMaxNominatorsRewardedPerValidatorBN = new BN(256);
     const defaultMinUserBondBN = new BN("100000000000000000000");
 
     it('returns the correct data', async () => {
@@ -208,13 +208,15 @@ describe('Query api calls:', async () => {
       const maxNominatorsRewardedPerValidatorBN = new BN(returnedData.maxNominatorsRewardedPerValidator);
       const totalStakersBN = new BN(returnedData.totalStakers);
 
-      assert(totalStakedBN.gte(BN_ZERO));
-      assert(averageStakedBN.gte(BN_ZERO));
-      assert(averageStakedBN.lte(totalStakedBN));
-      assert(totalStakersBN.gte(BN_ZERO));
-      assert(minimumStakedBN.lte(averageStakedBN));
-      assert(maxNominatorsRewardedPerValidatorBN.eq(defaultMinUserBondBN));
-      assert(minUserBondBN.eq(defaultMaxNominatorsRewardedPerValidatorBN));
+      console.log(JSON.stringify(returnedData, null, 2));
+
+      assert(totalStakedBN.gte(BN_ZERO), "Total stake is zero");
+      assert(averageStakedBN.gte(BN_ZERO), "Average stake is zero");
+      assert(averageStakedBN.lte(totalStakedBN), "Average stake must be less than total stake");
+      assert(totalStakersBN.gte(BN_ZERO), "Total number of stakers is zero");
+      assert(minimumStakedBN.lte(averageStakedBN), "Minimum stake must be less than or equal to average stake");
+      assert(minUserBondBN.eq(defaultMinUserBondBN), "Minimum user bond does not match default value");
+      assert(maxNominatorsRewardedPerValidatorBN.eq(defaultMaxNominatorsRewardedPerValidatorBN), "Maximum number of nominators doesn't match default value");
     });
   });
 });

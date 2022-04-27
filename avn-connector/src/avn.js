b8c0f87bbcf70827e8d4b92ab2504e31b88eb4c1
fluorostani <glyn.dimond@artos.io>
2022-04-27 17:23:41 +0100

    Update avn.js

--- avn-connector/src/avn.js
+++ avn-connector/src/avn.js
@@ -176,18 +176,22 @@ function calculateSummaryRange(blockNumber) {
 
 async function retrieveEthTxHashIfExists(summaryRange) {
   let ethTxHash = await redis.getSummaryEthTxHash(summaryRange);
+  console.log('A', ethTxHash)
 
   if (!ethTxHash) {
     let blockHash = await api.rpc.chain.getBlockHash(summaryRange[0]);
     let ingressCounter = (await api.query.summary.totalIngresses.at(blockHash)) + 1;
     let transactionId = (await api.query.summary.roots(summaryRange, ingressCounter)).tx_id.toString();
+    console.log('B', ingressCounter, transactionId)
     if (!transactionId) {
       return null;
     }
 
     let ethTransactionCandidate = await api.query.ethereumTransactions.repository(transactionId);
     ethTxHash = ethTransactionCandidate.eth_tx_hash.toString();
+    console.log('C', ethTxHash)
     if ((await ethereum.transactionExists(ethTxHash)) === false) {
+      console.log('D - POST CHECK')
       return null;
     }
 

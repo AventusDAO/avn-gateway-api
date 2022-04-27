eb6c42c5f8058a5148419ec3fe719c3013db6fe8
fluorostani <glyn.dimond@artos.io>
2022-04-27 14:39:39 +0100

    flush redis and check

--- avn-connector/src/ethereum.js
+++ avn-connector/src/ethereum.js
@@ -4,6 +4,7 @@ const ETHERSCAN_URL = config.etherscan.etherscan_url;
 const ETHERSCAN_KEY = config.etherscan.etherscan_api_key;
 
 async function transactionExists(txHash) {
+  console.log(  `${ETHERSCAN_URL}module=transaction&action=gettxreceiptstatus&txhash=${txHash}&apikey=${ETHERSCAN_KEY}`)
   let receipt = await axios.get(
     `${ETHERSCAN_URL}module=transaction&action=gettxreceiptstatus&txhash=${txHash}&apikey=${ETHERSCAN_KEY}`
   );

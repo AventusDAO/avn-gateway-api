cf06c3ef6e8b7a252880c41d959dd598086e0418
Nahu Seyoum <nahu.seyoum@artos.io>
2022-05-11 00:25:21 +0100

    update

--- avn-connector/src/stakingHelper.js
+++ avn-connector/src/stakingHelper.js
@@ -66,7 +66,10 @@ function calculateStakingStats(stakersData, minUserBond, maxNominatorsRewardedPe
 }
 
 async function payoutAllStakers(registry, logger, relayerAccount, proxyNonce, lastPayoutEra, currentEra) {
-  const lastPayoutEraBN = new BN(lastPayoutEra);
+  let lastPayoutEraBN = new BN(lastPayoutEra);
+  // If we have never paid, start paying from the previous era
+  lastPayoutEraBN = lastPayoutEraBN.gt(new BN(0)) ? lastPayoutEraBN : lastPayoutEraBN.sub(1);
+
   const maxPayoutEraBN = new BN(currentEra).sub(1);
   let proxyNonceBN = new BN(proxyNonce);
 

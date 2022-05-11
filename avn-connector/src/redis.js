edd18f76cbfb20834f65b4a8f1744eccd61c9e3e
fluorostani <glyn.dimond@artos.io>
2022-05-11 11:51:02 +0100

    Update redis.js

--- avn-connector/src/redis.js
+++ avn-connector/src/redis.js
@@ -56,8 +56,6 @@ async function connect() {
     redisClient = new Redis();
   }
 
-  redisClient.flushall();
-
   redisClient.defineCommand('nextzsubset', {
     numberOfKeys: 2,
     lua: `local subset = redis.call('ZRANGE', KEYS[1], 0, ARGV[1]-1)

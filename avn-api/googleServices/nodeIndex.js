29ded9cd9c13a7cf2c4acfa017200466ccbd56aa
Micael Franco <micael.franco@aventus.io>
2022-07-21 14:11:06 +0100

    Linter

--- avn-api/googleServices/nodeIndex.js
+++ avn-api/googleServices/nodeIndex.js
@@ -37,7 +37,9 @@ const driveRefreshToken = process.env.GOOGLE_DRIVE_REFRESH_TOKEN || '';
   // Nightly_test_report_2022-07-19T08:02:43.621Z
   const date_now = new Date();
   let fileName = date_now.getHours() >= 6 ? 'Normal_test_report_' : 'Nightly_test_report_';
-  await googleDriveService.saveFile(fileName + date_now.toISOString(), finalPath, 'text/html', folder.id).catch((error) => {
+  await googleDriveService
+      .saveFile(fileName + date_now.toISOString(), finalPath, 'text/html', folder.id)
+      .catch((error) => {
     console.error(error);
   });
 

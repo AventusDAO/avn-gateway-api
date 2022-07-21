9c281e32f24f0afb571eaddb8707e6f770692dfb
micaelffrancoAV <97438428+micaelffrancoAV@users.noreply.github.com>
2022-07-21 18:16:44 +0100

    Sys 2411 implement system to track test results (#406)
    * Report integration
    
    * Report merging
    
    * generate merged final report
    
    * report directory changed
    
    * Add google services and jenkins file updated to get env variables and upload files
    
    * check for env file
    
    * env variables check
    
    * more logs
    
    * remove jenkins logs
    
    * env variables check
    
    * JenkinsFixenv variablkes fixed for jenkins
    
    * index fix
    
    * Reports folder name changed
    
    * Add all tests report
    
    * googleDrive refactored from ts to js
    
    * refactored google drive
    
    * Delete ts files and update package.json
    
    * SYS-2410 Filename fixed
    
    * Jenkins fix
    
    * report name fix
    
    * remove unnecessary arguments
    
    * Fixes to google services
    
    * Using google drive services from npm library
    
    * Linter
    
    * Param added to jenkinsfile
    
    * Fixing file name by adding chain and test descriptive parameters
    
    * Fix gateway arg in upload file script
    
    * Update param and fix args description

---
+++ avn-api/googleServices/nodeIndex.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const dotenv = require('dotenv');
+const path = require('path');
+const fs = require('fs');
+const NodeGoogleService = require('gds-avt');
+const yargs = require('yargs');
+
+let argv = yargs
+  .usage('Run upload using a given Gateway environment')
+  .help('h')
+  .alias('h', 'help')
+  .demandOption('c')
+  .describe('c', 'Configuration file with gateway parameters')
+  .string('c')
+  .alias('c', 'gateway')
+  .argv;
+
+dotenv.config();
+
+const driveClientId = process.env.GOOGLE_DRIVE_CLIENT_ID || '';
+const driveClientSecret = process.env.GOOGLE_DRIVE_CLIENT_SECRET || '';
+const driveRedirectUri = process.env.GOOGLE_DRIVE_REDIRECT_URI || '';
+const driveRefreshToken = process.env.GOOGLE_DRIVE_REFRESH_TOKEN || '';
+
+const test_descriptive = argv.test_descriptive || '';
+
+(async () => {
+  const googleDriveService = new NodeGoogleService(driveClientId, driveClientSecret, driveRedirectUri, driveRefreshToken);
+
+  const finalPath = path.resolve(__dirname, '../../finalReport.html');
+  const folderName = 'GatewayTestResults';
+
+  if (!fs.existsSync(finalPath)) {
+    throw new Error('File not found!');
+  }
+
+  let folder = await googleDriveService
+      .searchFolder(folderName)
+      .catch((error) => {
+    console.error(error);
+    return null;
+  });
+
+  if (!folder) {
+    folder = await googleDriveService.createFolder(folderName);
+  }
+
+  // Example file name
+  // uat_gateway_api_nightly_test_report_2022-07-19T08:02:43.621Z
+  const date_now = new Date();
+  let fileName = test_descriptive ? `_${test_descriptive}_` : '_gateway_api_nightly_test_report_';
+  await googleDriveService
+      .saveFile(argv.gateway + fileName + date_now.toISOString(), finalPath, 'text/html', folder.id)
+      .catch((error) => {
+    console.error(error);
+  });
+
+  console.info('File uploaded successfully!');
+
+  // Delete the file on the server
+  fs.unlinkSync(finalPath);
+})();

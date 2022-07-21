9761951fbc72a3ce29f3b2763a8762172f875900
Micael Franco <micael.franco@aventus.io>
2022-07-21 14:53:31 +0100

    Fixing file name by adding chain and test descriptive parameters

--- avn-api/googleServices/nodeIndex.js
+++ avn-api/googleServices/nodeIndex.js
@@ -4,6 +4,17 @@ const dotenv = require('dotenv');
 const path = require('path');
 const fs = require('fs');
 const NodeGoogleService = require('gds-avt');
+const yargs = require('yargs');
+
+let argv = yargs
+  .usage('Run baseStateGenerator using a given Gateway environment')
+  .help('h')
+  .alias('h', 'help')
+  .demandOption('c')
+  .describe('c', 'Configuration file with gateway parameters')
+  .string('c')
+  .alias('c', 'gateway')
+  .argv;
 
 dotenv.config();
 
@@ -12,11 +23,13 @@ const driveClientSecret = process.env.GOOGLE_DRIVE_CLIENT_SECRET || '';
 const driveRedirectUri = process.env.GOOGLE_DRIVE_REDIRECT_URI || '';
 const driveRefreshToken = process.env.GOOGLE_DRIVE_REFRESH_TOKEN || '';
 
+const test_descriptive = argv.test_descriptive || '';
+
 (async () => {
   const googleDriveService = new NodeGoogleService(driveClientId, driveClientSecret, driveRedirectUri, driveRefreshToken);
 
   const finalPath = path.resolve(__dirname, '../../finalReport.html');
-  const folderName = 'TestReports';
+  const folderName = 'GatewayTestResults';
 
   if (!fs.existsSync(finalPath)) {
     throw new Error('File not found!');
@@ -34,11 +47,11 @@ const driveRefreshToken = process.env.GOOGLE_DRIVE_REFRESH_TOKEN || '';
   }
 
   // Example file name
-  // Nightly_test_report_2022-07-19T08:02:43.621Z
+  // uat_gateway_api_nightly_test_report_2022-07-19T08:02:43.621Z
   const date_now = new Date();
-  let fileName = date_now.getHours() >= 6 ? 'Normal_test_report_' : 'Nightly_test_report_';
+  let fileName = test_descriptive ? `_${test_descriptive}_` : '_gateway_api_nightly_test_report_';
   await googleDriveService
-      .saveFile(fileName + date_now.toISOString(), finalPath, 'text/html', folder.id)
+      .saveFile(argv.gateway + fileName + date_now.toISOString(), finalPath, 'text/html', folder.id)
       .catch((error) => {
     console.error(error);
   });

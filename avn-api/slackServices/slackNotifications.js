de9b404f9cdae48bd9d3c439c476d0182d89ca0d
Micael Franco <micael.franco@aventus.io>
2022-07-27 18:09:21 +0100

    Have our private library dependencies imported from git urls

--- avn-api/slackServices/slackNotifications.js
+++ avn-api/slackServices/slackNotifications.js
@@ -1,14 +1,8 @@
 const yargs = require('yargs');
-const SlackNotification = require('strn-avt');
+const SlackNotification = require('avn-strn');
 const dotenv = require('dotenv');
 
 let argv = yargs
-  .usage('Run baseStateGenerator using a given Gateway environment')
-  .help('h')
-  .alias('h', 'help')
-  .demandOption('c')
-  .describe('c', 'Configuration file with gateway parameters')
-  .string('c')
   .alias('c', 'gateway')
   .argv;
 
@@ -17,4 +11,4 @@ const slackWebhookUrl = process.env.SLACK_WEBHOOK_URL || '';
 
 const slack = new SlackNotification(slackWebhookUrl);
 
-slack.sendReportNotification('../../finalReport.json', argv.gateway);
+slack.sendReportNotification('../../finalReport.json', 'Gateway', argv.gateway);

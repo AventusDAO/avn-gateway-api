0f165f2634064fa1d056534210058907995c7d34
Rui Dos Santos Magalhaes <m.rui.s.magalhaes@gmail.com>
2022-08-10 10:42:25 +0100

    cors configuration added

--- _infra/terraform/modules/api-gateway/main.tf
+++ _infra/terraform/modules/api-gateway/main.tf
@@ -1,6 +1,15 @@
 resource "aws_apigatewayv2_api" "avn_gateway_api" {
   name          = "avn-gateway-api"
   protocol_type = "HTTP"
+
+  cors_configuration {
+    allow_credentials = false
+    allow_headers     = ["*"]
+    allow_methods     = ["*"]
+    allow_origins     = ["*"]
+    expose_headers    = ["*"]
+    max_age           = 100
+  }
 }
 
 resource "aws_apigatewayv2_integration" "poll" {
@@ -106,7 +115,7 @@ resource "aws_apigatewayv2_route" "vote" {
   api_id    = aws_apigatewayv2_api.avn_gateway_api.id
   route_key = "ANY /vote"
 
-  target    = "integrations/${aws_apigatewayv2_integration.vote["full"].id}"
+  target = "integrations/${aws_apigatewayv2_integration.vote["full"].id}"
 }
 
 resource "aws_apigatewayv2_authorizer" "authoriser" {
@@ -118,7 +127,7 @@ resource "aws_apigatewayv2_authorizer" "authoriser" {
   authorizer_uri          = var.authoriser_invoke_arn
   enable_simple_responses = true
   identity_sources        = ["$request.header.Authorization"]
-  
+
   authorizer_result_ttl_in_seconds  = var.auth_cache_duration
   authorizer_payload_format_version = "2.0"
 }
@@ -152,7 +161,7 @@ resource "aws_apigatewayv2_stage" "default" {
 
   access_log_settings {
     destination_arn = aws_cloudwatch_log_group.gateway.arn
-    format          = jsonencode({
+    format = jsonencode({
       httpMethod     = "$context.httpMethod"
       ip             = "$context.identity.sourceIp"
       protocol       = "$context.protocol"

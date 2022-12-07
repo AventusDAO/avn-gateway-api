458847021bcacc03fbce87f942891e1e36b30f07
Nahu Seyoum <nahu.seyoum@artos.io>
2022-12-07 14:23:23 +0000

    Remove temporary flush

--- avn-connector/src/redis.js
+++ avn-connector/src/redis.js
@@ -74,9 +74,6 @@ async function connect() {
             return {}
           end`
   });
-
-  // This is temporary to kill off the zombie transactions
-  redisClient.flushall();
 }
 
 function getKey(key) {

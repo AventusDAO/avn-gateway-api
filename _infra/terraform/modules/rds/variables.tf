64ffde5c919a92aa79dbd8502d2cf3d0ff732345
Rui Dos Santos Magalhaes <m.rui.s.magalhaes@gmail.com>
2023-01-06 15:19:45 +0000

    gateway rds module; rds database on sandbox; rds database on dev

---
+++ _infra/terraform/modules/rds/variables.tf
@@ -0,0 +1,45 @@
+variable "multi_az" {
+  description = "Specifies if the RDS instance is multi-AZ"
+  type        = bool
+  default     = false
+}
+
+variable "backup_retention_period" {
+  description = "The days to retain backups for"
+  type        = number
+  default     = null
+}
+
+variable "allocated_storage" {
+  description = "The allocated storage in gigabytes"
+  type        = string
+  default     = 20
+}
+
+variable "max_allocated_storage" {
+  description = "Specifies the value for Storage Autoscaling"
+  type        = number
+  default     = 50
+}
+
+variable "subnet_ids" {
+  description = "List of subnets ids to add to the subnet group"
+  type        = list(string)
+}
+
+variable "vpc_id" {
+  description = "VPC id where the RDS will be provisioned"
+  type        = string
+}
+
+variable "performance_insights_enabled" {
+  description = "Specifies whether Performance Insights are enabled"
+  type        = bool
+  default     = false
+}
+
+variable "performance_insights_retention_period" {
+  description = "The amount of time in days to retain Performance Insights data. Valid values are `7`, `731` (2 years) or a multiple of `31`"
+  type        = number
+  default     = 7
+}

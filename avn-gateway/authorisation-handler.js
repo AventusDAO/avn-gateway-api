7e6c1be0d91e74ce0e2957b68af0b336c66d2762
nahuseyoum <39748285+nahuseyoum@users.noreply.github.com>
2023-01-10 20:55:45 +0000

    SYS-2333 refactor auth lambda (#456)

--- avn-gateway/authorisation-handler.js
+++ avn-gateway/authorisation-handler.js
@@ -34,7 +34,7 @@ async function validateAwtToken(event) {
     return InvalidRequestResponse;
   }
 
-  if ((await isNewUser(awtToken)) && !(await userHasAvtBalance(awtToken))) {
+  if (isValidSelfPayUser(awtToken) === false) {
     console.info('User does not have enough AVT to access the gateway');
     return InvalidRequestResponse;
   }
@@ -66,32 +66,25 @@ function tokenAgeIsValid(token) {
   }
 }
 
-async function isNewUser(awtToken) {
-  try {
-    const avnResponse = await utils.axios.post(AVN_CONNECTOR_ENDPOINT + 'avnQuery', {
-      palletName: 'avnProxy',
-      storageName: 'paymentNonces',
-      params: [awtToken.pk]
-    });
-    const paymentNonce = avnResponse.data.toString();
-    return paymentNonce === '0';
-  } catch (err) {
-    console.error('failed to check if existing user', err);
-    return true;
-  }
+async function isValidSelfPayUser(token) {
+  const userInfo = await tryGetUserInfo(awtToken);
+  if (!userInfo) return false;
+
+  const avtBalance = new utils.BN(userInfo.freeBalance.toString().replace('0x', ''), 16);
+  const existingUser = new utils.BN(userInfo.paymentNonce).gt(new utils.BN(0));
+
+  return existingUser === true || avtBalance.gte(MIN_AVT_BALANCE);
 }
 
-async function userHasAvtBalance(awtToken) {
+async function tryGetUserInfo(awtToken) {
   try {
-    const avnResponse = await utils.axios.post(AVN_CONNECTOR_ENDPOINT + 'avnQuery', {
-      palletName: 'system',
-      storageName: 'account',
-      params: [awtToken.pk]
+    const avnResponse = await utils.axios.post(AVN_CONNECTOR_ENDPOINT + 'gatewayUserInfo', {
+      account: awtToken.pk
     });
-    const avtBalance = new utils.BN(avnResponse.data.data.free.toString().replace('0x', ''), 16);
-    return avtBalance.gte(MIN_AVT_BALANCE);
+
+    return avnResponse.data;
   } catch (err) {
-    console.error('failed to check user AVT balance', err);
-    return false;
+    console.error('Failed to get user info from chain', err);
+    return undefined;
   }
 }

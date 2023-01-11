2e76a6a42112ce4caa9b293a5dc3a1dbd7fbc5ac
Rui Dos Santos Magalhães <45366558+RuiSMagalhaes@users.noreply.github.com>
2023-01-11 14:19:43 +0000

    cognito pool + app + custom (#460)
    **WHAT**
    - cognito pool, app and domain module
    - aws acm cert for cognito
    - cognito added for sandbox and dev

--- _infra/terraform/environments/cba/gateway/provider.tf
+++ _infra/terraform/environments/cba/gateway/provider.tf
@@ -35,6 +35,15 @@ provider "aws" {
 }
 
 provider "aws" {
+  region = "us-east-1"
+  alias  = "us_east_1"
+
+  assume_role {
+    role_arn = "arn:aws:iam::602004642405:role/jenkins-access"
+  }
+}
+
+provider "aws" {
   alias  = "aventus"
   region = var.region
 

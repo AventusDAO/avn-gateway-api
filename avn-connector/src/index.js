ffe2ef16a0aed7ff388f152b33ff0e4f67259423
fluorostani <glyn.dimond@artos.io>
2023-01-16 14:00:23 +0000

    Update index.js

--- avn-connector/src/index.js
+++ avn-connector/src/index.js
@@ -205,7 +205,7 @@ async function instantiateConnector() {
   await avn.init();
   await mqConsumer.connectToMQ();
   await gatewayDb.init();
-  lowering.getLowers('0x0');
+  lowering.getLowers('0x0'); // populate redis with up-to-date lower data on startup
 }
 
 (async () => {

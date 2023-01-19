75bc0fb6ae5c75ed5821a0677334d54a624c9c2d
Nahu Seyoum <nahu.seyoum@artos.io>
2023-01-19 17:58:22 +0000

    Implement new send-handler logic

---
+++ avn-gateway/send-handler.js
@@ -0,0 +1,84 @@
+const utils = require('/opt/utils.js');
+const sqs = require('/opt/sqsUtils.js');
+
+const sqsClient = new sqs.SQSClient({ region: process.env.SECRET_MANAGER_REGION });
+
+const DEFAULT_SQS_URL = process.env.SQS_DEFAULT_QUEUE_URL;
+const PAYER_SQS_URL = process.env.SQS_PAYER_QUEUE_URL;
+
+exports.handler = async (event, context) => {
+  const result = await processRequest(event.body, event.requestContext.authorizer.lambda, context.awsRequestId);
+
+  if (utils.requestFailed(result) === true) {
+    return utils.buildErrorResponse(500, result.error.data, JSON.stringify(result));
+  }
+
+  return utils.buildSuccessResponse(JSON.stringify(result))
+};
+
+async function processRequest(request, authoriserContext, awsRequestId) {
+  let tx;
+
+  try {
+    tx = JSON.parse(request);
+  } catch (err) {
+    return utils.buildErrorBody('parse', 'failed to parse JSON', err.toString(), request, null);
+  }
+
+  try {
+    console.info('TX_ID <-> AWS_REQUESTID:', tx.id + ' : ' + awsRequestId);
+
+    if (isSplitFeeTransaction(authoriserContext) === true)
+    {
+      const data = await sendMessageToPayerQueue(tx, request, awsRequestId, authoriserContext);
+      console.info(`Sent split fee transaction to SQS. txID: ${tx.id}, awsRequestId: ${awsRequestId}, sqsMessageId: ${data.MessageId}`);
+    } else {
+      const data = await sendMessageToDefaultQueue(tx, awsRequestId);
+      console.info(`Sent self pay transaction to SQS. txID: ${tx.id}, awsRequestId: ${awsRequestId}, sqsMessageId: ${data.MessageId}`);
+    }
+
+    return utils.buildValidResponseBody(tx.id, awsRequestId);
+  } catch (err) {
+    return utils.buildErrorBody('internal', 'failed to handle send transaction', err.toString(), request, tx.id);
+  }
+}
+
+async function sendMessageToDefaultQueue(tx, awsRequestId) {
+  tx.awsRequestId = awsRequestId;
+  const messageBody = JSON.stringify(tx);
+
+  const params = {
+    QueueUrl: DEFAULT_SQS_URL,
+    MessageGroupId: 'DEFAULT',
+    MessageDeduplicationId: utils.hashString(messageBody),
+    MessageBody: messageBody,
+  };
+
+  return await sqsClient.send(new sqs.SendMessageCommand(params));
+}
+
+async function sendMessageToPayerQueue(tx, request, awsRequestId, authoriserContext) {
+  tx.splitFeePayerAddress = authoriserContext.splitFeePayerAddress;
+  tx.awsRequestId = awsRequestId;
+  const messageBody = JSON.stringify(tx);
+
+  const params = {
+    QueueUrl: PAYER_SQS_URL,
+    MessageGroupId: 'PAYER',
+    MessageDeduplicationId: utils.hashString(messageBody),
+    MessageBody: messageBody,
+  };
+
+  if (tx.params.feePaymentSignature) throw new Error('split fee tx already contains payment info');
+
+  return await sqsClient.send(new sqs.SendMessageCommand(params));
+}
+
+function isSplitFeeTransaction(authoriserContext) {
+  if (!authoriserContext.splitFeePayerAddress) {
+    return false;
+  }
+
+  const hasValidPayer = utils.isValidAccountId(authoriserContext.splitFeePayerAddress);
+  return authoriserContext.isSplitFeeUser === true && hasValidPayer === true
+}

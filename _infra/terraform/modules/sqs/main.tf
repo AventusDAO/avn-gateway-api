41390bcdb5d44da86bcc7b4916d352f1cc63afc6
Vucomir Ianculov <vukomir@ianculov.ro>
2023-01-20 14:08:00 +0200

    SYS-3058 updated SQS retention to 14 days

--- _infra/terraform/modules/sqs/main.tf
+++ _infra/terraform/modules/sqs/main.tf
@@ -2,9 +2,10 @@ module "sqs" {
   source  = "terraform-aws-modules/sqs/aws"
   version = "4.0.0"
 
-  name       = each.key
-  fifo_queue = lookup(each.value, "fifo_queue", var.queues)
-  tags       = local.all_resources_tags
+  name                      = each.key
+  fifo_queue                = lookup(each.value, "fifo_queue", var.queues)
+  tags                      = local.all_resources_tags
+  message_retention_seconds = lookup(each.value, "message_retention_seconds", var.queues)
 
   for_each = var.queues
 }

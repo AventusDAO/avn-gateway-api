4580f7115bf1e7f125df7a9c1f6933ded0571805
Micael Franco <micael.franco@aventus.io>
2023-01-20 12:11:39 +0000

    remove references to payout stakers in the avn-gateway

--- avn-api/lib/send.js
+++ avn-api/lib/send.js
@@ -159,26 +159,6 @@ function withdrawUnlocked(api, queryApi) {
   };
 }
 
-function payoutStakers(api, queryApi) {
-  return async function (relayer, era) {
-    common.validateAccount(relayer);
-
-    if (!era) {
-      era = await queryApi.getActiveEra();
-
-      if (era === 0) {
-        throw new Error('You must wait for at least 1 era to pass before calling this method. Current era index: ', era);
-      }
-
-      era = era - 1; // the default is to payout the previous era because the current one won't be ready yet.
-    }
-    common.validateNumber(era);
-    const methodArgs = { era };
-
-    return await this.proxyRequest(api, queryApi, relayer, methodArgs, TX_TYPE.ProxyPayoutStakers, NONCE_TYPE.Staking);
-  };
-}
-
 function generateFunction(functionName, api, queryApi) {
   return functionName(api, queryApi);
 }

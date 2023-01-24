e9d20feec8eb84da1857d22af2f26f9981c488a2
Vucomir Ianculov <2032768+vukomir@users.noreply.github.com>
2023-01-24 15:53:41 +0200

    Sys 3058 create lambda infrastructure terraform (#485)
    * SYS-3058 moved policy to separate file
    
    * SYS-3058 added RDS to TestNet
    
    * SYS-3058 added output from rds
    
    * SYS-3058 added splitfee lambda functions and policy
    
    * SYS-3058 updated outputs from RDS
    
    * SYS-3058 removed unused policy
    
    * SYS-3058 updated GH workflow
    
    * SYS-3058 added vault integration
    
    * SYS-3058 updated SQS retention to 14 days
    
    * SYS-3058 removed vault from lambda terraform infrastructure

---
+++ _infra/terraform/environments/testnet/gateway/rds.tf
@@ -0,0 +1,6 @@
+module "gateway_rds" {
+  source = "../../../modules/rds"
+
+  subnet_ids = data.terraform_remote_state.vpc.outputs.private_subnets
+  vpc_id     = data.terraform_remote_state.vpc.outputs.vpc_id
+}

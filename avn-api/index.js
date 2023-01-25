58012fa12996f10acc5a0e6b711350c052b16063
Nahu Seyoum <nahu.seyoum@artos.io>
2023-01-25 13:07:13 +0000

    code review comments

--- avn-api/index.js
+++ avn-api/index.js
@@ -30,10 +30,7 @@ AvnApi.prototype.init = async function () {
 
   // TODO: do we want to allow changing SURI on the fly?
   const getSuri = () => {
-    if (!this.options.suri && process.env.AVN_SURI) {
-        this.options.suri = process.env.AVN_SURI;
-    }
-
+    this.options.suri = this.options.suri ?? process.env.AVN_SURI;
     return this.options.suri;
   };
 

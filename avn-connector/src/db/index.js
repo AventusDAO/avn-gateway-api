9f7c536e123248473336b97148dfa350cb3d572d
Nahu Seyoum <nahu.seyoum@artos.io>
2023-01-26 17:45:43 +0000

    Code review comments

--- avn-connector/src/db/index.js
+++ avn-connector/src/db/index.js
@@ -4,12 +4,12 @@ const typeorm = require("typeorm");
 async function init() {
   const dataSource = new typeorm.DataSource({
     type: "postgres",
-    host: config.postgress.host,
-    port: config.postgress.port,
-    username: config.postgress.username,
-    password: config.postgress.password,
-    database: config.postgress.database,
-    synchronize: config.postgress.synchronize,
+    host: config.postgres.host,
+    port: config.postgres.port,
+    username: config.postgres.username,
+    password: config.postgres.password,
+    database: config.postgres.database,
+    synchronize: config.postgres.synchronize,
     entities: [
       require("./entity/payer"),
       require("./entity/splitFeeUser"),

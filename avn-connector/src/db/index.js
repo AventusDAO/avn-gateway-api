aee35a9b0fed2849b306348aea7e68bab58f6cfb
nahuseyoum <39748285+nahuseyoum@users.noreply.github.com>
2023-02-01 13:54:58 +0000

    SYS-3093 Update fees logic and tests (#511)

--- avn-connector/src/db/index.js
+++ avn-connector/src/db/index.js
@@ -1,9 +1,27 @@
 const config = require('multiconfig').load();
 const typeorm = require("typeorm");
+const { IsNull } = require("typeorm");
+const { isHex, u8aToHex } = require('@polkadot/util');
+const { decodeAddress, encodeAddress } = require('@polkadot/util-crypto');
 
 const SPLIT_FEE_USER_TABLE = 'splitFeeUser';
 const FEE_TABLE = 'fee';
 const RELAYER_TABLE = 'relayer';
+
+const transactionTypes = {
+  proxyAvtTransfer: 'proxyAvtTransfer',
+  proxyTokenTransfer: 'proxyTokenTransfer',
+  proxyConfirmTokenLift: 'proxyConfirmTokenLift',
+  proxyTokenLower: 'proxyTokenLower',
+  proxyMintSingleNft: 'proxyMintSingleNft',
+  proxyListNftOpenForSale: 'proxyListNftOpenForSale',
+  proxyTransferFiatNft: 'proxyTransferFiatNft',
+  proxyCancelListFiatNft: 'proxyCancelListFiatNft',
+  proxyStakeAvt: 'proxyStakeAvt',
+  proxyIncreaseStake: 'proxyIncreaseStake',
+  proxyUnstake: 'proxyUnstake',
+  proxyWithdrawUnlocked: 'proxyWithdrawUnlocked',
+}
 
 let dataSource;
 
@@ -20,7 +38,9 @@ async function init() {
       require("./entity/payer"),
       require("./entity/splitFeeUser"),
       require("./entity/transaction"),
-      require("./entity/payerTransaction")
+      require("./entity/payerTransaction"),
+      require("./entity/fee"),
+      require("./entity/relayer")
     ],
   });
 
@@ -31,9 +51,8 @@ async function init() {
 
 async function getPayer(user, payer) {
   const userPublicKey = getPublicKey(user);
-  const payerPublicKey = getPublicKey(payer);
 
-  if (!userPublicKey && !payerPublicKey) return undefined;
+  if (!userPublicKey && !payer) return undefined;
 
   const userDataSource = await dataSource.getRepository(SPLIT_FEE_USER_TABLE);
 
@@ -41,7 +60,8 @@ async function getPayer(user, payer) {
   if (!splitFeeUser) return undefined;
 
   // This check is useful when we start supporting multiple payers for the same user.
-  if (payerPublicKey) {
+  if (payer) {
+    const payerPublicKey = getPublicKey(payer);
     return splitFeeUser.payer.publicKey === payerPublicKey ? encodeAddress(payerPublicKey, 42) : undefined;
   }
 
@@ -49,62 +69,91 @@ async function getPayer(user, payer) {
 }
 
 // This function will return the fee as a string
-async function getFees(relayerAddress, user, txId) {
+async function getFees(relayerAddress, user, transactionName) {
+  let userPk;
+
   const relayer =  await getRelayer(relayerAddress);
   if (!relayer) throw new Error(`Relayer (${relayerAddress}) cannot be found.`);
+  if (!relayer.defaultFee) throw new Error(`Relayer  ${relayerAddress} does not have a default fee set`);
 
-  const userPk = getPublicKey(user);
   const feeDataSource = await dataSource.getRepository(FEE_TABLE);
 
-  let feeRecord;
-
-  if (userPk && txId) {
-    // check if there is a custom fee for transaction and user
-    feeRecord = await feeDataSource.findOne(
-      { where: {
-          relayerId: relayer.id,
-          transactionId: txId,
-          userPublicKey: userPk,
-          enabled: true
-        }
-      }
-    );
+  if (user) {
+    userPk = getPublicKey(user);
   }
 
-  // check if there is a specific fee for that transaction instead
-  if (!feeRecord && txId) {
-      feeRecord = await feeDataSource.findOne(
-          { where: { relayerId: relayer.id, transactionId: txId, enabled: true}}
-      );
+  if (transactionName) {
+      return await getSingleFee(feeDataSource, relayer, userPk, transactionName);
   }
 
-  // check if there is a default fee for the given relayer
-  if (!feeRecord) {
-      feeRecord = await feeDataSource.findOne(
-          { where: { relayerId: relayer.id, enabled: true}}
-      );
-  }
-
-  // There must be at least 1 fee entry for each relayer
-  if (!feeRecord) throw new Error(`Relayer fee cannot be found for relayer: ${relayerAddress}, user: ${user} and tx: ${txId}`);
-
-  return feeRecord.fee;
+  return await getAllFees(feeDataSource, relayer, userPk);
 }
 
-async function getRelayer(relayerId) {
-  const relayerPk = getPublicKey(relayerId);
+
+async function getRelayer(relayerAddress) {
+  if (!relayerAddress) return undefined;
+
+  const relayerPk = getPublicKey(relayerAddress);
   const relayerDataSource = await dataSource.getRepository(RELAYER_TABLE);
-  return await relayerDataSource.findOne({ where: { id: relayerPk, enabled: true }});
+  return await relayerDataSource.findOne({ where: { publicKey: relayerPk, enabled: true }});
+}
+
+// This function expects that each relayer has at least one default fee or a fee for each transaction type.
+function buildFeesJson(dbResult, relayerDefaultFee) {
+  let defaultFee;
+  let relayerFees = {};
+
+  (dbResult || []).forEach(r => {
+      const hasTransactionFee = !!r.transaction && !!r.transaction.name;
+      if (hasTransactionFee) {
+          // Prioritise user specific fees over default fees for transaction
+          if (!relayerFees[r.transaction.name] || r.userPublicKey) {
+            relayerFees[r.transaction.name] = r.fee;
+          }
+      } else if (r.userPublicKey) {
+          // This is the default fee for the user
+          defaultFee = r.fee;
+      }
+  });
+
+  defaultFee = defaultFee ?? relayerDefaultFee;
+  Object.values(transactionTypes).forEach(v => relayerFees[v] = relayerFees[v] ?? defaultFee);
+
+  return relayerFees;
+}
+
+async function getSingleFee(feeDataSource, relayer, userPk, transactionName) {
+  let feeRow = await feeDataSource.findOne(
+      { where: {
+          relayerId: relayer.id,
+          userPublicKey: userPk || IsNull(),
+          enabled: true,
+          transaction: { name: transactionName, enabled: true }
+      }
+  });
+
+  return feeRow ? feeRow.fee : relayer.defaultFee;
+}
+
+async function getAllFees(feeDataSource, relayer, userPk) {
+  const fees = await feeDataSource.find(
+      { where: {
+          relayerId: relayer.id,
+          userPublicKey: userPk || IsNull(),
+          enabled: true
+  }});
+
+  return buildFeesJson(fees, relayer.defaultFee);
 }
 
 function getPublicKey(account) {
-  if (!account) return undefined;
-  if (isHex(account) && account.length != 66) return undefined;
+  if (!account) throw new Error(`Address is NULL`);
+  if (isHex(account) && account.length != 66) throw new Error(`Invalid hex encoded address ${account}`);
 
   try {
       return u8aToHex(decodeAddress(account));
   } catch (err) {
-      return undefined;
+      throw new Error(`Invalid address ${account}: ${err.toString()}`)
   }
 }
 

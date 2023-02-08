23732d6c7565bf4e549eadeec192a8fdc67e992c
Rui <m.rui.s.magalhaes@gmail.com>
2023-02-08 21:01:26 +0000

    Fix sandbox env

--- _infra/terraform/environments/sandbox/gateway/sqs.tf
+++ _infra/terraform/environments/sandbox/gateway/sqs.tf
@@ -4,12 +4,12 @@ module "gateway_sqs" {
   queues = {
     gateway_default_queue = {
       fifo_queue                = true
-      message_retention_seconds = 1209600
+      message_retention_seconds = 36000
     }
 
     gateway_payer_queue = {
       fifo_queue                = true
-      message_retention_seconds = 1209600
+      message_retention_seconds = 36000
     }
   }
 }

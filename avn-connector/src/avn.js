fbf025079d55bdca709a7542bb3eec1cd7044309
Micael Franco <micael.franco@aventus.io>
2023-02-25 01:39:27 +0000

    dbg

--- avn-connector/src/avn.js
+++ avn-connector/src/avn.js
@@ -161,10 +161,11 @@ async function getStakingStats() {
       //   totalStaked = totalStaked.add(bondTotal);
       //   numActiveStakes++;
       // }
-      console.log(`STAKER: ${JSON.stringify(staker, null, 2)}`)
-      console.log(`STAKER: ${JSON.stringify(staker[1], null, 2)}`)
-      console.log(`STAKER: ${JSON.stringify(staker[1]['total'], null, 2)}`)
-      maxtotal = maxtotal.add(new BN(staker[1].total));
+      let parsed = JSON.parse(staker)
+      console.log(`STAKER: ${JSON.stringify(parsed, null, 2)}`)
+      console.log(`STAKER: ${JSON.stringify(parsed[1], null, 2)}`)
+      console.log(`STAKER: ${JSON.stringify(parsed[1]['total'], null, 2)}`)
+      maxtotal = maxtotal.add(new BN(parsed[1].total));
       ts++;
     })
     // stakersData.data.forEach( stake => {

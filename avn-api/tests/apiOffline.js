b769ce148d558919d2cfe4daaf31502904a25424
fluorostani <glyn.dimond@artos.io>
2023-03-20 11:43:17 +0000

    SYS-3182 Pre-1.0.0-release fixes

---
+++ avn-api/tests/apiOffline.js
@@ -0,0 +1,15 @@
+const AvnApi = require('../index.js');
+const assert = require('chai').assert;
+const helper = require('./helper.js');
+
+describe('Access rights:', async () => {
+  let api;
+
+  describe('Offline mode', async () => {
+    it('api is initializable', async () => {
+      const api = new AvnApi();
+      await api.init();
+      console.log(api);
+    });
+  });
+});

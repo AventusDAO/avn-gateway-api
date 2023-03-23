0159c418d465855405a346ff3657f5b1b9b2d29d
fluorostani <glyn.dimond@artos.io>
2023-03-23 17:00:31 +0000

    Update accessTests.js

--- avn-api/tests/accessTests.js
+++ avn-api/tests/accessTests.js
@@ -50,8 +50,7 @@ describe('Access rights:', async () => {
 
     it('can set SURI via the options', async () => {
       const options = { suri: newUserSURI, relayer: relayer };
-      const apiWithOptions = new AvnApi(null, options);
-      await apiWithOptions.init();
+      const apiWithOptions = helper.avnApi(options);
       assert.equal(apiWithOptions.myAddress(), newUser);
       assert.equal(apiWithOptions.signer().address, newUser);
     });

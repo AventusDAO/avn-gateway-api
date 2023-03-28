7b0ee6e6d014c3ced995c938ef607e16cd6332ce
Nahu Seyoum <nahu.seyoum@artos.io>
2023-03-28 11:36:48 +0100

    Update params

--- avn-gateway/tx-dispatch-handler.js
+++ avn-gateway/tx-dispatch-handler.js
@@ -467,7 +467,7 @@ async function processProxyStakeAvt(call, request, requestId) {
     if (utils.isValidArray(call.params.targets) === false || call.params.targets.length === 0) throw 'targets';
     if (utils.isValidAmount(amount) === false) throw 'amount';
     if (utils.isValidProxySignature(proxySignature, user, signData) === false) throw 'proxy signature';
-  } catch (errParam) {
+  } catch (param) {
     return utils.buildErrorBody('params', 'invalid ' + param, param, request, call.id);
   }
 

f2227f9d2f5e215cd09e57dde3a7755587202105
Leonardo Baiao <57899945+lbaiao2019@users.noreply.github.com>
2023-03-31 09:50:35 +0100

    DEVOPS-171 Fix SQS Queues (#589)

--- _infra/terraform/modules/sqs/main.tf
+++ _infra/terraform/modules/sqs/main.tf
@@ -7,8 +7,14 @@ module "sqs" {
   tags                          = local.all_resources_tags
   message_retention_seconds     = lookup(each.value, "message_retention_seconds", var.queues)
   visibility_timeout_seconds    = lookup(each.value, "visibility_timeout_seconds", var.queues)
-  create_dlq                    = lookup(each.value, "create_dlq", var.queues)
   dlq_message_retention_seconds = lookup(each.value, "dlq_message_retention_seconds", var.queues)
+  receive_wait_time_seconds     = lookup(each.value, "receive_wait_time_seconds", 0)
+
+  create_dlq                    = lookup(each.value, "create_dlq", var.queues)
+
+  redrive_policy = {
+    maxReceiveCount = lookup(each.value, "max_receive_count", 5)
+  }  
 
   for_each = var.queues
 }

bb105d002d09db710f543b17640ee47a1d013e0f
Leonardo Baiao <57899945+lbaiao2019@users.noreply.github.com>
2023-04-11 10:05:59 +0100

    DEVOPS-171 (testnet) Fix SQS queue configuration (#591)

--- _infra/terraform/environments/testnet/gateway/sqs.tf
+++ _infra/terraform/environments/testnet/gateway/sqs.tf
@@ -4,19 +4,22 @@ module "gateway_sqs" {
   queues = {
     gateway_default_queue = {
       fifo_queue                    = true
-      message_retention_seconds     = 900
-      visibility_timeout_seconds    = 180
+      message_retention_seconds     = 86400
+      visibility_timeout_seconds    = 60
       create_dlq                    = true
       dlq_message_retention_seconds = 1209600
-
+      receive_wait_time_seconds     = 10
+      max_receive_count             = 3
     }
 
     gateway_payer_queue = {
       fifo_queue                    = true
-      message_retention_seconds     = 900
-      visibility_timeout_seconds    = 180
+      message_retention_seconds     = 86400
+      visibility_timeout_seconds    = 60
       create_dlq                    = true
       dlq_message_retention_seconds = 1209600
+      receive_wait_time_seconds     = 10
+      max_receive_count             = 3      
     }
   }
 }

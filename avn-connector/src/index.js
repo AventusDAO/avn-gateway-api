947f8ba6f7fe0c7d64c951b0b2c38d84b2d1c7ea
Nahu Seyoum <nahu.seyoum@artos.io>
2023-04-14 16:28:00 +0100

    adds missing await

--- avn-connector/src/index.js
+++ avn-connector/src/index.js
@@ -225,7 +225,7 @@ app.post('/isPayerTransaction', async (req, res, next) => {
 app.post('/addNewTransactionStatus', async (req, res, next) => {
   try {
     log.trace({ addNewTransactionStatus: JSON.stringify(req.body) });
-    avn.addNewTransaction(req.body.requestId);
+    await avn.addNewTransaction(req.body.requestId);
     res.status(200).send({});
   } catch (err) {
     next(err);
@@ -235,7 +235,7 @@ app.post('/addNewTransactionStatus', async (req, res, next) => {
 app.post('/setTransactionRefusedByPayerStatus', async (req, res, next) => {
   try {
     log.trace({ setTransactionRefusedByPayerStatus: JSON.stringify(req.body) });
-    avn.setSendingFailedStatus(req.body.requestId, redis.transactionStatus.PayerRefused);
+    await avn.setSendingFailedStatus(req.body.requestId, redis.transactionStatus.PayerRefused);
     res.status(200).send({});
   } catch (err) {
     next(err);
@@ -245,7 +245,7 @@ app.post('/setTransactionRefusedByPayerStatus', async (req, res, next) => {
 app.post('/setTransactionFailedToBeSentStatus', async (req, res, next) => {
   try {
     log.trace({ setTransactionFailedToBeSentStatus: JSON.stringify(req.body) });
-    avn.setSendingFailedStatus(req.body.requestId, redis.transactionStatus.SendingFailed);
+    await avn.setSendingFailedStatus(req.body.requestId, redis.transactionStatus.SendingFailed);
     res.status(200).send({});
   } catch (err) {
     next(err);

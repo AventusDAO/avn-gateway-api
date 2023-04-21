e1f015c9976c0e762eff8a7ea7545a433b016d9c
fluorostani <glyn.dimond@artos.io>
2023-04-21 18:01:26 +0100

    Update loadTest.js

--- avn-api/tests/loadTest.js
+++ avn-api/tests/loadTest.js
@@ -24,7 +24,7 @@ describe('LOAD TEST', async () => {
     for (let j=0; j < RUNS; j++) {
       const reqId = await api.send.transferAvt(RECIPIENT, 1);
       requestIds.push(reqId);
-      console.log(`Tx: ${j} Request Id: ${reqId}\t${new Date().toString().substring(16,25)}`);
+      console.log(`Tx: ${j} Request Id: ${reqId}\t\t${new Date().toString().substring(16,25)}`);
     }
 
     const timeEndSend = Date.now();
@@ -68,7 +68,7 @@ async function confirmStatus(api, id, requestId, expectedStatus, optionalTimeout
       response = await api.poll.requestState(requestId);
       status = response.status;
       if (status === 'Processed' || status === 'Rejected' ) {
-        console.log(`Tx: ${id} Request Id: ${requestId} Status: ${status}, relayer nonce: ${response.senderNonce}, block: ${response.blockNumber}, index ${response.transactionIndex}\t${new Date().toString().substring(16,25)}`);
+        console.log(`Tx: ${id} Request Id: ${requestId} Status: ${status}, relayer nonce: ${response.senderNonce}, block: ${response.blockNumber}, index ${response.transactionIndex}\t\t${new Date().toString().substring(16,25)}`);
         if (id === 0) relayerNonceStart = response.senderNonce - 1;
         relayerNonceEnd = response.senderNonce;
         return status === expectedStatus;

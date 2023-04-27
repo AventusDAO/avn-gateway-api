826d0303e788d9e2ca0d5c200a58f41ccf00655d
fluorostani <glyn.dimond@artos.io>
2023-04-27 11:41:28 +0100

    Update redis.js

--- avn-connector/src/redis.js
+++ avn-connector/src/redis.js
@@ -408,7 +408,7 @@ module.exports = {
   addFailedAvnTransaction,
   getAvnTransaction,
   lockNonce,
-  releaseNonceLock
+  releaseNonceLock,
   lockPayerNonce,
   releasePayerNonceLock,
   getNextNonce,

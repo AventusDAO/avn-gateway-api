a9f8fafa67b3d7f69eeb79604730a4e60e57f789
fluorostani <glyn.dimond@artos.io>
2023-04-28 16:31:14 +0100

    Update redis.js

--- avn-connector/src/redis.js
+++ avn-connector/src/redis.js
@@ -223,12 +223,14 @@ function buildTransactionJson(senderAddress, senderNonce, status) {
 async function lock(address) {
   let isLocked = 0;
   while (isLocked === 0) {
+    log.trace(`LOCKKKKKKKKKKK ${address}`);
     isLocked = await redisClient.setnx(LOCK_NAMESPACE + address, 1);
   }
   return;
 }
 
 async function unlock(address) {
+  log.trace(`UNNNNNNNNNNLOCKKKKKKKKKKK ${address}`);
   await redisClient.del(LOCK_NAMESPACE + address);
 }
 

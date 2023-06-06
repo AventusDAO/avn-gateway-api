4fbfadfd4fe420dd68f18b60e8bca1f720aa700a
fluorostani <glyn.dimond@artos.io>
2023-06-06 16:02:13 +0100

    Update lowering.js

--- avn-connector/src/lowering.js
+++ avn-connector/src/lowering.js
@@ -47,7 +47,7 @@ async function retrieveLatestLowerTransactions(latestPublishedBlock) {
   let retrieveFromBlock = await redis.getRetrieveLowersFromAvnBlock();
   const lowerTransactions = await getLowerTransactions(retrieveFromBlock);
 
-  console.log(`\tChecking for lowers from block ${retrieveFromBlockk} - found ${lowerTransactions.length}`);
+  console.log(`\tChecking for lowers from block ${retrieveFromBlock} - found ${lowerTransactions.length}`);
   for (let i = 0; i < lowerTransactions.length; i++) {
     const lowerTx = lowerTransactions[i];
     const txHash = lowerTx.txHash;

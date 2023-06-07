2cb11a72b2d9804a399774728bf6026eb8c05e4b
fluorostani <glyn.dimond@artos.io>
2023-06-07 17:31:26 +0100

    Update lowering.js

--- avn-connector/src/lowering.js
+++ avn-connector/src/lowering.js
@@ -207,7 +207,7 @@ async function getLowerDataFromIndexer(txHashes) {
 
     const query = `query ConnectorLowerTxData {
       events( where: { extrinsic: { hash_in: ${JSON.stringify(txHashes)} }, name_in: ${JSON.stringify(extrinsicsFilter)} },
-      limit: ${eventsLimit}, orderBy: block_height_ASC) { name args extrinsic { hash indexInBlock block { height } } } }`;
+      limit: ${eventsLimit}, orderBy: [id_ASC) { name args extrinsic { hash id indexInBlock block { height } } } }`;
 
     const response = await axios.post(AVN_EXPLORER_URL, { query: query, operationName: 'ConnectorLowerTxData' });
     const events = response.data.data.events;

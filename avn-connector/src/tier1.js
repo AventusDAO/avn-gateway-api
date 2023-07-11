56e496e791d2f6f95c2ef82e85d05a530fb0a98a
fluorostani <39763263+fluorostani@users.noreply.github.com>
2023-07-11 16:30:59 +0100

    SYS-3488 Part 1 - Rename "ethereum" files to "tier1" (#649)
    * SYS-3488 Part 2 - Renames after file replacement (#650)
    
    * SYS-3488 Part 3 - Update config - remove etherscan, replace "infura" with "tier1 provider" (#651)
    
    * SYS-3488 Part 4 - Update Redis (#652)
    
    * SYS-3488 Part 5 - Replace tier1 setup (#653)
    
    * SYS-3488 Part 6 - Update getLockedBalance and getLatestClaimedLowers (#654)
    
    * SYS-3488 Part 7 Update getLiftEvents and getPublishedRoots (#655)

---
+++ avn-connector/src/tier1.js
@@ -0,0 +1,94 @@
+const redis = require('./redis');
+const config = require('multiconfig').load();
+const { ethers } = require('ethers');
+const provider = new ethers.providers.JsonRpcProvider(config.tier1.tier1_provider_url);
+const log4js = require('log4js');
+const log = log4js.getLogger();
+
+const EVM_TOKEN = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee';
+const MAX_LIFT_AGE_IN_BLOCKS = 60 * 60 * 24 * 5 / 12; // ~5 days @ ~12 secs per block
+const REQUIRED_CONFIRMATION_BLOCKS = 20;
+
+const EVENT_SIG = {
+  LIFT: ethers.utils.id('LogLifted(address,address,bytes32,uint256)'),
+  LOWER: ethers.utils.id('LogLowered(address,address,bytes32,uint256)'),
+  ROOT: ethers.utils.id('LogRootPublished(bytes32,uint256)')
+}
+
+async function getLockedBalance(avnContract, tokenAddress) {
+  let balance = 0;
+
+  try {
+    if (tokenAddress.toLowerCase() === EVM_TOKEN) {
+      balance = await provider.getBalance(avnContract);
+    } else {
+      const abi = 'function balanceOf(address) view returns (uint256)';
+      const tokenContract = new ethers.Contract(tokenAddress, abi, provider);
+      balance = await tokenContract.balanceOf(avnContract);
+    }
+  } catch (error) {
+    log.error('Error getting locked balance:', error);
+  }
+
+  return balance.toString();
+}
+
+async function getLiftEvents(avnContract) {
+  let fromBlock = 0, toBlock = 0;
+  const liftEvents = [];
+
+  try {
+    const currentBlock = await provider.getBlockNumber();
+    const fromBlock = (await redis.getLiftsFromTier1Block()) || currentBlock - MAX_LIFT_AGE_IN_BLOCKS;
+    const toBlock = currentBlock - REQUIRED_CONFIRMATION_BLOCKS;
+
+    if (fromBlock <= toBlock) {
+      const events = await provider.getLogs({ address: avnContract, topics: [EVENT_SIG.LIFT], fromBlock, toBlock });
+      events.forEach(event => liftEvents.push([EVENT_SIG.LIFT, event.transactionHash]));
+    }
+  } catch (error) {
+    log.error('Error getting lift events:', error);
+  }
+
+  return { fromBlock, toBlock, liftEvents };
+}
+
+async function getLatestClaimedLowers(avnContract) {
+  let fromBlock = 0;
+  const claimedLowers = [];
+
+  try {
+    fromBlock = await redis.getClaimedLowersFromTier1Block();
+    const events = await provider.getLogs({ address: avnContract, topics: [EVENT_SIG.LOWER], fromBlock, toBlock: 'latest' });
+    if (events.length > 0) fromBlock = events[events.length - 1].blockNumber + 1;
+
+    for await (const txHash of events.map(event => event.transactionHash)) {
+      const txData = await provider.getTransaction(txHash);
+      const inputs = ethers.utils.defaultAbiCoder.decode(['bytes','bytes32[]'], ethers.utils.hexDataSlice(txData.data, 4));
+      claimedLowers.push(ethers.utils.keccak256(inputs[0]));
+    }
+  } catch (error) {
+    log.error('Error getting claimed lowers:', error);
+  }
+
+  return { claimedLowers, fromBlock };
+}
+
+async function getPublishedRoots(avnContract) {
+  let events = [];
+
+  try {
+    events = await provider.getLogs({ address: avnContract, topics: [EVENT_SIG.ROOT], fromBlock: 0, toBlock: 'latest' });
+  } catch (error) {
+    log.error('Error getting published roots:', error);
+  }
+
+  return events.map(event => event.topics[1].toLowerCase()); // topic 1 = rootHash
+}
+
+module.exports = {
+  getLatestClaimedLowers,
+  getLiftEvents,
+  getLockedBalance,
+  getPublishedRoots
+};

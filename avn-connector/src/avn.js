a47339f2ac7973a2f563e7e3fe1b3c6404a091a0
Micael Franco <micael.franco@aventus.io>
2023-07-11 14:43:32 +0100

    Parse collators

--- avn-connector/src/avn.js
+++ avn-connector/src/avn.js
@@ -156,6 +156,8 @@ async function getCollatorsToNominate() {
   // if (collators === undefined) {
   let collators = await api.query.parachainStaking.selectedCandidates();
 
+  collators = JSON.parse(collators);
+
   log.trace(`COLLATORS -- ${JSON.stringify(collators, null, 2)} -- END COLLATORS`);
 
   await redis.setCollatorsToNominate(collators);

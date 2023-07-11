8c0a67758916419455bec5218bdf2ed87f4327f2
fluorostani <glyn.dimond@artos.io>
2023-07-11 09:29:59 +0100

    Update tier1.js

--- avn-connector/src/tier1.js
+++ avn-connector/src/tier1.js
@@ -60,7 +60,7 @@ async function getLatestClaimedLowers(avnContract) {
 
   try {
     fromBlock = await redis.getCheckClaimedLowersFromAvnBlock();
-    console.log("FROMBLOCK", fromBlock)
+    fromBlock = 9242752
     const events = await provider.getLogs({ address: avnContract, topics: [EVENT_SIG.LOWER], fromBlock, toBlock: 'latest' });
     console.log("EVENTS", events)
     if (events.length > 0) fromBlock = events[events.length - 1].blockNumber + 1;

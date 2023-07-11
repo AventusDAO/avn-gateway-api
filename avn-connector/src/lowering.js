08b0ff32bf85384954c0481783048f5f9389841e
fluorostani <39763263+fluorostani@users.noreply.github.com>
2023-07-11 16:30:17 +0100

    SYS-3488 Part 3 - Update config - remove etherscan, replace "infura" with "tier1 provider" (#651)
    * SYS-3488 Part 4 - Update Redis (#652)
    
    * SYS-3488 Part 5 - Replace tier1 setup (#653)
    
    * SYS-3488 Part 6 - Update getLockedBalance and getLatestClaimedLowers (#654)
    
    * SYS-3488 Part 7 Update getLiftEvents and getPublishedRoots (#655)

--- avn-connector/src/lowering.js
+++ avn-connector/src/lowering.js
@@ -134,7 +134,7 @@ async function updateAwaitingClaimDataLowers() {
 }
 
 async function updateUnclaimedLowers(avnContract, account) {
-  const { claimedLowers, nextFromBlock } = await tier1.getLatestClaimedLowers(avnContract);
+  const { claimedLowers, fromBlock } = await tier1.getLatestClaimedLowers(avnContract);
   const unclaimed = await redis.getUnclaimedLowers();
   let claimed = 0;
 
@@ -152,7 +152,7 @@ async function updateUnclaimedLowers(avnContract, account) {
 
   console.log(`\tRecently claimed: ${claimed} `);
   console.log(`\tPublished but unclaimed: ${unclaimed.length - claimed} `);
-  await redis.setCheckClaimedLowersFromAvnBlock(nextFromBlock);
+  await redis.setClaimedLowersFromTier1Block(fromBlock);
 }
 
 async function getLowerTransactions(fromBlock) {

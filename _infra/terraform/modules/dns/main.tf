4f712d4c07181d4cb6ebf20cd714dbaf5fbe4a38
Rui <m.rui.s.magalhaes@gmail.com>
2023-08-08 18:25:15 +0100

    custom domain typo fix

--- _infra/terraform/modules/dns/main.tf
+++ _infra/terraform/modules/dns/main.tf
@@ -133,8 +133,8 @@ resource "aws_route53_record" "api_gateway" {
   type    = "A"
 
   alias {
-    name                   = aws_apigatewayv2_domain_name.api_gateway.domain_name_configuration[0].target_domain_name
-    zone_id                = aws_apigatewayv2_domain_name.api_gateway.domain_name_configuration[0].hosted_zone_id
+    name                   = aws_apigatewayv2_domain_name.api_gateway[0].domain_name_configuration[0].target_domain_name
+    zone_id                = aws_apigatewayv2_domain_name.api_gateway[0].domain_name_configuration[0].hosted_zone_id
     evaluate_target_health = false
   }
 }

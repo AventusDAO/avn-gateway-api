288c3bf36d86f1cc6fca02e6ccf0af4cd2a7ba70
Rui <m.rui.s.magalhaes@gmail.com>
2023-08-09 19:29:27 +0100

    change connector fucntion to invoke lambda based on a env var

--- avn-connector/src/lambdas.js
+++ avn-connector/src/lambdas.js
@@ -2,7 +2,7 @@ const config = require('multiconfig').load();
 const { LambdaClient, InvokeCommand } = require('@aws-sdk/client-lambda');
 const log = require('log4js').configure(config.log4Js).getLogger();
 
-const lambdaFunctionName = config.statusLambdaName;
+const lambdaFunctionName = config.lambdas.statusLambdaName;
 
 async function resolvePendingTransactionsState() {
   log.trace(`Invoking status-update-handler lambda`);

d69b6f380e405f49b9b30f76be2eb290f50e6e89
Rui <m.rui.s.magalhaes@gmail.com>
2023-08-22 18:41:43 +0100

    remove testnet api gateway and lambdas

--- _infra/terraform/environments/mainnet/gateway/main.tf
+++ _infra/terraform/environments/mainnet/gateway/main.tf
@@ -4,10 +4,6 @@ locals {
   cluster_version        = "1.21"
   eks_node_size          = 50
   account_id             = "503742778456"
-  avn_connector_endpoint = "http://avn-connector.${local.environment}.aventus.internal/"
-  avn_votes_bucket       = "avn-votes-mainnet"
-  block_explorer_url     = "https://archive-explorer.mainnet.aventus.io/graphql"
-  vault_recovery_window  = 30
 }
 
 module "dns" {

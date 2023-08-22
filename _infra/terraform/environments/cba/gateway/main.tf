130fe70ca2dd09cb411f8b89f31f3e4f985d4200
Rui <m.rui.s.magalhaes@gmail.com>
2023-08-22 18:39:38 +0100

    remove mainnet api gateway and lambdas

--- _infra/terraform/environments/cba/gateway/main.tf
+++ _infra/terraform/environments/cba/gateway/main.tf
@@ -13,9 +13,9 @@ locals {
 module "dns" {
   source = "../../../modules/dns"
 
-  vpc_id                           = data.terraform_remote_state.vpc.outputs.vpc_id
-  parachain_vpc_id                 = data.terraform_remote_state.parachain_dev.outputs.vpc_id
-  environment                      = local.environment
+  vpc_id           = data.terraform_remote_state.vpc.outputs.vpc_id
+  parachain_vpc_id = data.terraform_remote_state.parachain_dev.outputs.vpc_id
+  environment      = local.environment
 
   providers = {
     aws         = aws

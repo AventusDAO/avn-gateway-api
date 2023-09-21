e5951ce6eb46839334b1f98c128c60d744d09633
Rui Dos Santos Magalhães <45366558+RuiSMagalhaes@users.noreply.github.com>
2023-09-21 16:22:57 +0100

    old gateway RDS destruction (#694)

--- _infra/terraform/environments/testnet/gateway/rds.tf
+++
@@ -1,7 +0,0 @@
-module "gateway_rds" {
-  source = "../../../modules/rds"
-
-  subnet_ids               = data.terraform_remote_state.vpc.outputs.private_subnets
-  vpc_id                   = data.terraform_remote_state.vpc.outputs.vpc_id
-  parachain_vpc_cidr_block = data.terraform_remote_state.parachain_testnet.outputs.vpc_cidr_block
-}

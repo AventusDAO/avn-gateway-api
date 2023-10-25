0fc0ef1b6d0fe68ce466bf85db32b089dcb931ea
fluorostani <39763263+fluorostani@users.noreply.github.com>
2023-10-25 17:18:53 +0100

    SYS-3704 Inject AVT token into lowering query results (#704)

--- avn-connector/src/lowering.js
+++ avn-connector/src/lowering.js
@@ -12,7 +12,7 @@ const AVN_EXPLORER_URL = config.avnExplorerUrl;
 
 async function getLowers(account) {
   console.log(`\nProcessing lowers`);
-  const { avnContract } = await redis.getChainInfo();
+  const { avnContract } = await avn.getChainInfo();
 
   const latestPublishedBlock = await updateSummaries(avnContract);
   console.log(`\tLatest block published: ${latestPublishedBlock}`);
@@ -168,10 +168,11 @@ async function getLowerTransactions(fromBlock) {
   let newLowers = [];
   let lowers = [];
   let fromId = generateId(fromBlock, 0);
+  const { avtContract } = await avn.getChainInfo();
 
   // Loop to retrieve lowers so as not to exceed the indexer limit:
   do {
-    newLowers = await getLowersFromIndexer(fromId, txLimit);
+    newLowers = await getLowersFromIndexer(fromId, txLimit, avtContract);
     if (newLowers.length > 0) {
       lowers = lowers.concat(newLowers);
       // Update the starting position (lowers are ordered so the last entry is always the most recent):
@@ -182,7 +183,8 @@ async function getLowerTransactions(fromBlock) {
   return lowers;
 }
 
-async function getLowersFromIndexer(fromId, txLimit) {
+async function getLowersFromIndexer(fromId, txLimit, avtContract) {
+
   try {
     const query = `query ConnectorLower { events( where: { name_in:[ "TokenManager.TokenLowered", "TokenManager.AvtLowered"], call: { id_gte: "${fromId}" } },
         limit: ${txLimit}, orderBy: id_ASC) { args extrinsic { hash id indexInBlock block { height } } } }`;
@@ -192,7 +194,7 @@ async function getLowersFromIndexer(fromId, txLimit) {
       txHash: event.extrinsic.hash,
       blockNumber: event.extrinsic.block.height.toString(),
       index: event.extrinsic.indexInBlock.toString(),
-      token: event.args.tokenId,
+      token: event.args.tokenId || avtContract,
       amount: event.args.amount,
       from: event.args.sender,
       to: event.args.t1Recipient

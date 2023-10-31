e52a98db78a907205eba68f210c818469c4e7068
Rui <m.rui.s.magalhaes@gmail.com>
2023-10-31 15:46:45 +0000

    delete dev document db; deleted all eks cluster helm charts

--- _infra/terraform/modules/documentdb/main.tf
+++ _infra/terraform/modules/documentdb/main.tf
@@ -4,7 +4,7 @@ locals {
     password = random_password.docdb_password.result
   }
   subnet_ids         = var.subnet_ids
-  subnet_cidr_blocks = [for k, subnet in data.aws_subnet.subnets: subnet.cidr_block]
+  subnet_cidr_blocks = [for k, subnet in data.aws_subnet.subnets : subnet.cidr_block]
   whitelist_ips      = concat(local.subnet_cidr_blocks, var.additional_whitelist_ips)
 }
 
@@ -21,19 +21,19 @@ resource "random_password" "docdb_password" {
 }
 
 resource "aws_docdb_cluster" "docdb" {
-  cluster_identifier      = "avn-connector"
-  engine                  = "docdb"
-  engine_version          = "4.0.0"
-  master_username         = local.user.username
-  master_password         = local.user.password
-  backup_retention_period = var.backup_retention_period
-  preferred_backup_window = "02:00-04:00"
-  skip_final_snapshot     = false
+  cluster_identifier        = "avn-connector"
+  engine                    = "docdb"
+  engine_version            = "4.0.0"
+  master_username           = local.user.username
+  master_password           = local.user.password
+  backup_retention_period   = var.backup_retention_period
+  preferred_backup_window   = "02:00-04:00"
+  skip_final_snapshot       = false
   final_snapshot_identifier = "avn-connector-docdb-final-snap"
-  apply_immediately       = var.apply_changes_immediately
-  deletion_protection     = var.deletion_protection
-  db_subnet_group_name    = aws_docdb_subnet_group.docdb.id
-  vpc_security_group_ids  = [aws_security_group.documentdb.id]
+  apply_immediately         = var.apply_changes_immediately
+  deletion_protection       = var.deletion_protection
+  db_subnet_group_name      = aws_docdb_subnet_group.docdb.id
+  vpc_security_group_ids    = [aws_security_group.documentdb.id]
 
   tags = {
     Name = "avn-connector"
@@ -62,18 +62,18 @@ resource "aws_security_group" "documentdb" {
   vpc_id      = var.vpc_id
 
   ingress {
-    description      = "MongoDB port from vpc subnets"
-    from_port        = "27017"
-    to_port          = "27017"
-    protocol         = "tcp"
-    cidr_blocks      = local.whitelist_ips
+    description = "MongoDB port from vpc subnets"
+    from_port   = "27017"
+    to_port     = "27017"
+    protocol    = "tcp"
+    cidr_blocks = local.whitelist_ips
   }
 
   egress {
-    from_port        = "0"
-    to_port          = "0"
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = "0"
+    to_port     = "0"
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
@@ -87,7 +87,7 @@ resource "aws_secretsmanager_secret" "documentdb" {
 }
 
 resource "aws_secretsmanager_secret_version" "documentdb" {
-  secret_id     = aws_secretsmanager_secret.documentdb.id
+  secret_id = aws_secretsmanager_secret.documentdb.id
   secret_string = jsonencode(
     merge(local.user, { server = aws_docdb_cluster.docdb.endpoint })
   )

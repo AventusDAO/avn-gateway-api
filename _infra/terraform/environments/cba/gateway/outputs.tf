8320d2d047eb1c6e0dbef503ebb306576a32bcda
Rui Dos Santos Magalhães <45366558+RuiSMagalhaes@users.noreply.github.com>
2023-10-31 15:45:50 +0000

    delete amq dev cluster (#706)

--- _infra/terraform/environments/cba/gateway/outputs.tf
+++ _infra/terraform/environments/cba/gateway/outputs.tf
@@ -15,19 +15,3 @@ output "redis_security_group_id" {
 output "private_hosted_zone_id" {
   value = module.dns.private_hosted_zone_id
 }
-
-#
-# SQS
-#
-
-output "queue_arn" {
-  value = module.gateway_sqs.queue_arn
-}
-
-output "queue_name" {
-  value = module.gateway_sqs.queue_name
-}
-
-output "queue_url" {
-  value = module.gateway_sqs.queue_url
-}

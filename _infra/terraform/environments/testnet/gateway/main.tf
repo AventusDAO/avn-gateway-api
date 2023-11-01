2c5edd75be96672270472831bd0d9792beb201b2
Rui <m.rui.s.magalhaes@gmail.com>
2023-11-01 15:34:05 +0000

    delete amq testnet cluster

--- _infra/terraform/environments/testnet/gateway/main.tf
+++ _infra/terraform/environments/testnet/gateway/main.tf
@@ -16,15 +16,6 @@ module "dns" {
   }
 }
 
-module "rabbitmq" {
-  source = "../../../modules/rabbitmq"
-
-  vpc_id                   = data.terraform_remote_state.vpc.outputs.vpc_id
-  parachain_vpc_cidr_block = data.terraform_remote_state.parachain_testnet.outputs.vpc_cidr_block
-  subnet_ids               = data.terraform_remote_state.vpc.outputs.private_subnets
-  deployment_mode          = "CLUSTER_MULTI_AZ"
-}
-
 module "redis" {
   source = "../../../modules/redis"
 

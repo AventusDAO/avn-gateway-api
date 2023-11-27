e82ddbbff3af2fbd730a9406060cc4cc3fa69fb4
Rui <m.rui.s.magalhaes@gmail.com>
2023-11-27 20:57:01 +0000

    fix terraform plan

--- _infra/terraform/environments/testnet/gateway/data.tf
+++ _infra/terraform/environments/testnet/gateway/data.tf
@@ -10,7 +10,7 @@ data "aws_caller_identity" "current" {}
 data "terraform_remote_state" "parachain_testnet" {
   backend = "s3"
   config = {
-    role_arn = "arn:aws:iam::189013141504:role/jenkins-access"
+    role_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/jenkins-access"
     bucket   = "avn-testnet-tf-state"
     key      = "terraform.state"
     region   = "eu-west-1"

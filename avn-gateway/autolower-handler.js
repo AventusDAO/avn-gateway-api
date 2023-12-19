193f960a3efe2263c23f5a702dd99ef5e340dd9c
fluorostani <glyn.dimond@artos.io>
2023-12-19 13:16:59 +0000

    Update autolower-handler.js

--- avn-gateway/autolower-handler.js
+++ avn-gateway/autolower-handler.js
@@ -4,8 +4,8 @@ const AVN_CONNECTOR_ENDPOINT = process.env.AVN_CONNECTOR_ENDPOINT;
 exports.handler = async _ => {
   try {
     const response = await utils.axios.get(AVN_CONNECTOR_ENDPOINT + 'autolower');
-    console.log(response.data);
+    return console.log(response.data);
   } catch (error) {
-    console.error(error);
+    return console.error(error);
   }
 };

d59dbf2288cc0ce74ed5bb6edcbd2f73bd3dbbe3
nahuseyoum <nahu.seyoum@aventus.io>
2023-12-20 00:46:36 +0000

    temp disable old lower to avoid corrupted data

--- avn-connector/src/index.js
+++ avn-connector/src/index.js
@@ -185,7 +185,7 @@ app.post('/avnNftContractAddresses', async (req, res, next) => {
 app.post('/lowers', async (req, res, next) => {
   try {
     log.trace({ lowerDataRequest: req.body });
-    let oldLowers = await lowering.getLowers(req.body.account);
+    let oldLowers = [];//await lowering.getLowers(req.body.account);
     let newLowers = await loweringV2.getLowers(req.body.account);
     res.send((oldLowers || []).concat(newLowers));
   } catch (err) {

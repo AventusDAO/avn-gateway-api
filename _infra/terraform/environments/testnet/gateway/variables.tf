ff28b7b22db040cfceb873171bd888cc04146716
Rui <m.rui.s.magalhaes@gmail.com>
2024-01-05 16:52:38 +0000

    gateway redis destruction on cba and testnet

--- _infra/terraform/environments/testnet/gateway/variables.tf
+++ _infra/terraform/environments/testnet/gateway/variables.tf
@@ -1,9 +1,3 @@
 variable "region" {
   default = "eu-west-1"
 }
-
-variable "service_version" {
-  type        = string
-  description = "Version of the lambda functions, will usually be a github short sha or pr number"
-  default     = "latest"
-}

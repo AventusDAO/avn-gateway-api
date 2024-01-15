c394033d590c620610970cd4012d3ef58cc2a210
Rui Dos Santos Magalhães <45366558+RuiSMagalhaes@users.noreply.github.com>
2024-01-15 13:43:04 +0000

    DEVOPS-597: delete bastions and clean up infra folder (#731)
    * delete bastions and clean up infra folder
    
    * delete bastions and clean up infra folder

--- _infra/terraform/environments/testnet/gateway/vault.tf
+++ _infra/terraform/environments/testnet/gateway/vault.tf
@@ -1,13 +1,3 @@
-locals {
-  ssh_allowed_ips = [
-    "18.135.167.38/32",  # IP of the nat of Testnet (Jenkins)
-    "86.126.81.150/32",  # Vuko
-    "109.101.202.38/32", # Vuko
-    "94.60.55.242/32",   # Rui
-    "82.6.48.63/32",     # Nahu
-  ]
-}
-
 resource "aws_key_pair" "vault-ssh-key" {
   key_name   = "technical-account-vault"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDShHUS4gloN0Q5ATSHEwPCogmKIiVYNH3WUtmcmI4eyKt1yDmw7iQRHFpMK0rOISkBmCtor5kNdzuYjEXEIZryoEk4WeGCodXBGmMfVCAVi6Q6yc+0yABIFqcticyoKtndBmIIiSIZdy/66H3QvqgFxABoeOy98ja+QySmxNtVtI5ks1xkLYxINjuSGgNsA48tUWgfaUTENjBX8p9qff+iFygZalL7318mugzqMDOo3lfmu/mqy1/eKkEdNObHmBOZA341+HDA12L1Nd4Y9xnPNoZrjFQKxwe7+KT3C0NuMlVj0HdPkg6Yr6WT5eDEfqowPoofE+zgDC/f4hLdDE4i55fotGzMR50JTX2XGvEcRHYQtlu4+Ttirvrlt+3vyOUCxVjRQunwFfBTKa+v63tJjlJupS7MJefzVY4nRHEUSGPtOes5HvAS4HhjvojhHkFNY5hyNbqTXNvbvLjqtxN6ca690udEHBIaJ+ogoPhrB9VLN4vRKUHqcIQ0dOGEJ5a2qiodJJsgUqze65I+9xm2a7m8IAnSRKlHFm8ZfjBHqRciR4+MlqQcB3oKgNc0WRGt8GQktJr4DEY2bEwQWB1SRiftqPlp2x4xuok/1c+UDSu5cq56fU/EyfHNsarmL9TFmdDBraODNnaB6bAfAqHQNcRGSWD38G4hayyiXkN+Cw== technical-account-vault"
@@ -34,31 +24,4 @@ module "avn-vault" {
   dynamodb_table_name      = "avn-gw-vault-${local.environment}-db"
   avn-ingress-ports        = []
   ami_image_id             = "ami-0a8e758f5e873d1c1"
-}
-
-module "bastion" {
-  source           = "../../../modules/bastion"
-  vpc_id           = data.terraform_remote_state.vpc.outputs.vpc_id
-  ssh_key_name     = aws_key_pair.vault-ssh-key.key_name
-  public_subnet_id = data.terraform_remote_state.vpc.outputs.primary_public_subnet.id
-  ssh_allowed_ips  = local.ssh_allowed_ips
-  ami_image_id     = "ami-0a8e758f5e873d1c1"
-}
-
-resource "local_file" "avn-gateway-vault-instance-file" {
-  content  = <<-EOD
-[bastion]
-${module.bastion.public_ip} ansible_user=ubuntu
-
-[vault_server]
-${module.avn-vault.instance_ip_addr} ansible_user=ubuntu
-
-[vault_server:vars]
-ansible_ssh_common_args='-o ProxyCommand="ssh -o StrictHostKeyChecking=no -W %h:%p -q ubuntu@${module.bastion.public_ip}"'
-api_addr_value='https://${module.avn-vault.fqdn}:8200'
-aws_region='${module.avn-vault.aws_region}'
-kms_key_id='${module.avn-vault.kms_key_id}'
-dynamodb_table='${module.avn-vault.dynamodb_table}'
-EOD
-  filename = "${path.module}/vault.inventory"
 }

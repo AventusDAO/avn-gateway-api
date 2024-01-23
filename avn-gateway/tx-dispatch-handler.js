2ec03867a464490155030ac522b8040986087531
fluorostani <glyn.dimond@artos.io>
2024-01-23 14:41:30 +0000

    Update tx-dispatch-handler.js

--- avn-gateway/tx-dispatch-handler.js
+++ avn-gateway/tx-dispatch-handler.js
@@ -1,8 +1,7 @@
 const utils = require('/opt/utils.js');
 const fees = require('/opt/paymentUtils.js');
 const sqs = require('/opt/sqsUtils.js');
-const sqsClient = new sqs.SQSClient({ region: process.env.AWS_REGION });
-const AVN_TX_SQS_URL = process.env.SQS_AVN_TX_QUEUE_URL;
+
 const AVN_CONNECTOR_ENDPOINT = process.env.AVN_CONNECTOR_ENDPOINT;
 
 exports.handler = async (event, context) => {

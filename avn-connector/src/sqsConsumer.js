68812be0db54de303e2da0f4ef6198111298ce5f
fluorostani <glyn.dimond@artos.io>
2024-01-24 16:13:18 +0000

    update logs

--- avn-connector/src/sqsConsumer.js
+++ avn-connector/src/sqsConsumer.js
@@ -15,7 +15,7 @@ async function processTxQueue() {
         await deleteProcessedMessages(processed);
       }
     } catch (error) {
-      logger.error('Error processing SQS TX queue:', error);
+      logger.error('[SQS tx] Error processing queue:', error);
       await new Promise(resolve => setTimeout(resolve, 20000)); // 20-second delay to avoid a tight loop
     }
   }
@@ -28,7 +28,7 @@ async function receiveMessages() {
     WaitTimeSeconds: 20 // wait max time for messages to arrive to minimize AWS costs
   };
   const received = await sqsClient.send(new ReceiveMessageCommand(receiveParams));
-  logger.info(`Received ${received.Messages?.length || 0} SQS TX messages`);
+  logger.trace(`[SQS tx] Received ${received.Messages?.length || 0} messages`);
   return received.Messages || [];
 }
 
@@ -36,11 +36,10 @@ async function processMessages(messages) {
   const processed = [];
   for (const message of messages) {
     try {
-      logger.info(`Processing SQS TX message: ${message.MessageId}`);
       await processMessage(message);
       processed.push({ Id: message.MessageId, ReceiptHandle: message.ReceiptHandle });
     } catch (error) {
-      logger.error(`Error processing SQS TX message: ${message.MessageId}`, error);
+      logger.error(`[SQS tx] Error processing message ${message.MessageId}`, error);
     }
   }
   return processed;
@@ -53,29 +52,29 @@ async function processMessage(message) {
 
   switch (txType) {
     case 'avnProxy':
-      logger.info(`${requestId} - Processing new transaction from queue: ${JSON.stringify(txData)}`);
+      logger.trace(`[SQS tx] ${requestId} - Sending proxy transaction: ${JSON.stringify(txData)}`);
       const { palletName, method, params } = txData;
 
       if (isSplitFeeTransaction(txData)) {
         const paymentNonce = await avn.getPayerPaymentNonce(requestId, params.splitFeePayerAddress);
-        logger.trace(`${requestId} - Split fee transaction. Payment nonce: ${paymentNonce}`);
+        logger.trace(`[SQS tx] ${requestId} - Split fee transaction - payment nonce: ${paymentNonce}`);
         params.paymentInfo = await avn.generateSplitFeePaymentInfo(requestId, params, paymentNonce);
         params.paymentNonce = paymentNonce;
       }
 
       result = await avn.proxy(requestId, palletName, method, params);
-      logger.info(`${requestId} - Processing completed. Result: ${JSON.stringify(result)}`);
+      logger.trace(`[SQS tx] ${requestId} - Proxy transaction sent: ${JSON.stringify(result)}`);
       break;
 
     case 'avnProcessLifts':
-      logger.info(`${requestId} - Processing lift transaction from queue: ${JSON.stringify(txData)}`);
+      logger.trace(`[SQS tx] ${requestId} - Sending lift transaction: ${JSON.stringify(txData)}`);
       const { toBlock, unprocessedLifts } = txData;
       result = await avn.processLifts(requestId, toBlock, unprocessedLifts);
-      logger.info(`${requestId} - Processing completed. Result: ${JSON.stringify(result)}`);
+      logger.trace(`[SQS tx] ${requestId} - Lift transaction sent: ${JSON.stringify(result)}`);
       break;
 
     default:
-      logger.error(`${requestId} - Transaction "${txType}" not supported - will be deleted from SQS TX queue`);
+      logger.error(`${requestId} - Transaction "${txType}" not supported - will be removed from queue`);
   }
 }
 
@@ -86,9 +85,9 @@ function isSplitFeeTransaction(params) {
 async function deleteProcessedMessages(entries) {
   if (entries.length > 0) {
     const result = await sqsClient.send(new DeleteMessageBatchCommand({ QueueUrl: SQS_TX_QUEUE_URL, Entries: entries }));
-    logger.info(`Successfully deleted ${result.Successful?.length || 0} processed SQS TX messages`);
-    if (result.Failed.length > 0) {
-      logger.error('Failed to delete processed SQS TX messages:', result.Failed);
+    logger.trace(`[SQS tx] Successfully processed ${result.Successful?.length || 0} messages`);
+    if (result.Failed !== undefined) {
+      logger.error('[SQS tx] Failed to delete processed messages:', result.Failed);
     }
   }
 }

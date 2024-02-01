d9d308511b0536057d8a7e79a4ec90315982a607
fluorostani <glyn.dimond@artos.io>
2024-02-01 17:45:35 +0000

    Update autolowering.js

--- avn-connector/src/lowers/autolowering.js
+++ avn-connector/src/lowers/autolowering.js
@@ -40,8 +40,6 @@ async function autolower() {
     return 'No autolower account set';
   }
 
-  console.log("XXXXXXXXXXXXX", autolowerAccount)
-
   try {
     const { avnContract } = await avn.getChainInfo();
     const bridge = await tier1.connectToBridge(avnContract, LOWERING_ABI, autolowerAccount);

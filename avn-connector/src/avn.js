eeb92797ce50423b97f16454e91bd72f9c205763
Nahu <39748285+nahuseyoum@users.noreply.github.com>
2024-02-14 14:11:54 +0000

    Update avn-connector/src/avn.js
    Co-authored-by: fluorostani <39763263+fluorostani@users.noreply.github.com>

--- avn-connector/src/avn.js
+++ avn-connector/src/avn.js
@@ -311,7 +311,7 @@ async function processLifts(requestId, toBlock, unprocessedLifts) {
   try {
     result = await signAndSend(requestId, RELAYER_ADDRESS, txn);
     const lastT1BlockChecked = await redis.getLiftsFromTier1Block();
-    // extra safety to prevent reseting the last checked block
+    // extra safety to prevent resetting the last checked block
     if (toBlock >= lastT1BlockChecked) {
       await redis.setLiftsFromTier1Block(parseInt(toBlock) + 1);
     }

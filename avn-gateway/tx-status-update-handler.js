af1e42b3d73cce68119899906cb155320c5089e0
nahuseyoum <nahu.seyoum@aventus.io>
2024-02-29 15:56:58 +0000

    SYS-3913 Update poller to check for cross chain transactions

--- avn-gateway/tx-status-update-handler.js
+++ avn-gateway/tx-status-update-handler.js
@@ -2,12 +2,19 @@ const utils = require('/opt/utils.js');
 
 const AVN_CONNECTOR_ENDPOINT = process.env.AVN_CONNECTOR_ENDPOINT;
 const BLOCK_EXPLORER_BASE_URL = process.env.BLOCK_EXPLORER_BASE_URL;
+const TX_LIMIT = 2500;
 
 // Make sure this is kept in sync with the state names defined in avn-connector/src/redis.js
 const transactionStatus = {
   Processed: 'Processed',
-  Rejected: 'Rejected'
+  Rejected: 'Rejected',
+  Validating: 'Validating'
 };
+
+// any cross chain transactions such as lifting will emit this event
+const NEW_CROSS_CHAIN_EVENT = 'EthereumEvents.EthereumEventAdded';
+const SUCCESS_CROSS_CHAIN_EVENT = 'EthereumEvents.EventAccepted';
+const FAILED_CROSS_CHAIN_EVENT = 'EthereumEvents.EventRejected';
 
 exports.handler = async _event => {
   try {
@@ -43,6 +50,7 @@ async function getTransactionsStatusFromIndexer(transactionHashes) {
   try {
     log('Requesting', transactionHashes);
 
+    const crossChainFilter = [NEW_CROSS_CHAIN_EVENT];
     const successFilter = ['System.ExtrinsicSuccess'];
     const failureFilter = ['System.ExtrinsicFailed', 'AvnProxy.InnerCallFailed', 'EthereumEvents.EventRejected'];
     // Any extrinsics for which we wish to capture event output can be added to the argsFilter:
@@ -52,36 +60,87 @@ async function getTransactionsStatusFromIndexer(transactionHashes) {
       'NftManager.BatchCreated',
       'TokenManager.LowerRequested'
     ];
-    const extrinsicFilter = successFilter.concat(failureFilter).concat(argsFilter);
-    const limit = Math.min(extrinsicFilter.length * transactionHashes.length, 2500);
+
+    const extrinsicFilter = successFilter.concat(failureFilter).concat(argsFilter).concat(crossChainFilter);
+    const limit = Math.min(extrinsicFilter.length * transactionHashes.length, TX_LIMIT);
     const query = `query GatewayApiStatus { events(where: {extrinsic: {hash_in: ${JSON.stringify(transactionHashes)}},
         name_in: ${JSON.stringify(extrinsicFilter)}}, limit: ${limit})
         { name args extrinsic { hash indexInBlock success block { height } } } }`;
+
     const response = await utils.axios.post(BLOCK_EXPLORER_BASE_URL, { query, operationName: 'GatewayApiStatus' });
     const events = response.data.data.events;
 
     // The same transaction can have multiple events returned for it. Here we reduce them
     // by having any failure events or events with args supplant success events:
-    const txStatuses = {};
+    const txStatusEvents = {};
+    const crossChainTransactions = {};
+
     events.forEach(event => {
       const txHash = event.extrinsic.hash;
-      if (failureFilter.concat(argsFilter).includes(event.name) || txHash in txStatuses === false) txStatuses[txHash] = event;
+      if (failureFilter.concat(argsFilter).includes(event.name) || txHash in txStatusEvents === false) {
+        txStatusEvents[txHash] = event;
+      }
+
+      if (event.name in failureFilter === false && event.name === NEW_CROSS_CHAIN_EVENT) {
+        // we have a successfull new cross chain transaction, set the status to validating
+        crossChainTransactions[txHash] = event;
+        crossChainTransactions[txHash].status = transactionStatus.Validating
+      }
     });
 
-    log('Received', Object.keys(txStatuses));
+    log('All transactions - Received: ', Object.keys(txStatusEvents));
+    log('Cross chain transactions - Received: ', Object.keys(crossChainTransactions));
 
-    return Object.values(txStatuses).map(status => {
+    // Check the status of cross chain events, one at at time
+    // TODO: check if there a performance issue here?
+    for (const [txHash, tx] of Object.entries(crossChainTransactions)) {
+      const status = await getCrossChainTransactionStatus(tx.args.ethEventId);
+
+      if (status) {
+        // We have a status so update it
+        crossChainTransactions[txHash].status = status
+      }
+    }
+
+    return Object.values(txStatusEvents).map(statusEvent => {
+      let status;
+
+      if (failureFilter.includes(statusEvent.name)) {
+        status = transactionStatus.Rejected
+      } else {
+        if (crossChainTransactions[statusEvent.extrinsic.hash]?.status) {
+          status = crossChainTransactions[statusEvent.extrinsic.hash].status
+        } else {
+          status = transactionStatus.Processed
+        }
+      }
+
       return {
-        transactionHash: status.extrinsic.hash,
-        status: failureFilter.includes(status.name) ? transactionStatus.Rejected : transactionStatus.Processed,
-        blockNumber: status.extrinsic.block.height,
-        index: status.extrinsic.indexInBlock,
-        eventArgs: argsFilter.includes(status.name) ? status.args : {}
+        transactionHash: statusEvent.extrinsic.hash,
+        status,
+        blockNumber: statusEvent.extrinsic.block.height,
+        index: statusEvent.extrinsic.indexInBlock,
+        eventArgs: argsFilter.includes(statusEvent.name) ? statusEvent.args : {}
       };
     });
   } catch (error) {
     throw new Error(`Error getting transaction status from indexer: ${error}`);
   }
+}
+
+async function getCrossChainTransactionStatus(txEvent) {
+  const query = `query CrossChainTransactions { events(where: {
+    name_in: ["${SUCCESS_CROSS_CHAIN_EVENT}", "${FAILED_CROSS_CHAIN_EVENT}"],
+    args_jsonContains: "{\\"ethEventId\\": {\\"signature\\":\\"${txEvent.signature}\\",\\"transactionHash\\":\\"${txEvent.transactionHash}\\"}}"
+    },
+  limit: 1) {id}}`
+
+  const response = await utils.axios.post(BLOCK_EXPLORER_BASE_URL, { query, operationName: 'GatewayApiStatus' });
+  if (response?.data?.data?.events && response?.data?.data?.events.length > 0) {
+    return response?.data?.data?.events[0].name === SUCCESS_CROSS_CHAIN_EVENT ? transactionStatus.Processed : transactionStatus.Rejected
+  }
+
+  return transactionStatus.Validating
 }
 
 function log(state, txHashes) {
@@ -93,3 +152,5 @@ function log(state, txHashes) {
     console.info(`${state} 0 transaction statuses from graphQL`);
   }
 }
+
+

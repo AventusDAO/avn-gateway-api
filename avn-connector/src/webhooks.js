f9e06d5b9c7617e7531ee69d98493583b926796f
fluorostani <glyn.dimond@artos.io>
2024-03-02 18:16:34 +0000

    Update webhooks.js

--- avn-connector/src/webhooks.js
+++ avn-connector/src/webhooks.js
@@ -4,42 +4,40 @@ const config = require('multiconfig').load();
 const log = require('log4js').configure(config.log4Js).getLogger();
 const snsClient = new SNSClient({ region: config.aws.region });
 
-const REFRESH_TIME = 60 * 1000; // 1 minute
+const ACCOUNTS_REFRESH_PERIOD = 60 * 1000; // 1 minute
 const trackedAccounts = new Map();
-let lastRefresh = Date.now();
+let accountsLastRefreshed = Date.now();
 
 // TODO: Get these from the database
 const Events = {
-  Transaction: {
-    Received: 'Transaction received',
-    Sending: 'Transaction sending in progress',
-    Sent: 'Transaction sent',
-    Succeeded: 'Transaction executed successfully',
-    Failed: 'Transaction failed'
+  transaction: {
+    received: 'transaction received',
+    sending: 'transaction sending',
+    sent: 'transaction sent',
+    succeeded: 'transaction succeeded',
+    failed: 'transaction failed'
   },
-  Lower: {
-    Received: 'Lower received',
-    Failed: 'Lower failed'
+  lower: {
+    received: 'lower received',
+    succeeded: 'lower succeeded',
+    failed: 'lower failed'
   }
 };
 
 async function publishEvent(category, state, account, requestId, eventData) {
   const eventTime = Date.now();
-  refreshTrackedAccountsIfRequired(eventTime);
+  await refreshTrackedAccountsIfRequired(eventTime);
 
   if (!trackedAccounts.has(account)) return;
   if (!Events[category]) throw new Error('Invalid event category');
   if (!Events[category][state]) throw new Error('Invalid event state');
-
   const eventType = Events[category][state];
-  const payload = JSON.stringify({ requestId, eventType, eventTime, account, eventData });
 
   try {
     const eventData = {
       TopicArn: trackedAccounts.get(account),
-      Message: payload,
+      Message: JSON.stringify({ eventData, eventTime, eventType, requestId }),
       MessageAttributes: {
-        // Filtering on attributes is less costly than filtering on payload data
         eventType: {
           DataType: 'String',
           StringValue: eventType
@@ -49,21 +47,20 @@ async function publishEvent(category, state, account, requestId, eventData) {
           StringValue: account
         }
       },
-      MessageGroupId: hash(account), // SNS FIFO Topic grouping
+      MessageGroupId: hash(account),
       MessageDeduplicationId: hash(requestId, eventType)
     };
 
     await snsClient.send(new PublishCommand(eventData));
   } catch (err) {
-    log.error(`Error publishing webhook event: ${payload}`, err);
-    throw err;
+    log.error(`Error publishing webhook event: ${JSON.stringify({ account, eventType, eventData, requestId })}`, err);
   }
 }
 
-function refreshTrackedAccountsIfRequired(timeNow) {
-  if (timeNow - lastRefresh > REFRESH_TIME) {
-    lastRefresh = timeNow;
-    updateAccountTracking();
+async function refreshTrackedAccountsIfRequired(timeNow) {
+  if (timeNow - accountsLastRefreshed > ACCOUNTS_REFRESH_PERIOD) {
+    accountsLastRefreshed = timeNow;
+    await updateAccountTracking();
   }
 }
 
@@ -76,7 +73,6 @@ async function updateAccountTracking() {
     }
   } catch (err) {
     log.error('Error updating webhook account tracking', err);
-    throw err;
   }
 }
 

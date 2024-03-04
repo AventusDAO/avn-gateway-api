e5cc9df8190f0e590f8aa1cfcdbd0007dc505483
fluorostani <glyn.dimond@artos.io>
2024-03-04 17:18:23 +0000

    Update webhooks-handler.js

--- avn-gateway/webhooks-handler.js
+++ avn-gateway/webhooks-handler.js
@@ -21,8 +21,8 @@ exports.handler = async event => {
   }
 };
 
-// Registers an https listener endpoint to receive specific tx events for an account.
-// The endpoint requires confirmation before it can start receiving these events.
+// Registers an https listener endpoint to receive specific tx events for an account
+// The endpoint requires confirmation before it can start receiving these events
 async function register(account, eventTypes, endpoint) {
   const topicArn = (await processAccount(account)) || (await sns.createTopic(account));
   const filterPolicy = { account, eventType: eventTypes };
@@ -30,7 +30,7 @@ async function register(account, eventTypes, endpoint) {
   return processResult('Webhook registered', { account, eventTypes, endpoint });
 }
 
-// Enables a registered endpoint to start receiving events by confirming the token it should have received upon registration.
+// Enables a registered endpoint to start receiving events by confirming the token it should have received upon registration
 async function confirm(account, token) {
   const topicArn = await getValidatedTopicArn(account);
   const subscriptionArn = await sns.confirmTopicSubscription(topicArn, token);
@@ -38,7 +38,7 @@ async function confirm(account, token) {
   return processResult('Webhook confirmed', { account, webhook });
 }
 
-// Updates the set of events an existing webook will receive for an account.
+// Updates the set of events an existing webook will receive for an account
 async function update(account, eventTypes, webhook) {
   const topicArn = await getValidatedTopicArn(account);
   const newFilterPolicy = { account, eventType: eventTypes };
@@ -47,7 +47,7 @@ async function update(account, eventTypes, webhook) {
   return processResult('Webhook updated', { account, eventTypes, webhook });
 }
 
-// Deregisters a webhook (removing listening to that account's activity entirely if no other webhooks remain attached).
+// Deregisters a webhook (removing listening to the associated account's activity entirely if no other webhooks remain attached)
 async function deregister(account, webhook) {
   const topicArn = await getValidatedTopicArn(account);
   const subscriptionArn = [topicArn, webhook].join(':');

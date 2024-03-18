ad524c87c8f66d3067e53ab24adb2c46701d9999
fluorostani <glyn.dimond@artos.io>
2024-03-18 16:50:40 +0000

    fix emitter

--- avn-gateway/webhooks-event-emitter-handler.js
+++ avn-gateway/webhooks-event-emitter-handler.js
@@ -5,24 +5,26 @@ const KMS_KEY_ID = process.env.WEBHOOKS_SIGNER_KMS_KEY_ID;
 
 exports.handler = async event => {
   for (const record of event.Records) {
-    const { endpoint, eventData } = JSON.parse(record.body); // eventData: { timestamp, event, publicKey, requestId, data };
-    const eventId = record.messageId;
+    const { endpoint, eventData: data } = JSON.parse(record.body); // eventData: { timestamp, event, publicKey, requestId, data };
+    const id = record.messageId;
 
     try {
-      const messageData = { eventId, eventData };
-      const message = JSON.stringify(messageData);
+      const freshness = new Date().toISOString();
+      const message = JSON.stringify({ id, freshness, data });
       const signature = await signMessage(KMS_KEY_ID, message);
+
       const headers = {
         'content-type': 'application/json',
-        'x-avn-event-id': eventId,
-        'x-avn-event-signature': signature
+        'x-avn-event-id': id,
+        'x-avn-event-signature': signature,
+        'x-avn-event-freshness': freshness
       };
 
-      await axios.post(endpoint, eventData, { headers });
-      console.log(`Event ID ${eventId} sent to ${endpoint}: ${JSON.stringify(eventData)}`);
+      await axios.post(endpoint, data, { headers });
+      console.log(`Event ID ${id} sent to ${endpoint}: ${JSON.stringify(data)}`);
     } catch (error) {
       console.error(
-        `Error sending event ID ${eventId} to ${endpoint}: ${error.response ? error.response.statusText : error.message}`
+        `Error sending event ID ${id} to ${endpoint}: ${error.response ? error.response.statusText : error.message}`
       );
     }
   }
--- tools/webhooks-endpoint/index.js
+++ tools/webhooks-endpoint/index.js
@@ -1,21 +1,18 @@
-const axios = require('axios');
-const crypto = require('crypto');
 const express = require('express');
 const { format } = require('date-fns');
-const formatTS = ts => format(new Date(ts), 'HH:mm:ss.SSS');
+const Verifier = require('./verifier.js');
 
-const verificationKeyURL = (process.argv[2] || 'https://uat.gateway.aventus.io') + '/webhook-verification-key';
-
+const verifier = new Verifier();
 const app = express();
-
 app.use(express.json());
 
 app.listen(4443, async () => {
   console.log('Listening...');
   try {
-    await initialiseVerfier();
+    await verifier.init(process.argv[2]);
   } catch (error) {
     console.error('Error fetching verification key');
+    process.exit(1);
   }
 });
 
@@ -25,34 +22,12 @@ app.get('/ping', (req, res) => {
 });
 
 app.post('/listen', (req, res) => {
-  const { isVerified, eventId, event, requestId, timestamp, publicKey, data } = decodeEvent(req);
-  console.log(`${formatTS(timestamp)} - ${requestId} - ${publicKey} - ${event} - verified: ${isVerified}`);
-  res.status(200).send();
-});
-
-let verifier;
-
-async function initialiseVerfier() {
-  const response = await axios.get(verificationKeyURL);
-  verifier = response.data.publicKeyPEM;
-}
-
-function decodeEvent(req) {
-  const eventId = req.headers['x-avn-event-id'];
-  const eventData = req.body;
-  const eventSignature = req.headers['x-avn-event-signature'];
-  const isVerified = verifyEvent(eventId, eventData, eventSignature);
-  const { event, requestId, timestamp, publicKey, data } = eventData;
-  return { isVerified, eventId, event, requestId, timestamp, publicKey, data };
-}
-
-function verifyEvent(eventId, eventData, eventSignature) {
-  const signature = Buffer.from(eventSignature ,'base64');
-  const message = Buffer.from(JSON.stringify({ eventId, eventData }));
   try {
-    return crypto.verify('SHA256', message, verifier, signature);
+    const { event, requestId, timestamp, publicKey, data } = verifier.verifyEvent(req);
+    console.log(`${format(new Date(timestamp), 'HH:mm:ss.SSS')} - ${requestId} - ${publicKey} - ${event}`);
+    res.status(200).send();
   } catch (error) {
-    console.error('Verification error:', error);
-    return false;
+    console.error(error);
+    res.status(400).send('Verification failed');
   }
-}
+});

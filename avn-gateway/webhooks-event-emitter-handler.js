6330e7d3d30ef486417de239c1ea5e6f5f357868
fluorostani <glyn.dimond@artos.io>
2024-03-19 19:01:32 +0000

    SYS-3904 Add webhooks event emitter

---
+++ avn-gateway/webhooks-event-emitter-handler.js
@@ -0,0 +1,25 @@
+const { axios } = require('/opt/utils.js');
+
+exports.handler = async event => {
+  for (const record of event.Records) {
+    const { endpoint, eventData: data } = JSON.parse(record.body); // eventData: { timestamp, event, publicKey, requestId, data };
+    const id = record.messageId;
+
+    try {
+      const freshness = new Date().toISOString();
+      const message = JSON.stringify({ id, freshness, data });
+
+      const headers = {
+        'content-type': 'application/json',
+        'x-avn-event-id': id,
+        'x-avn-event-freshness': freshness
+      };
+
+      await axios.post(endpoint, data, { headers });
+      console.log(`Event ${id} sent to ${endpoint}: ${JSON.stringify(data)}`);
+    } catch (error) {
+      console.error(`Failed sending event ${id} to ${endpoint}: ${error.response ? error.response.statusText : error.message}`);
+      throw error;
+    }
+  }
+};

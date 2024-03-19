f704d3c9da22326ec083fcb1dc2aa451af5e402b
fluorostani <glyn.dimond@artos.io>
2024-03-19 19:38:28 +0000

    SYS-3905 Add verification to test endpoint

--- tools/webhooks-endpoint/index.js
+++ tools/webhooks-endpoint/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const { format } = require('date-fns');
+const Verifier = require('./verifier.js');
 
+const verifier = new Verifier();
 const app = express();
 app.use(express.json());
 
@@ -14,7 +16,12 @@ app.get('/ping', (req, res) => {
 });
 
 app.post('/listen', (req, res) => {
-  const { event, requestId, timestamp, publicKey, data } = req.body;
-  console.log(`${format(new Date(timestamp), 'HH:mm:ss.SSS')} - ${requestId} - ${publicKey} - ${event}`);
-  res.status(200).send();
+  try {
+    const { event, requestId, timestamp, publicKey, data } = verifier.verifyEvent(req);
+    console.log(`${format(new Date(timestamp), 'HH:mm:ss.SSS')} - ${requestId} - ${publicKey} - ${event}`);
+    res.status(200).send();
+  } catch (error) {
+    console.error(error);
+    res.status(400).send('Verification failed');
+  }
 });
---
+++ tools/webhooks-endpoint/verifier.js
@@ -0,0 +1,40 @@
+const crypto = require('crypto');
+const axios = require('axios');
+
+class Verifier {
+  constructor() {
+    this.publicKeyPEM = null;
+  }
+
+  async init(freshnessWindow = 5000, gatewayURL = 'https://uat.gateway.aventus.io') {
+    this.freshnessWindow = freshnessWindow;
+    const verificationKeyURL = `${gatewayURL}/webhook-verification-key`;
+    try {
+      const response = await axios.get(verificationKeyURL);
+      this.publicKeyPEM = response.data.publicKeyPEM;
+    } catch (error) {
+      throw new Error(`Error fetching verification key: ${error}`);
+    }
+  }
+
+  verifyEvent(request) {
+    const { body: data, headers } = request;
+    const id = headers['x-avn-event-id'];
+    const signature = headers['x-avn-event-signature'];
+    const freshness = headers['x-avn-event-freshness'] || 0;
+
+    if (Date.now() > new Date(freshness).getTime() + this.freshnessWindow) {
+      throw new Error(`Reason: Freshness, Request ID: ${data.requestId}, event: ${data.event}`);
+    }
+
+    const message = Buffer.from(JSON.stringify({ id, freshness, data }));
+
+    if (!crypto.verify('SHA256', message, this.publicKeyPEM, Buffer.from(signature, 'base64'))) {
+      throw new Error(`Reason: Signature, Request ID: ${data.requestId}, event: ${data.event}`);
+    }
+
+    return data;
+  }
+}
+
+module.exports = Verifier;

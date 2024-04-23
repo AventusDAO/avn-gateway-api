a238f4d27f0dd995a454120ee506eae45a3aa30b
IvanCholakov <ivan.cholakov@aventus.io>
2024-04-23 14:45:32 +0300

    SYS-3988: converted lift processing handler to typescript

---
+++ avn-gateway/lift-processing-handler.ts
@@ -0,0 +1,39 @@
+import * as utils from '/opt/utils.js';
+import * as sqs from '/opt/sqsUtils.js';
+import { Handler, Context } from 'aws-lambda';
+
+
+const AVN_CONNECTOR_ENDPOINT: string = process.env.AVN_CONNECTOR_ENDPOINT!;
+const SQS_TX_QUEUE_URL: string = process.env.SQS_TX_QUEUE_URL!;
+
+export const handler: Handler = async (_event: any, context: Context): Promise<void> => {
+  try {
+    await processLifts(context.awsRequestId);
+  } catch (error) {
+    console.error(`CHECKING FOR LIFTS TO PROCESS: ${error}`);
+  }
+};
+
+interface LiftData {
+  fromBlock: number;
+  toBlock: number;
+  unprocessedLifts: string[];
+}
+
+interface Transaction {
+  txType: string;
+  requestId: string;
+  toBlock: number;
+  unprocessedLifts: string[];
+}
+
+async function processLifts(requestId: string): Promise<void> {
+  let { fromBlock, toBlock, unprocessedLifts }: LiftData = (await utils.axios.get(`${AVN_CONNECTOR_ENDPOINT}unprocessedLifts`)).data;
+  if (!unprocessedLifts || unprocessedLifts.length === 0) {
+    console.info(`Checked Ethereum blocks ${fromBlock} to ${toBlock} - no lifts to process`);
+  } else {
+    console.info(`Checked Ethereum blocks ${fromBlock} to ${toBlock} - found lifts to process: ${unprocessedLifts.join(', ')}`);
+    const tx: Transaction = { txType: 'avnProcessLifts', requestId, toBlock, unprocessedLifts };
+    await sqs.sendToQueue(SQS_TX_QUEUE_URL, tx);
+  }
+}

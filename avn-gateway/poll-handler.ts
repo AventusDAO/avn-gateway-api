8c063189cc2ab48400e34c35a6f2f4ddce6c8043
Ivan Cholakov <icholakov1@gmail.com>
2024-04-24 13:25:08 +0300

    Sys 3981 convert poll handler to ts (#775)
    * converted poll-handler to ts
    
    * deleted poll-handler js file
    
    * fixup! deleted poll-handler js file
    
    * fixup! deleted poll-handler js file

--- avn-gateway/poll-handler.ts
+++ avn-gateway/poll-handler.ts
@@ -1,8 +1,34 @@
-const utils = require('/opt/utils.js');
+import utils from '/opt/utils.js';
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 
-const AVN_CONNECTOR_ENDPOINT = process.env.AVN_CONNECTOR_ENDPOINT;
+const axios = utils.axios.default;
 
-exports.handler = async event => {
+interface Call {
+  id: string | null;
+  method: string;
+  params?: {
+    requestId: string;
+  };
+}
+
+interface ErrorResponse {
+  jsonrpc: string;
+  id: string | null;
+  error?: {
+    code: number;
+    message: string;
+    data?: any;
+  }
+}
+
+interface ValidResponse {
+  statusCode: number;
+  body: string;
+}
+
+const AVN_CONNECTOR_ENDPOINT: string | undefined = process.env.AVN_CONNECTOR_ENDPOINT;
+
+export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   await utils.init();
   return {
     statusCode: 200,
@@ -10,8 +36,8 @@ exports.handler = async event => {
   };
 };
 
-async function processRequest(request) {
-  let call;
+async function processRequest(request: string): Promise<ErrorResponse | ValidResponse> {
+  let call: Call;
 
   try {
     call = JSON.parse(request);
@@ -28,14 +54,14 @@ async function processRequest(request) {
   }
 }
 
-async function makeCall(call, request) {
+async function makeCall(call: Call, request: string): Promise<ErrorResponse | ValidResponse> {
   console.info(`Processing call: ${JSON.stringify(call)}`);
 
   if (call.method !== 'requestState') {
     return utils.buildErrorBody('method', "method must be 'requestState'", call.method, request, call.id);
   }
 
-  const { requestId } = call.params;
+  const { requestId } = call.params ?? {};
 
   if (utils.isValidRequestId(requestId) === false) {
     return utils.buildErrorBody('params', 'invalid request ID', requestId, request, call.id);
@@ -44,10 +70,10 @@ async function makeCall(call, request) {
   return await poll(call, request, requestId);
 }
 
-async function poll(call, request, requestId) {
+async function poll(call: Call, request: string, requestId?: string): Promise<ErrorResponse | ValidResponse> {
   try {
     const callId = call.id;
-    const avnResponse = await utils.axios.post(AVN_CONNECTOR_ENDPOINT + 'avnPoll', { callId, requestId });
+    const avnResponse = await axios.post(AVN_CONNECTOR_ENDPOINT + 'avnPoll', { callId, requestId });
 
     if (!avnResponse.data)
       return utils.buildErrorBody('internal', 'failed to poll chain', 'Invalid data returned', request, call.id);
@@ -55,7 +81,7 @@ async function poll(call, request, requestId) {
       return utils.buildErrorBody('internal', 'failed to poll chain', avnResponse.data.error, request, call.id);
 
     return utils.buildValidResponseBody(callId, avnResponse.data);
-  } catch (err) {
+  } catch (err: any) {
     return utils.buildErrorBody('internal', 'failed to poll chain', err.toString(), request, call.id);
   }
 }

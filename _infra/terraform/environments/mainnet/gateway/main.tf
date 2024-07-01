ede0f82601cffebd30a86bbdc30b7226a7176da1
Rui Dos Santos Magalhães <45366558+RuiSMagalhaes@users.noreply.github.com>
2024-07-01 14:12:36 +0100

    decomission old-gateway hosted zones (#813)

--- _infra/terraform/environments/mainnet/gateway/main.tf
+++ _infra/terraform/environments/mainnet/gateway/main.tf
@@ -2,16 +2,3 @@ locals {
   environment           = "mainnet"
   vault_recovery_window = 0
 }
-
-module "dns" {
-  source = "../../../modules/dns"
-
-  vpc_id           = data.terraform_remote_state.vpc.outputs.vpc_id
-  parachain_vpc_id = data.terraform_remote_state.parachain_mainnet.outputs.vpc_id
-  environment      = local.environment
-
-  providers = {
-    aws         = aws
-    aws.aventus = aws.aventus
-  }
-}

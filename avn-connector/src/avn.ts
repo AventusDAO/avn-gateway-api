aa6747a5669c7c3d3827611a2e6dbe6fa2e2201e
Micael Franco <micael.franco@aventus.io>
2024-08-29 05:15:37 +0100

    Fix ss58

--- avn-connector/src/avn.ts
+++ avn-connector/src/avn.ts
@@ -696,9 +696,9 @@ async function connectToAvN(): Promise<void> {
   const provider = new WsProvider(AVN_URL);
   api = await ApiPromise.create({ provider });
 
-  const keyring = new Keyring({ type: 'sr25519' });
-  keyring.setSS58Format(42);
-
+  // const keyring = new Keyring({ type: 'sr25519' });
+  // keyring.setSS58Format(42);
+  const keyring = new Keyring({ type: 'sr25519', ss58Format: 2024 });
   const [chain, nodeName, nodeVersion] = await Promise.all([
     api.rpc.system.chain(),
     api.rpc.system.name(),

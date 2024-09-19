8f18875a2b064e0ad4c0d1c3e6750de8ec18b867
nahuseyoum <nahu.seyoum@aventus.io>
2024-09-19 15:29:08 +0100

    update

--- avn-connector/src/db/migrations/1726749398507-multi-currency-3.ts
+++ avn-connector/src/db/migrations/1726749398507-multi-currency-3.ts
@@ -4,7 +4,7 @@ export class multiCurrency31726749398507 implements MigrationInterface {
     name = 'multiCurrency31726749398507'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "payer_transaction" ADD CONSTRAINT "PK_5e0e58d0d7ae267ff4696924f6a" PRIMARY KEY ("transactionId", "payerId", "currencyId")`);
+        //await queryRunner.query(`ALTER TABLE "payer_transaction" ADD CONSTRAINT "PK_5e0e58d0d7ae267ff4696924f6a" PRIMARY KEY ("transactionId", "payerId", "currencyId")`);
         await queryRunner.query(`ALTER TABLE "fee" DROP CONSTRAINT "FK_4d28f6d5f2558ebf9535bbd96ca"`);
         await queryRunner.query(`ALTER TABLE "fee" DROP CONSTRAINT "UQ_a6f588f77a9624084f58f901802"`);
         await queryRunner.query(`ALTER TABLE "fee" ALTER COLUMN "currencyId" SET NOT NULL`);
@@ -16,7 +16,7 @@ export class multiCurrency31726749398507 implements MigrationInterface {
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "payer_transaction" DROP CONSTRAINT "PK_5e0e58d0d7ae267ff4696924f6a"`);
+        //await queryRunner.query(`ALTER TABLE "payer_transaction" DROP CONSTRAINT "PK_5e0e58d0d7ae267ff4696924f6a"`);
         await queryRunner.query(`ALTER TABLE "payer_transaction" DROP CONSTRAINT "FK_8444ee59f786ff114e38537ddb1"`);
         await queryRunner.query(`ALTER TABLE "fee" DROP CONSTRAINT "FK_4d28f6d5f2558ebf9535bbd96ca"`);
         await queryRunner.query(`ALTER TABLE "fee" DROP CONSTRAINT "UQ_a6f588f77a9624084f58f901802"`);

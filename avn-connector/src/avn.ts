b3fd4b3837eb98e40eadd615cc6a560df6975148
ivan-cholakov <ivan.cholakov@aventus.io>
2024-11-25 18:40:39 +0200

    fix: remove debug logs

--- avn-connector/src/avn.ts
+++ avn-connector/src/avn.ts
@@ -804,9 +804,7 @@ function toBn(val: any): BN {
 
 async function getLowerProof(lowerId: number): Promise<string | null> {
   const rawProof = await api.query.tokenManager.lowersReadyToClaim(lowerId);
-  console.log("[RAW PROOF] ", rawProof.toJSON())
   let proof = rawProof.toJSON() as any;
-  console.log("[PROOF] ", proof)
   return proof?.encodedLowerData ?? null;
 }
 

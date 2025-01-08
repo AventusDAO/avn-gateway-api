b27c38656921f99739b78a6bca3710a973abf90c
nahuseyoum <nahu.seyoum@aventus.io>
2025-01-08 18:10:41 +0000

    adding string message

--- avn-gateway/common/utils.ts
+++ avn-gateway/common/utils.ts
@@ -308,10 +308,11 @@ function determineFormatAndVerifySignature(encodedData: Uint8Array, signature: s
 function verifyEthereumSignature(encodedData: Uint8Array, signature: string, publicKey: string): boolean {
   try {
     const signerPublicKey = convertToPublicKey(publicKey);
-    const messageHash = ethers.utils.hashMessage(encodedData);
+    const encodedDataString = u8aToString(encodedData);
+    const messageHash = ethers.utils.hashMessage(encodedDataString);
     const ethereumAddress = ethers.utils.recoverAddress(messageHash, signature);
     const derivedPublicKey = blake2AsHex(hexToU8a(ethereumAddress));
-    console.log(`Encoded data: ${u8aToString(encodedData)},\nMessage hash: ${messageHash},\nrecovered eth address: ${ethereumAddress},\nderived public key: ${derivedPublicKey},\nsigner public key: ${signerPublicKey}`);
+    console.log(`Encoded data: ${encodedDataString},\nMessage hash: ${messageHash},\nrecovered eth address: ${ethereumAddress},\nderived public key: ${derivedPublicKey},\nsigner public key: ${signerPublicKey}`);
     return derivedPublicKey === signerPublicKey;
   } catch (error) {
     console.error(`Ethereum signature verification error:`, error);

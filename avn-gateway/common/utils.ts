fd4529cf6d782c6007489acf32b5811cf249e4b1
nahuseyoum <nahu.seyoum@aventus.io>
2025-01-08 02:01:27 +0000

    update

--- avn-gateway/common/utils.ts
+++ avn-gateway/common/utils.ts
@@ -308,7 +308,7 @@ function verifyEthereumSignature(encodedData: Uint8Array, signature: string, pub
   try {
     const signerPublicKey = convertToPublicKey(publicKey);
     const encodedDataHex = u8aToHex(encodedData);
-    const messageHash = ethers.utils.hashMessage(ethers.utils.arrayify(encodedDataHex));
+    const messageHash = ethers.utils.hashMessage(toUTF8Array(encodedDataHex));
     const ethereumAddress = ethers.utils.recoverAddress(messageHash, signature);
     const derivedPublicKey = blake2AsHex(hexToU8a(ethereumAddress));
     console.log(`Encoded data: ${encodedDataHex},\nMessage hash: ${messageHash},\nrecovered eth address: ${ethereumAddress},\nderived public key: ${derivedPublicKey},\nsigner public key: ${signerPublicKey}`);
@@ -317,6 +317,37 @@ function verifyEthereumSignature(encodedData: Uint8Array, signature: string, pub
     console.error(`Ethereum signature verification error:`, error);
     return false;
   }
+}
+
+function toUTF8Array(str: string) {
+  var utf8 = [];
+  for (var i=0; i < str.length; i++) {
+      var charcode = str.charCodeAt(i);
+      if (charcode < 0x80) utf8.push(charcode);
+      else if (charcode < 0x800) {
+          utf8.push(0xc0 | (charcode >> 6),
+                    0x80 | (charcode & 0x3f));
+      }
+      else if (charcode < 0xd800 || charcode >= 0xe000) {
+          utf8.push(0xe0 | (charcode >> 12),
+                    0x80 | ((charcode>>6) & 0x3f),
+                    0x80 | (charcode & 0x3f));
+      }
+      // surrogate pair
+      else {
+          i++;
+          // UTF-16 encodes 0x10000-0x10FFFF by
+          // subtracting 0x10000 and splitting the
+          // 20 bits of 0x0-0xFFFFF into two halves
+          charcode = 0x10000 + (((charcode & 0x3ff)<<10)
+                    | (str.charCodeAt(i) & 0x3ff));
+          utf8.push(0xf0 | (charcode >>18),
+                    0x80 | ((charcode>>12) & 0x3f),
+                    0x80 | ((charcode>>6) & 0x3f),
+                    0x80 | (charcode & 0x3f));
+      }
+  }
+  return utf8;
 }
 
 function verifySignatureWithOrWithoutWrapping(encodedData: Uint8Array, signature: string, publicKey: string): boolean {

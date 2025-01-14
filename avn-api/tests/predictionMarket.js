6599ea8b519baa42bf07ad2c6b76395fc71c8f69
Micael Franco <micael.franco@aventus.io>
2025-01-14 08:55:28 +0000

    SYS-4298: Improve gateway tests

--- avn-api/tests/predictionMarket.js
+++ avn-api/tests/predictionMarket.js
@@ -21,13 +21,12 @@ async function waitForEndBlock(api, finalBlock) {
 }
 
 describe('Prediction Market tests', async () => {
-  let api, otherUserApi, blockRange;
+  let api, otherUserApi, blockRange, winnerOutcomeTokens;
   let user = accounts.user;
   let otherUser = accounts.otherUser;
   let token = "0xbfaffd8001493dfeb51c26748d2aff53c2984190";
   const buyAmount = "10000000000";
 
-  let marketId = 11;
   let winningChoice = 1;
   let losingChoice = 0;
 
@@ -53,7 +52,7 @@ describe('Prediction Market tests', async () => {
     let requestId;
     before(async () => {
       blockRange = await getBlockRange(api);
-      const amount = 10000000000000;
+      const amount = 1000000000000;
       const spotPrices = [5000000000, 5000000000];
 
       requestId = await api.send.createMarketAndDeployPool(
@@ -65,6 +64,8 @@ describe('Prediction Market tests', async () => {
         amount,
         spotPrices,
       );
+      marketId = (await api.query.getMarketCounter() - 1);
+      console.log(`marketId: ${marketId}`);
     });
 
     describe('succeeds if', async function () {
@@ -79,24 +80,61 @@ describe('Prediction Market tests', async () => {
   });
 
   describe('Transfer PM tokens', function () {
-    let requestId;
+    let requestId, senderBalanceBefore, receiverBalanceBefore, senderBalanceAfter, receiverBalanceAfter;
     before(async () => {
+      senderBalanceBefore = (await api.query.getMarketTokenBalance(user.address, { ForeignAsset: 0 })).free;
+      receiverBalanceBefore = (await api.query.getMarketTokenBalance(otherUser.address, { ForeignAsset: 0 })).free;
+
       requestId = await api.send.transferMarketToken(token, otherUser.address, buyAmount);
+
+      senderBalanceAfter = (await api.query.getMarketTokenBalance(user.address, { ForeignAsset: 0 })).free;
+      receiverBalanceAfter = (await api.query.getMarketTokenBalance(otherUser.address, { ForeignAsset: 0 })).free;
     });
 
     describe('succeeds if', function () {
       it('Request is processed', async function () {
         await helper.confirmStatus(api.poll, requestId, 'Processed');
       });
-      xit('PM Balance if updated correctly', function () {});
+      it('PM Balance if updated correctly', function () {
+        assert.equal(receiverBalanceBefore.add(new BN(buyAmount)).toString(), receiverBalanceAfter.toString());
+        assert.equal(senderBalanceBefore.sub(new BN(buyAmount)).toString(), senderBalanceAfter.toString());
+      });
     });
   });
 
   describe('Buy tokens via hybrid router', function () {
-    let requestId, requestId2;
+    let requestId, requestId2, winnerTokenBalanceBefore, winnerTokenBalanceAfter, loserTokenBalanceBefore, loserTokenBalanceAfter, loserOutcomeTokens;
     before(async () => {
+      let poolInfo = await api.query.getMarketPoolInfo(marketId);
+      let choiceReserve = getOutcomeReserve(poolInfo.reserves, marketId, winningChoice);
+      winnerOutcomeTokens = await calculateSwapAmountOutForBuy(
+        new Decimal(choiceReserve),
+        new Decimal(buyAmount),
+        new Decimal(poolInfo.liquidityParameter),
+        new Decimal(0.03),
+        new Decimal(0)
+      );
+
+      winnerTokenBalanceBefore = (await api.query.getMarketTokenBalance(user.address, { CategoricalOutcome: [marketId, winningChoice] })).free;
+      loserTokenBalanceBefore = (await api.query.getMarketTokenBalance(user.address, { CategoricalOutcome: [marketId, winningChoice] })).free;
+
       requestId = await api.send.buyMarketOutcomeTokens(marketId, winningChoice, buyAmount, "7500000000");
+
+      // Since pool is afected after the first buy we need to recalculate here
+      poolInfo = (await avnManager.queryMarketPoolInfo(marketId)).toJSON();
+      choiceReserve = getOutcomeReserve(poolInfo.reserves, marketId, losingChoice);
+      loserOutcomeTokens = await calculateSwapAmountOutForBuy(
+        new Decimal(choiceReserve),
+        new Decimal(buyAmount),
+        new Decimal(poolInfo.liquidityParameter),
+        new Decimal(0.03),
+        new Decimal(0)
+      );
+
       requestId2 = await otherUserApi.send.buyMarketOutcomeTokens(marketId, losingChoice, buyAmount, "7500000000");
+
+      winnerTokenBalanceAfter = (await api.query.getMarketTokenBalance(user.address, { CategoricalOutcome: [marketId, winningChoice] })).free;
+      loserTokenBalanceAfter = (await api.query.getMarketTokenBalance(user.address, { CategoricalOutcome: [marketId, winningChoice] })).free;
     });
 
     describe('succeeds if', function () {
@@ -104,8 +142,12 @@ describe('Prediction Market tests', async () => {
         await helper.confirmStatus(api.poll, requestId, 'Processed');
         await helper.confirmStatus(otherUserApi.poll, requestId2, 'Processed');
       });
-      xit('Winning token is correctly bought', function () {});
-      xit('Losing token is correctly bought', function () {});
+      it('Winning token is correctly bought', function () {
+        assert.equal(winnerTokenBalanceBefore.add(new BN(winnerOutcomeTokens)).toString(), winnerTokenBalanceAfter.toString());
+      });
+      it('Losing token is correctly bought', function () {
+        assert.equal(loserTokenBalanceBefore.add(new BN(loserOutcomeTokens)).toString(), loserTokenBalanceAfter.toString());
+      });
     });
   });
 
@@ -128,10 +170,16 @@ describe('Prediction Market tests', async () => {
   });
 
   describe('redeemShares', function () {
-    let requestId, requestId2;
+    let requestId, requestId2, winnerBalanceBefore, winnerBalanceAfter, loserBalanceBefore, loserBalanceAfter;
     before(async () => {
+      winnerBalanceBefore = (await api.query.getMarketTokenBalance(user.address, { ForeignAsset: 0 })).free;
+      loserBalanceBefore = (await api.query.getMarketTokenBalance(otherUser.address, { ForeignAsset: 0 })).free;
+
       requestId = await api.send.redeemMarketShares(marketId);
       requestId2 = await otherUserApi.send.redeemMarketShares(marketId);
+
+      winnerBalanceAfter = (await api.query.getMarketTokenBalance(user.address, { ForeignAsset: 0 })).free;
+      loserBalanceAfter = (await api.query.getMarketTokenBalance(otherUser.address, { ForeignAsset: 0 })).free;
     });
 
     describe('succeeds if', function () {
@@ -140,19 +188,28 @@ describe('Prediction Market tests', async () => {
         await helper.confirmStatus(otherUserApi.poll, requestId2, 'Processed');
       });
 
-      it('Rewards are collected for winning user', function () {});
-      it('Rewards are zero for losing user', function () {});
+      it('Rewards are collected for winning user', function () {
+        assert.equal(winnerBalanceBefore.add(new BN(winnerOutcomeTokens)).toString(), winnerBalanceAfter.toString());
+      });
+      it('Rewards are zero for losing user', function () {
+        assert.equal(loserBalanceBefore.toString(), loserBalanceAfter.toString());
+      });
     });
   });
 
   describe('withdraw tokens', function () {
-    let requestId, balanceBeforeWithdraw;
+    let requestId, tokenBalanceBeforeWithdraw, ForeignAssetBalanceBefore, ForeignAssetBalanceAfter, tokenBalanceAfterWithdraw;
     let floor = "100000000";
     let withdrawAmount = "1000000000";
 
     before(async () => {
-      balanceBeforeWithdraw = new BN(await api.query.getTokenBalance(user.address, token));
+      ForeignAssetBalanceBefore = (await api.query.getMarketTokenBalance(user.address, { ForeignAsset: 0 })).free;
+      tokenBalanceBeforeWithdraw = new BN(await api.query.getTokenBalance(user.address, token));
+
       requestId = await api.send.withdrawMarketTokens(token, withdrawAmount);
+
+      ForeignAssetBalanceAfter = (await api.query.getMarketTokenBalance(user.address, { ForeignAsset: 0 })).free;
+      tokenBalanceAfterWithdraw = new BN(await api.query.getTokenBalance(user.address, token));
     });
 
     describe('succeeds if', function () {
@@ -161,10 +218,55 @@ describe('Prediction Market tests', async () => {
       });
 
       it('Tokens are withdrawn', async function () {
-        let balanceAfterWithdraw = new BN(await api.query.getTokenBalance(user.address, token));
-        let flooredWithdrawAmount = balanceBeforeWithdraw.div(new BN(floor));
-        assert.equal(balanceBeforeWithdraw.toString(), balanceAfterWithdraw.add(new BN(flooredWithdrawAmount)).toString());
+        let flooredWithdrawAmount = tokenBalanceBeforeWithdraw.div(new BN(floor));
+        assert.equal(tokenBalanceBeforeWithdraw.add(new BN(flooredWithdrawAmount)).toString(), tokenBalanceAfterWithdraw.toString());
+        assert.equal(ForeignAssetBalanceBefore.toString(), ForeignAssetBalanceAfter.add(new BN(withdrawAmount)).toString());
       });
     });
   });
 });
+
+function getOutcomeReserve(reserves, marketId, choice) {
+  return reserves[`{"categoricalOutcome":[${marketId},${choice}]}`];
+}
+
+// buy outcome token with the base asset
+// 0.01 for 1% on poolFee and creatorFee
+async function calculateSwapAmountOutForBuy(reserve, amountIn, liquidity, poolFee, creatorFee) {
+  // remove the fees before executing the buy
+  const totalFee = poolFee.plus(creatorFee);
+  const feeMultiplier = new Decimal(1).minus(totalFee);
+  const amountInMinusFees = amountIn.mul(feeMultiplier);
+
+  const exp1 = amountInMinusFees.div(liquidity).exp();
+  const exp2 = new Decimal(0).minus(reserve.div(liquidity)).exp();
+
+  return Math.round(exp1
+    .minus(new Decimal(1))
+    .plus(exp2)
+    .ln()
+    .mul(liquidity)
+    .plus(reserve)
+    .minus(amountIn)
+    .plus(amountIn).toNumber());
+};
+
+// sell outcome token for the base asset
+async function calculateSwapAmountOutForSell(reserve, amountIn, liquidity, poolFee, creatorFee) {
+  const exp1 = amountIn.plus(reserve).div(liquidity).exp();
+  const exp2 = amountIn.div(liquidity).exp();
+
+  const amountOut = exp1
+    .minus(exp2)
+    .plus(1)
+    .ln()
+    .mul(liquidity)
+    .mul(-1)
+    .plus(reserve)
+    .plus(amountIn);
+
+  // remove the fees after executing the sell
+  const totalFee = poolFee.plus(creatorFee);
+  const feeMultiplier = new Decimal(1).minus(totalFee);
+  return amountOut.mul(feeMultiplier);
+};

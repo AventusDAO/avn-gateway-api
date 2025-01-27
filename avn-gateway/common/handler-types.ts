6cb11cf6097e30d2f5765c3f42c79118d44bebf5
ivan-cholakov <ivan.cholakov@aventus.io>
2025-01-27 16:42:13 +0200

    feat: new query endpoint to get checkpoint by originId

--- avn-gateway/common/handler-types.ts
+++ avn-gateway/common/handler-types.ts
@@ -220,6 +220,7 @@ export interface CallParams {
     ethTokenAddress?: string;
     txHash?: string;
     predictionMarketAsset?: PredictionMarketAsset;
+    originId?: string;
 };
 
 type PredictionMarketAsset = 
